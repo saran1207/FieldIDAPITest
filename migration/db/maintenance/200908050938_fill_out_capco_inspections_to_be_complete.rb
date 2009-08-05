29935e93e902a418c475b530c09f1c3926b12d9a
Mark Frederiksen <mark.frederiksen@fieldid.com>
2009-08-05 13:46:11 +0000

    Added capco inspection import fix

---
+++ migration/db/maintenance/200908050938_fill_out_capco_inspections_to_be_complete.rb
@@ -0,0 +1,24 @@
+require "organization"
+require "inspection_completor"
+require "state_set"
+
+class FillOutCapcoInspectionsToBeComplete < ActiveRecord::Migration
+  def self.up
+    capco = Organization.find(:first, :conditions => { :name => "capco" })
+    cm_button_group = StateSet.find(:first, :conditions => { :name => "CM Default", :r_tenant => capco.id })
+    
+    skip_state = nil
+    
+    cm_button_group.states.each do |state|
+      skip_state = state unless state.displaytext != "Skip"
+    end
+    
+    inspection_completor = InspectionCompletor.new(capco, skip_state, cm_button_group)
+    inspection_completor.process
+  
+  end
+  
+  def self.down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
