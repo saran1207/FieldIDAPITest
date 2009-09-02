1e983e7f78b10f7b7acc1034124573014eae577b
Jesse Miller <jesse.miller@fieldid.com>
2009-09-02 15:25:35 +0000

    WEB-1134
    Made local subscription agent properly return prices

--- rfidEJB/src/com/n4systems/subscription/local/LocalSubscriptionAgent.java
+++ rfidEJB/src/com/n4systems/subscription/local/LocalSubscriptionAgent.java
@@ -1,10 +1,12 @@
 package com.n4systems.subscription.local;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.n4systems.subscription.CommunicationException;
 import com.n4systems.subscription.Company;
 import com.n4systems.subscription.ContractPrice;
+import com.n4systems.subscription.PaymentOption;
 import com.n4systems.subscription.Person;
 import com.n4systems.subscription.PriceCheckResponse;
 import com.n4systems.subscription.SignUpTenantResponse;
@@ -38,10 +40,40 @@ public class LocalSubscriptionAgent extends SubscriptionAgent {
 	}
 
 	@Override
-	public List<ContractPrice> retrieveContractPrices()
-			throws CommunicationException {
-		// TODO Auto-generated method stub
-		return null;
+	public List<ContractPrice> retrieveContractPrices()	throws CommunicationException {
+		List<ContractPrice> contractPrices = new ArrayList<ContractPrice>();
+		
+		contractPrices.add(populateContractPrice(134L, "FIDFREE", PaymentOption.MONTH_TO_MONTH, 0F));
+		contractPrices.add(populateContractPrice(134L, "FIDFREE", PaymentOption.ONE_YEAR_UP_FRONT, 0F));
+		contractPrices.add(populateContractPrice(134L, "FIDFREE", PaymentOption.TWO_YEARS_UP_FRONT, 0F));
+		
+		contractPrices.add(populateContractPrice(146L, "FIDBASIC", PaymentOption.MONTH_TO_MONTH, 40F));
+		contractPrices.add(populateContractPrice(146L, "FIDBASIC", PaymentOption.ONE_YEAR_UP_FRONT, 40F));
+		contractPrices.add(populateContractPrice(157L, "FIDBASIC", PaymentOption.TWO_YEARS_UP_FRONT, 40F));
+
+		contractPrices.add(populateContractPrice(143L, "FIDPLUS", PaymentOption.MONTH_TO_MONTH, 100F));
+		contractPrices.add(populateContractPrice(143L, "FIDPLUS", PaymentOption.ONE_YEAR_UP_FRONT, 100F));
+		contractPrices.add(populateContractPrice(159L, "FIDPLUS", PaymentOption.TWO_YEARS_UP_FRONT, 100F));
+
+		contractPrices.add(populateContractPrice(149L, "FIDENTERPRISE", PaymentOption.MONTH_TO_MONTH, 175F));
+		contractPrices.add(populateContractPrice(149L, "FIDENTERPRISE", PaymentOption.ONE_YEAR_UP_FRONT, 175F));
+		contractPrices.add(populateContractPrice(158L, "FIDENTERPRISE", PaymentOption.TWO_YEARS_UP_FRONT, 175F));
+
+		contractPrices.add(populateContractPrice(151L, "FIDUNLIMITED", PaymentOption.MONTH_TO_MONTH, 225F));
+		contractPrices.add(populateContractPrice(151L, "FIDUNLIMITED", PaymentOption.ONE_YEAR_UP_FRONT, 225F));
+		contractPrices.add(populateContractPrice(160L, "FIDUNLIMITED", PaymentOption.TWO_YEARS_UP_FRONT, 225F));
+
+		return contractPrices;
+	}
+	
+	private LocalContractPrice populateContractPrice(Long externalId, String syncId, PaymentOption paymentOption, Float price) {
+		LocalContractPrice localContractPrice = new LocalContractPrice();
+		localContractPrice.setExternalId(externalId);
+		localContractPrice.setPaymentOption(paymentOption);
+		localContractPrice.setPrice(price);
+		localContractPrice.setSyncId(syncId);
+		
+		return localContractPrice;
 	}
 
 }
