e9b44d5737a3c6da41ff92b801867457106d36f0
Travis Choi <tchoi@fieldid.com>
2009-09-09 21:31:19 +0000

    Refactor customer, division and organization to work with the web (MOBILE-445)

--- rfidEJB/src/com/n4systems/webservice/dto/DivisionOrgListResponse.java
+++ rfidEJB/src/com/n4systems/webservice/dto/DivisionOrgListResponse.java
@@ -1,10 +1,11 @@
 package com.n4systems.webservice.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DivisionOrgListResponse extends AbstractListResponse {
 
-	private List<DivisionOrgServiceDTO> divisions;
+	private List<DivisionOrgServiceDTO> divisions = new ArrayList<DivisionOrgServiceDTO>();
 
 	public List<DivisionOrgServiceDTO> getDivisions() {
 		return divisions;
