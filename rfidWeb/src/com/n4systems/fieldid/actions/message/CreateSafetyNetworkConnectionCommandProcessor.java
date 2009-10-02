d2f23e1d77db6a93dbeb0db31989b15deac4d844
Alex Aitken <alex.aitken@fieldid.com>
2009-10-02 14:08:07 +0000

    Fixed the test for next schedule date.
    This had to do with using millisecond addition for days ( daylight savings time )

---
+++ rfidWeb/src/com/n4systems/fieldid/actions/message/CreateSafetyNetworkConnectionCommandProcessor.java
@@ -0,0 +1,71 @@
+package com.n4systems.fieldid.actions.message;
+
+import rfid.ejb.entity.UserBean;
+
+import com.n4systems.model.messages.CreateSafetyNetworkConnectionMessageCommand;
+import com.n4systems.model.messages.MessageCommandSaver;
+import com.n4systems.model.orgs.BaseOrg;
+import com.n4systems.model.orgs.InternalOrg;
+import com.n4systems.model.safetynetwork.OrgConnection;
+import com.n4systems.model.safetynetwork.OrgConnectionSaver;
+import com.n4systems.persistence.Transaction;
+import com.n4systems.persistence.loaders.NonSecureLoaderFactory;
+import com.n4systems.util.ConfigContext;
+import com.n4systems.util.ConfigEntry;
+
+public class CreateSafetyNetworkConnectionCommandProcessor {
+
+	private UserBean actor;
+	
+	private CreateSafetyNetworkConnectionMessageCommand command;
+	
+	private NonSecureLoaderFactory nonSecureLoaderFactory;
+
+	public void process(Transaction transaction) {
+		OrgConnection connection = buildConnection(command);
+		
+		saveConnection(transaction, connection);
+		
+		markCommandProcessed(command);
+	}
+	
+	private void saveConnection(Transaction transaction, OrgConnection connection) {
+		OrgConnectionSaver saver = new OrgConnectionSaver(ConfigContext.getCurrentContext().getLong(ConfigEntry.HOUSE_ACCOUNT_ID));
+		saver.save(transaction, connection);
+	}
+
+	private void markCommandProcessed(CreateSafetyNetworkConnectionMessageCommand command) {
+		command.setProcessed(true);
+		new MessageCommandSaver().update(command);
+	}
+
+	private OrgConnection buildConnection(CreateSafetyNetworkConnectionMessageCommand command) {
+		OrgConnection connection = new OrgConnection();
+		connection.setModifiedBy(actor);
+		
+		connection.setVendor(fetchOrg(command.getVendorOrgId()));
+		connection.setCustomer(fetchOrg(command.getCustomerOrgId()));
+		
+		return connection;
+	}
+
+	private InternalOrg fetchOrg(Long orgId) {
+		return (InternalOrg)nonSecureLoaderFactory.createNonSecureIdLoader(BaseOrg.class).setId(orgId).load();
+	}
+
+	
+	public CreateSafetyNetworkConnectionCommandProcessor setActor(UserBean actor) {
+		this.actor = actor;
+		return this;
+	}
+
+	public CreateSafetyNetworkConnectionCommandProcessor setCommand(CreateSafetyNetworkConnectionMessageCommand command) {
+		this.command = command;
+		return this;
+	}
+
+	public CreateSafetyNetworkConnectionCommandProcessor setNonSecureLoaderFactory(NonSecureLoaderFactory nonSecureLoaderFactory) {
+		this.nonSecureLoaderFactory = nonSecureLoaderFactory;
+		return this;
+	}
+}
--- rfidWeb/test/com/n4systems/fieldid/actions/helpers/InspectionScheduleSuggestionTest.java
+++ rfidWeb/test/com/n4systems/fieldid/actions/helpers/InspectionScheduleSuggestionTest.java
@@ -14,21 +14,21 @@ import com.n4systems.util.DateHelper;
 
 public class InspectionScheduleSuggestionTest {
 	
-	@Test public void test_suggested_schedule_with_no_schedulesid() {
+	@Test public void should_suggested_schedule_with_no_schedulesid() {
 		List<InspectionSchedule> schedules = new ArrayList<InspectionSchedule>();
 		
 		InspectionScheduleSuggestion suggestion = new InspectionScheduleSuggestion(schedules);
 		assertEquals(InspectionScheduleSuggestion.NO_SCHEDULE, suggestion.getSuggestedScheduleId());
 	}
 	
-	@Test public void test_suggested_schedule_with_1_schedule_inside_30_days() {
+	@Test public void should_suggested_schedule_with_1_schedule_inside_30_days() {
 		List<InspectionSchedule> schedules = new ArrayList<InspectionSchedule>();
 		schedules.add(createSchedule(DateHelper.getTomorrow(), 1L));
 		InspectionScheduleSuggestion suggestion = new InspectionScheduleSuggestion(schedules);
 		assertEquals(new Long(1), suggestion.getSuggestedScheduleId());
 	}
 	
-	@Test public void test_suggested_schedule_with_2_schedule_inside_30_days() {
+	@Test public void should_suggested_schedule_with_2_schedule_inside_30_days() {
 		List<InspectionSchedule> schedules = new ArrayList<InspectionSchedule>();
 		schedules.add(createSchedule(DateHelper.getTomorrow(), 1L));
 		schedules.add(createSchedule(DateHelper.getToday(), 2L));
@@ -36,7 +36,7 @@ public class InspectionScheduleSuggestionTest {
 		assertEquals(new Long(2), suggestion.getSuggestedScheduleId());
 	}
 	
-	@Test public void test_suggested_schedule_with_yesterday_vs_tomorrow() {
+	@Test public void should_suggested_schedule_with_yesterday_vs_tomorrow() {
 		List<InspectionSchedule> schedules = new ArrayList<InspectionSchedule>();
 		schedules.add(createSchedule(DateHelper.getTomorrow(), 1L));
 		schedules.add(createSchedule(DateHelper.getYesterday(), 2L));
@@ -44,7 +44,7 @@ public class InspectionScheduleSuggestionTest {
 		assertEquals(new Long(2), suggestion.getSuggestedScheduleId());
 	}
 	
-	@Test public void test_suggests_no_schedule_when_schedules_are_more_than_30_days_away() {
+	@Test public void should_suggests_no_schedule_when_schedules_are_more_than_30_days_away() {
 		List<InspectionSchedule> schedules = new ArrayList<InspectionSchedule>();
 		schedules.add(createSchedule(DateHelper.addDaysToDate(DateHelper.getToday(),31L), 1L));
 		schedules.add(createSchedule(DateHelper.addDaysToDate(DateHelper.getToday(),-41L), 2L));
@@ -52,7 +52,7 @@ public class InspectionScheduleSuggestionTest {
 		assertEquals(InspectionScheduleSuggestion.NO_SCHEDULE, suggestion.getSuggestedScheduleId());
 	}
 	
-	@Test public void test_suggested_schedule_with_today_vs_yesterday() {
+	@Test public void should_suggested_schedule_with_today_vs_yesterday() {
 		List<InspectionSchedule> schedules = new ArrayList<InspectionSchedule>();
 		schedules.add(createSchedule(DateHelper.getToday(), 1L));
 		schedules.add(createSchedule(DateHelper.getYesterday(), 2L));
@@ -60,7 +60,7 @@ public class InspectionScheduleSuggestionTest {
 		assertEquals(new Long(1), suggestion.getSuggestedScheduleId());
 	}
 	
-	@Test public void test_suggested_schedule_with_today_vs_tomorrow() {
+	@Test public void should_suggested_schedule_with_today_vs_tomorrow() {
 		List<InspectionSchedule> schedules = new ArrayList<InspectionSchedule>();
 		schedules.add(createSchedule(DateHelper.getToday(), 1L));
 		schedules.add(createSchedule(DateHelper.getTomorrow(), 2L));
