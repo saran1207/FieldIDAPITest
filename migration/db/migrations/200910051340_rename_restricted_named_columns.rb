ec5e3495d0586421053000fc36ff01135403aa12
Alex Aitken <alex.aitken@fieldid.com>
2009-10-05 19:51:18 +0000

    fixed migration to work with trunk line changes.

--- migration/db/migrations/200910051340_rename_restricted_named_columns.rb
+++ migration/db/migrations/200910051340_rename_restricted_named_columns.rb
@@ -1,8 +1,7 @@
 class RenameRestrictedNamedColumns < ActiveRecord::Migration
   def self.up
     rename_column(:lineitems, :index, :idx)
-    rename_column(:tagoptions, :key, :optionkey)
-    rename_column(:findproductoption, :key, :identifier)
-    rename_column(:configurations, :key, :identifier)
+    rename_column(:findproductoption, :optionkey, :identifier)
+    rename_column(:configurations, :name, :identifier)
   end
 end
