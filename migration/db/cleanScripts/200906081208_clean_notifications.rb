d3c82e505dc1945a7c412f741449bd518f6f92b1
Alex Aitken <alex.aitken@fieldid.com>
2009-10-07 18:46:25 +0000

    fixed clean script.

--- migration/db/cleanScripts/200906081208_clean_notifications.rb
+++ migration/db/cleanScripts/200906081208_clean_notifications.rb
@@ -1,6 +1,5 @@
 class CleanNotifications < ActiveRecord::Migration
   def self.up
-	  execute "delete from notificationsettings_owner"
 	  execute "delete from notificationsettings_inspectiontypes"
 	  execute "delete from notificationsettings_addresses"
 	  execute "delete from notificationsettings_producttypes"
