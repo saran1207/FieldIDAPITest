ea907f2dc5ad3c27038166cf1a24f184856d2721
Mark Frederiksen <mark.frederiksen@fieldid.com>
2009-11-19 19:29:25 +0000

    WEB-1400
    Removed possible null pointer in CriteriaSetValidator
    Also made it validate correctly

--- rfidWeb/src/com/n4systems/fieldid/validators/inspection/CriteriaSetValidator.java
+++ rfidWeb/src/com/n4systems/fieldid/validators/inspection/CriteriaSetValidator.java
@@ -8,30 +8,41 @@ import com.n4systems.model.CriteriaSection;
 import com.opensymphony.xwork2.validator.ValidationException;
 import com.opensymphony.xwork2.validator.validators.FieldValidatorSupport;
 
+/**
+ * Validates that all CriteriaSections have at least one non-retired Criteria
+ */
 public class CriteriaSetValidator extends FieldValidatorSupport {
 
 	@SuppressWarnings("unchecked")
-	public void validate( Object object ) throws ValidationException {
+	public void validate(Object object) throws ValidationException {
 		String fieldName = getFieldName();
 		AbstractAction action = (AbstractAction)object;
-		Collection<CriteriaSection> set = (Collection<CriteriaSection>)this.getFieldValue(fieldName, action);
 		
-		if( set != null && !set.isEmpty() ) {
-			for( CriteriaSection section : set ) {
-				if( section.getCriteria() != null && !section.getCriteria().isEmpty() && !section.isRetired()  ) {
-					boolean foundNonRetiredCriteria = false;
-					for( Criteria criteria : section.getCriteria() ) {
-						if( criteria != null && !criteria.isRetired() ) {
-							foundNonRetiredCriteria = true;	
-							return;
-						}
-					}
-					if( foundNonRetiredCriteria == false ) {
-						addFieldError( fieldName, action );
-					}
+		Collection<CriteriaSection> sections = (Collection<CriteriaSection>)getFieldValue(fieldName, action);
+		if (sections == null) {
+			return;
+		}
+	
+		for(CriteriaSection section: sections) {
+			if (section == null || section.isRetired() || section.getCriteria() == null) {
+				continue;
+			}
+			
+			boolean hadNoValidCriteria = true;
+			for(Criteria criteria : section.getCriteria()) {
+				if(criteria != null && !criteria.isRetired()) {
+					hadNoValidCriteria = false;
+					break;
 				}
+			}
+			
+			if (hadNoValidCriteria) {
+				// since we arn't targeting the sections directly for our errors
+				// we can exit after we find the first section with no valid criteria 
+				addFieldError(fieldName, action);
+				return;
 			}
 		}
 	}
-
+	
 }
