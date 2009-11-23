a19dfe495df24868e8b487efec475c17bd0a3b28
Alex Aitken <alex.aitken@fieldid.com>
2009-11-23 17:10:48 +0000

    updated clean script to clear new customer email addresses and the external_username for netsuite has an _ added to the end.

--- migration/db/cleanScripts/200807301757_clean_email_addresses.rb
+++ migration/db/cleanScripts/200807301757_clean_email_addresses.rb
@@ -3,9 +3,11 @@ class CleanEmailAddresses < ActiveRecord::Migration
   def self.up
     
     ProductType.transaction do 
-      execute "update customers set contactemail = 'dev@n4systems.com'"
+      
       execute "update users set emailaddress = 'dev@n4systems.com'"
-      execute "update organization set adminemail = 'dev@n4systems.com'"
+      execute "update org_division set contactemail = 'dev@n4systems.com'"
+      execute "update org_customer set contactemail = 'dev@n4systems.com'"
+      execute "update org_primary set externalusername = concat(externalusername, '_')"
     end
 
   end
