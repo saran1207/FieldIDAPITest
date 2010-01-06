a0506a14eccdbf8381b9d083db9379b6b15abfc5
Alex Aitken <alex.aitken@fieldid.com>
2010-01-06 19:03:39 +0000

    WEB-1499
    the map is now checked before accessing the first key.  This will stop the null pointer and produce a file processing exception instead.

--- rfidWeb/src/com/n4systems/webservice/server/InspectionServiceImpl.java
+++ rfidWeb/src/com/n4systems/webservice/server/InspectionServiceImpl.java
@@ -9,6 +9,7 @@ import org.apache.log4j.Logger;
 
 import rfid.ejb.entity.UserBean;
 
+import com.n4systems.exceptions.FileProcessingException;
 import com.n4systems.fileprocessing.FileProcessorFactory;
 import com.n4systems.model.Inspection;
 import com.n4systems.tools.FileDataContainer;
@@ -44,52 +45,72 @@ public class InspectionServiceImpl extends AbstractWebServiceImpl implements Ins
 		//authenticate the user
 		UserBean user = authenticateUser(authUser);
 		
-		// used in processing below
-		Map<String, Inspection> inspectionMap;
-		FileDataContainer dataContainer;
+		List<ProofTestStatusBundle> statusList = new ArrayList<ProofTestStatusBundle>();
+		
+		for(ProofTestBundle bundle: bundles) {
+			prcessBundle(authUser, user, statusList, bundle);
+		}
+		
+		return statusList;
+	}
+
+	private void prcessBundle(AuthBundle authUser, UserBean user, List<ProofTestStatusBundle> statusList, ProofTestBundle bundle) {
+		try {
+			
+			logger.info("Processing Inspection File [" + bundle.getFileName() + "] for tenant [" + authUser.getTenantName() + "] user [" + authUser.getUserName() + "]");
+			FileDataContainer dataContainer = FileProcessorFactory.getFileProcessor(bundle.getFileType()).processFile(bundle.getFileData(), bundle.getFileName());
+
+			configureDataContainerForWebServiceProcessing(dataContainer, bundle);
+			
+			// proocess the file data container
+			Map<String, Inspection> inspectionMap = ServiceLocator.getProofTestHandler().inspectionServiceUpload(dataContainer, user);
+			
+			
+			
+			applyProcessingResults(statusList, inspectionMap, bundle);
+		} catch (Exception e) {
+			logger.error("Failure during processing of file: " + bundle.getFileName(), e);
+			statusList.add(new ProofTestStatusBundle(bundle.getFileName(), WebServiceStatus.FAILED, "Unable to process file"));
+		}
+	}
+
+	
+	private void configureDataContainerForWebServiceProcessing(FileDataContainer dataContainer, ProofTestBundle bundle) {
+		// calls coming in from the web service should always be set to resolve a customer
+		dataContainer.setResolveCustomer(true);
+		dataContainer.setCreateCustomer(bundle.isCreateCustomer());
+		dataContainer.setCreateProduct(bundle.isCreateProduct());
+	}
+
+	/*
+	 * the inspection map allows for multiple serials to have been processed out of one dataContainer (aka one proof test).
+	 * This functionality really only exists for CG's use and is not a standard of any proof test type.
+	 * For simplicity, we're going to assume there was only one product serial in the file and thus only have a single entry in our inspectionMap
+	 */
+	private void applyProcessingResults(List<ProofTestStatusBundle> statusListCollector, Map<String, Inspection> inspectionMap, ProofTestBundle bundle) throws FileProcessingException {
 		String serial;
 		String message;
 		WebServiceStatus status;
 		
-		List<ProofTestStatusBundle> statusList = new ArrayList<ProofTestStatusBundle>();
-		for(ProofTestBundle bundle: bundles) {
-			try {
-	
-				logger.info("Processing Inspection File [" + bundle.getFileName() + "] for tenant [" + authUser.getTenantName() + "] user [" + authUser.getUserName() + "]");
-				dataContainer = FileProcessorFactory.getFileProcessor(bundle.getFileType()).processFile(bundle.getFileData(), bundle.getFileName());
-	
-				// calls comming in from the web service should always be set to resolve a customer
-				dataContainer.setResolveCustomer(true);
-				dataContainer.setCreateCustomer(bundle.isCreateCustomer());
-				dataContainer.setCreateProduct(bundle.isCreateProduct());
-				
-				// proocess the file data container
-				inspectionMap = ServiceLocator.getProofTestHandler().inspectionServiceUpload(dataContainer, user);
-				
-				/*
-				 * the inspection map allows for multiple serials to have been processed out of one dataContainer (aka one proof test).
-				 * This functionality really only exists for CG's use and is not a standard of any proof test type.
-				 * For simplicity, we're going to assume there was only one product serial in the file and thus only have a single entry in our inspectionMap
-				 */
-				serial = inspectionMap.keySet().iterator().next();
-				
-				if(inspectionMap.get(serial) != null) {
-					status = WebServiceStatus.SUCCESSFUL;
-					message = "Created or updated an inspection for serial number: " + serial;
-					
-				} else {
-					status = WebServiceStatus.FAILED;
-					message = "Unable to create or update an inspection for serial number: " + serial;
-				}
-				// construct the status message
-				statusList.add(new ProofTestStatusBundle(bundle.getFileName(), status, message));
-			} catch (Exception e) {
-				logger.error("Failure during processing of file: " + bundle.getFileName(), e);
-				statusList.add(new ProofTestStatusBundle(bundle.getFileName(), WebServiceStatus.FAILED, "Unable to process file"));
-			}
+		if (inspectionMap.isEmpty()) {
+			throw new FileProcessingException("File had no serail numbers defined.");
 		}
 		
-		return statusList;
+		serial = getSerialNumber(inspectionMap);
+		
+		if(inspectionMap.get(serial) != null) {
+			status = WebServiceStatus.SUCCESSFUL;
+			message = "Created or updated an inspection for serial number: " + serial;
+		} else {
+			status = WebServiceStatus.FAILED;
+			message = "Unable to create or update an inspection for serial number: " + serial;
+		}
+		// construct the status message
+		statusListCollector.add(new ProofTestStatusBundle(bundle.getFileName(), status, message));
+	}
+
+	private String getSerialNumber(Map<String, Inspection> inspectionMap) {
+		return inspectionMap.keySet().iterator().next();
 	}
 
 }
