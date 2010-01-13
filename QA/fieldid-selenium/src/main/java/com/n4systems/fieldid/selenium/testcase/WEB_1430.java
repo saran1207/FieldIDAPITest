c7895c321f69d4ce37a5d50aeb8f587df865d019
Darrell Grainger <darrel.grainger@fieldid.com>
2010-01-13 22:12:11 +0000

    Test case for WEB-1430. Currently configured such that all users need to have their password reset to the usual password.

---
+++ QA/fieldid-selenium/src/main/java/com/n4systems/fieldid/selenium/testcase/WEB_1430.java
@@ -0,0 +1,63 @@
+package com.n4systems.fieldid.selenium.testcase;
+
+import org.junit.*;
+
+import com.n4systems.fieldid.selenium.FieldIDTestCase;
+import com.n4systems.fieldid.selenium.login.Choose;
+import com.n4systems.fieldid.selenium.login.Login;
+
+public class WEB_1430 extends FieldIDTestCase {
+
+	Login login;
+	Choose choose;
+	
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		login = new Login(selenium, misc);
+		choose = new Choose(selenium, misc);
+		// TODO: SQL code to update all users to have the same, known password
+	}
+
+	@Test
+	public void shouldBeAbleToLogIntoFieldIDAsAdminForTenantsWithDifferentTimeZones() throws Exception {
+		// Assumes all users have had their password set to the same value
+		String password = getStringProperty("password");
+		int max = getIntegerProperty("numcompanies");
+
+		try {
+			for(int i = 0; i < max; i++) {
+				String companyID = getStringProperty("company" + i);
+				String username = getStringProperty("userid" + i);
+				setCompany(companyID);
+				loginAcceptingEULAIfNecessary(username, password);
+				login.verifySignedIn();
+				misc.gotoSignOut();
+			}
+		} catch(Exception e) {
+			throw e;
+		}
+	}
+	
+	private void loginAcceptingEULAIfNecessary(String username, String password) {
+		login.setUserName(username);
+		login.setPassword(password);
+		login.gotoSignIn();
+		if(misc.isEULA()) {
+			misc.scrollToBottomOfEULA();
+			misc.gotoAcceptEULA();
+		}
+	}
+
+	private void setCompany(String companyID) {
+		misc.info("Changing to company '" + companyID);
+		login.gotoIsNotTheCompanyIWant();
+		choose.setCompanyID(companyID);
+		choose.gotoFindSignInPage();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
+}
