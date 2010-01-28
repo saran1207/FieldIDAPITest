e5384c37d846417c66bb356ae4823d730efda2e1
Travis Choi <tchoi@fieldid.com>
2010-01-28 19:34:37 +0000

    If an end user mass updates the location, the modified by field does not get updated. (MOBILE-598)

--- rfidEJB/src/com/n4systems/webservice/dto/limitedproductupdate/LimitedProductUpdateRequest.java
+++ rfidEJB/src/com/n4systems/webservice/dto/limitedproductupdate/LimitedProductUpdateRequest.java
@@ -1,11 +1,13 @@
 package com.n4systems.webservice.dto.limitedproductupdate;
 
+import static com.n4systems.webservice.dto.MobileDTOHelper.*;
 import com.n4systems.webservice.dto.RequestInformation;
 
 public class LimitedProductUpdateRequest extends RequestInformation {
 	
 	private ProductLookupInformation productLookupInformation = new ProductLookupInformation();
 	private String location;
+	private long modifiedById;
 
 	public ProductLookupInformation getProductLookupInformation() {
 		return productLookupInformation;
@@ -20,4 +22,16 @@ public class LimitedProductUpdateRequest extends RequestInformation {
 	public void setLocation(String location) {
 		this.location = location;
 	}
+	
+	public long getModifiedById() {
+		return modifiedById;
+	}
+	public void setModifiedById(long modifiedById) {
+		this.modifiedById = modifiedById;
+	}
+	
+	public boolean modifiedByIdExists() {
+		return isValidServerId(modifiedById);
+	}
+	
 }
