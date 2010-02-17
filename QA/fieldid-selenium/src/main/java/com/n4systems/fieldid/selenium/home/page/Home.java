226ecf2d04171f7e0ee621d3c6b8c66e3266d613
Darrell Grainger <darrel.grainger@fieldid.com>
2010-02-17 19:13:19 +0000

    practice merging

--- QA/fieldid-selenium/src/main/java/com/n4systems/fieldid/selenium/home/page/Home.java
+++ QA/fieldid-selenium/src/main/java/com/n4systems/fieldid/selenium/home/page/Home.java
@@ -1,6 +1,8 @@
 package com.n4systems.fieldid.selenium.home.page;
 
 import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+import java.util.List;
 import com.n4systems.fieldid.selenium.lib.FieldIdSelenium;
 import com.n4systems.fieldid.selenium.misc.Misc;
 
@@ -12,6 +14,9 @@ public class Home {
 	
 	// Locators
 	private String homePageHeaderLocator = "xpath=//DIV[@id='contentTitle']/H1[contains(text(),'Home')]";
+	private String jobsAssignedToMeTableXPathLocator = "//DIV[@id='jobs']/TABLE";
+	private String jobsAssignedToMeTableLocator = "xpath=" + jobsAssignedToMeTableXPathLocator;
+	private String noOpenJobsAssignedToYouCellLocator = jobsAssignedToMeTableLocator + "/tbody/tr[1]/td[contains(text(),'You currently have no open jobs assigned to you.')]";
 	
 
 	public Home(FieldIdSelenium selenium, Misc misc) {
@@ -22,5 +27,28 @@ public class Home {
 	public void verifyHomePageHeader() {
 		assertTrue("Could not find the header for the Home page", selenium.isElementPresent(homePageHeaderLocator));
 		misc.checkForErrorMessages(null);
+	}
+	
+	public List<String> getJobIDsAssignedToMe() {
+		List<String> result = new ArrayList<String>();
+		int row = 2;
+		int column = 0;
+		String jobIDCellLocator = jobsAssignedToMeTableLocator + "." + row + "." + column;
+		if(isJobsAssignedToMe() && true) {
+			String jobsAssignedToMeRowXPathLocator = jobsAssignedToMeTableXPathLocator + "/tbody/tr[position() > " + row + "]";
+			Number n = selenium.getXpathCount(jobsAssignedToMeRowXPathLocator);
+			int numRows = n.intValue();
+			for(int currentRow = row; currentRow < numRows+row; currentRow++) {
+				String s = selenium.getTable(jobIDCellLocator);
+				result.add(s);
+				jobIDCellLocator = jobIDCellLocator.replaceFirst("\\." + currentRow + "\\.", "." + (currentRow+1) + ".");
+			}
+		}
+		return result;
+	}
+
+	public boolean isJobsAssignedToMe() {
+		boolean result = !selenium.isElementPresent(noOpenJobsAssignedToYouCellLocator);
+		return result;
 	}
 }
