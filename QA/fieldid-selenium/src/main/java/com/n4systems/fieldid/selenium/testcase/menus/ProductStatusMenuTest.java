78389f515b5afb96a622d270b1ed9ff8f3fe6584
Alex Aitken <alex.aitken@fieldid.com>
2010-02-24 22:31:45 +0000

    WEB-1588
    automation for checking that menu option.

---
+++ QA/fieldid-selenium/src/main/java/com/n4systems/fieldid/selenium/testcase/menus/ProductStatusMenuTest.java
@@ -0,0 +1,25 @@
+package com.n4systems.fieldid.selenium.testcase.menus;
+
+import org.junit.Test;
+
+import com.n4systems.fieldid.selenium.misc.Misc;
+import com.n4systems.fieldid.selenium.testcase.LoggedInTest;
+
+public class ProductStatusMenuTest extends LoggedInTest {
+
+	public ProductStatusMenuTest() {
+		super("n4systems", "makemore$");
+	}
+
+	
+	@Test
+	public void should_be_able_to_go_back_to_the_list_of_product_status_from_the_view_all_link_on_the_menu() throws Exception {
+		selenium.open("/fieldid/productStatusEdit.action");
+		selenium.waitForPageToLoad(Misc.defaultTimeout);
+		
+		selenium.click("link=View All");
+		selenium.waitForPageToLoad(Misc.defaultTimeout);
+		
+		assertEquals("Manage Product Statuses", selenium.getText("css=#contentTitle h1"));
+	}
+}
