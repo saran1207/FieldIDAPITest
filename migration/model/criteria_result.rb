789ace421d85508202f47d32c7038e0c8f2ad0f8
Roumen Pavlov <roumen.pavlov@fieldid.com>
2010-04-22 14:22:04 +0000

    Added associations for recommendations and deficiencies models.

--- migration/model/criteria_result.rb
+++ migration/model/criteria_result.rb
@@ -3,7 +3,8 @@ require "state"
 require "criteria"
 require "abstract_inspection"
 require "tenant"
-
+require "criteriaresults_deficiencies"
+require "criteriaresults_recommendations"
 class CriteriaResult < ActiveRecord::Base
   set_table_name :criteriaresults
 
@@ -12,5 +13,6 @@ class CriteriaResult < ActiveRecord::Base
   belongs_to  :state,         :foreign_key => 'state_id',         :class_name => 'State'
   belongs_to  :criteria,      :foreign_key => 'criteria_id',      :class_name => 'Criteria'
   belongs_to  :inspection,    :foreign_key => 'inspection_id',    :class_name => 'AbstractInspection'
-  
+  has_many :deficiencies,     :foreign_key => 'criteriaresults_id', :class_name => 'CriteriaResultDeficiency'
+  has_many :recommendations,     :foreign_key => 'criteriaresults_id', :class_name => 'CriteriaResultRecommendation'
 end
