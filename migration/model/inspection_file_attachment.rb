8945331040615b1920e89d65e2a92e563f2da323
Roumen Pavlov <roumen.pavlov@fieldid.com>
2010-04-22 13:27:30 +0000

    Changed require inspection to abstract_inspection

--- migration/model/inspection_file_attachment.rb
+++ migration/model/inspection_file_attachment.rb
@@ -1,12 +1,12 @@
 require 'composite_primary_keys'
-require 'inspection'
+require 'abstract_inspection'
 require 'file_attachment'
 
 class InspectionFileAttachment < ActiveRecord::Base
   set_primary_keys :inspections_id, :attachments_id
   set_table_name :inspections_fileattachments
   
-  belongs_to :inspection, :foreign_key => 'inspections_id', :class_name => 'Inspection'
+  belongs_to :inspection, :foreign_key => 'inspections_id', :class_name => 'AbstractInspection'
   belongs_to :attachment, :foreign_key => 'attachments_id', :class_name => 'FileAttachment'
   
 end
