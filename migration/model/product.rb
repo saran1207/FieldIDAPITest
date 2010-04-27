479355ff6345379bdd28f4cc91e1e7455e322ddc
Roumen Pavlov <roumen.pavlov@fieldid.com>
2010-04-27 18:49:58 +0000

    Added ProductStatus association

--- migration/model/product.rb
+++ migration/model/product.rb
@@ -4,20 +4,22 @@ require "info_option"
 require "productserial_infooption"
 require "sub_product"
 require "base_org"
+require "product_status"
+
 #require "rubygems"
 #require 'active_record'
 class Product < ActiveRecord::Base
   set_table_name :products
   
-belongs_to  :tenant,        :foreign_key => 'tenant_id',              :class_name => 'Tenant'
+belongs_to  :tenant,        :foreign_key => 'tenant_id',                :class_name => 'Tenant'
   belongs_to  :productinfo,   :foreign_key => 'type_id',                :class_name => 'ProductType'
   has_many    :infoOptionsFK, :foreign_key => 'r_productserial',        :class_name => 'ProductserialInfooption'
   has_many    :infoOptions,                                             :class_name => 'InfoOption',				:through => :infoOptionsFK
   belongs_to  :identifiedBy,  :foreign_key => 'identifiedby_uniqueid',  :class_name => 'User'
   belongs_to  :assignedUser,  :foreign_key => 'assigneduser_id',        :class_name => 'User'
   has_many    :subProducts,	  :foreign_key => 'masterproduct_id',       :class_name => 'SubProduct'
-  belongs_to  :owner,         :foreign_key => 'owner_id',  :class_name => 'BaseOrg'
-
+  belongs_to  :owner,         :foreign_key => 'owner_id',               :class_name => 'BaseOrg'
+  has_many    :product_statuses,   :foreign_key => 'uniqueid',     :class_name => 'ProductStatus'
   
   def findInfoOptionByInfoField(infoField)
     
