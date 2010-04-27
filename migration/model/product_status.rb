8a2e3f3324a1968ab44b047643c2a7a5d786da58
Roumen Pavlov <roumen.pavlov@fieldid.com>
2010-04-27 18:49:34 +0000

    Added Product associations.

--- migration/model/product_status.rb
+++ migration/model/product_status.rb
@@ -1,5 +1,8 @@
+require "product"
+
 class ProductStatus < ActiveRecord::Base
   set_table_name :productstatus
   set_primary_key :uniqueid
-  
+  belongs_to :product,   :foreign_key => 'productstatus_uniqueid',     :class_name => 'Product'
 end
+
