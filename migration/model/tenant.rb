f8ca16f3a9f60b4389b8907660b6bf61f5fd1736
Roumen Pavlov <roumen.pavlov@fieldid.com>
2010-04-27 18:52:02 +0000

    Added Order association.

--- migration/model/tenant.rb
+++ migration/model/tenant.rb
@@ -1,8 +1,8 @@
 require "base_org"
-
+require "order"
 class Tenant < ActiveRecord::Base
   set_table_name :tenants
-  
+   has_many  :orders,          :foreign_key => 'tenant_id',           :class_name => 'Order'
   
   def displayString
     "#{name} (#{id.to_s})"
