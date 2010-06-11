eefc3f18855d5b1b5580af38528078912b6a05a5
Travis Choi <tchoi@fieldid.com>
2010-06-11 15:31:11 +0000

    new ProductServiceDTOBuilder class for unit test code

---
+++ rfidEJB/src/com/n4systems/model/builders/ProductServiceDTOBuilder.java
@@ -0,0 +1,32 @@
+package com.n4systems.model.builders;
+
+import com.n4systems.webservice.dto.ProductServiceDTO;
+
+public class ProductServiceDTOBuilder extends BaseBuilder<ProductServiceDTO> {
+
+	private final long assignedUserId;
+	
+	@Override
+	public ProductServiceDTO build() {
+		ProductServiceDTO productServiceDTO = new ProductServiceDTO();
+		productServiceDTO.setId(id);
+		productServiceDTO.setAssignedUserId(assignedUserId);
+		
+		return productServiceDTO;
+	}
+	
+	public static ProductServiceDTOBuilder aProductServiceDTO() {
+		return new ProductServiceDTOBuilder(1L);
+	}
+	
+	private ProductServiceDTOBuilder(long assignedUserId) {
+		super();
+		this.assignedUserId = assignedUserId;
+	}
+	
+	public ProductServiceDTOBuilder withAssignedUserId(long assisgnedUserId) {
+		return new ProductServiceDTOBuilder(assisgnedUserId);
+	}
+	
+
+}
