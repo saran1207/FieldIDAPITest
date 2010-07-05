c7c37eacadd27dfa417453331bbc4247af45961b
Mark Frederiksen <mark.frederiksen@fieldid.com>
2010-07-05 14:58:42 +0000

    WEB-1717
    Improved formatting of Location and PredefinedLocation

--- rfidEJB/src/com/n4systems/model/location/Location.java
+++ rfidEJB/src/com/n4systems/model/location/Location.java
@@ -8,61 +8,49 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 
 @Embeddable
 public class Location {
-	
+
 	private static final PredefinedLocation noPredefinedLocation = null;
-	
+
 	public static Location onlyFreeformLocation(String freeformLocation) {
 		return new Location(noPredefinedLocation, freeformLocation);
 	}
-	
-	
 
 	@ManyToOne
 	private PredefinedLocation predefinedLocation;
-	
-	@Column(name="location")
+
+	@Column(name = "location")
 	private String freeformLocation;
-	
+
 	public Location() {
 		this(null, null);
 	}
-	
-	
+
 	public Location(PredefinedLocation predefinedLocation, String freeformLocation) {
 		super();
 		this.predefinedLocation = predefinedLocation;
 		this.freeformLocation = freeformLocation;
 	}
 
-
 	public PredefinedLocation getPredefinedLocation() {
 		return predefinedLocation;
 	}
 
-
 	public String getFreeformLocation() {
 		return freeformLocation;
 	}
-
-	
-	
 
 	@Override
 	public String toString() {
 		return ToStringBuilder.reflectionToString(this);
 	}
 
-
 	public Location createForAdjustedFreeformLocation(String freeformLocation) {
 		return new Location(predefinedLocation, freeformLocation);
 	}
 
-
-	public Location createForAdjustedPredefinedLocation(PredefinedLocation predefinedLocation) {
+	public Location createForAdjustedPredefinedLocation(
+			PredefinedLocation predefinedLocation) {
 		return new Location(predefinedLocation, freeformLocation);
 	}
-	
-	
-	
-		
+
 }
--- rfidEJB/src/com/n4systems/model/location/PredefinedLocation.java
+++ rfidEJB/src/com/n4systems/model/location/PredefinedLocation.java
@@ -8,37 +8,33 @@ import com.n4systems.model.Tenant;
 import com.n4systems.model.api.NamedEntity;
 import com.n4systems.model.parents.EntityWithTenant;
 
-@Entity(name="predefinedlocations")
+@SuppressWarnings("serial")
+@Entity(name = "predefinedlocations")
 public class PredefinedLocation extends EntityWithTenant implements NamedEntity {
 
-	@Column(nullable=false, length=255)
+	@Column(nullable = false, length = 255)
 	private String name;
 
 	@ManyToOne
 	private PredefinedLocation parent;
-	
-	
+
 	public PredefinedLocation() {
 		super();
 	}
 
-	
 	public PredefinedLocation(Tenant tenant, PredefinedLocation parent) {
 		super(tenant);
 		this.parent = parent;
 	}
 
-	
 	public void setName(String name) {
 		this.name = name;
 	}
-	
 
 	public String getName() {
 		return name;
 	}
 
-	
 	public PredefinedLocation getParent() {
 		return parent;
 	}
