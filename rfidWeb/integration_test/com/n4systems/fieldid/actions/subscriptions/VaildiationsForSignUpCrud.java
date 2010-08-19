c6e665b1230973596515765122ef342a32121f02
Neil Curzon <neil.curzon@fieldid.com>
2010-08-19 18:54:25 +0000

    Updating integration test for easymock upgrade

--- rfidWeb/integration_test/com/n4systems/fieldid/actions/subscriptions/VaildiationsForSignUpCrud.java
+++ rfidWeb/integration_test/com/n4systems/fieldid/actions/subscriptions/VaildiationsForSignUpCrud.java
@@ -1,6 +1,6 @@
 package com.n4systems.fieldid.actions.subscriptions;
 
-import static org.easymock.classextension.EasyMock.*;
+import static org.easymock.EasyMock.*;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
