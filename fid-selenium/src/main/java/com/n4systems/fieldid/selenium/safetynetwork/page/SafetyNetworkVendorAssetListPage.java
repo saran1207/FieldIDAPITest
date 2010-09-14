7216a89e7745a0d3c2ec08522b6de93d7d05b2e3
Diana Lai <diana.lai@fieldid.com>
2010-09-14 19:27:21 +0000

    WEB-1757 Added selenium tests for search

---
+++ fid-selenium/src/main/java/com/n4systems/fieldid/selenium/safetynetwork/page/SafetyNetworkVendorAssetListPage.java
@@ -0,0 +1,39 @@
+package com.n4systems.fieldid.selenium.safetynetwork.page;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.n4systems.fieldid.selenium.pages.FieldIDPage;
+import com.n4systems.model.Product;
+import com.thoughtworks.selenium.Selenium;
+
+public class SafetyNetworkVendorAssetListPage extends FieldIDPage {
+
+	public SafetyNetworkVendorAssetListPage(Selenium selenium) {
+		super(selenium);
+	}
+
+	public SafetyNetworkTraceabilityPage clickAsset(String serialNumber){
+		selenium.click("//a[.='" + serialNumber + "']");
+		return new SafetyNetworkTraceabilityPage(selenium);
+	}
+	
+	public boolean hasAssetList() {
+		return selenium.isElementPresent("//table[@id='productTable']");
+	}
+	
+	public List<Product> getAssetList() {
+		List<Product> results = new ArrayList<Product>();
+		
+		List<String> serialNumbers = collectTableValuesUnderCellForCurrentPage(2, 1, "a");
+		
+		for(String serialNumber: serialNumbers) {
+			Product product = new Product();
+			product.setSerialNumber(serialNumber);
+			results.add(product);
+		}
+		
+		return results;
+	}
+	
+}
