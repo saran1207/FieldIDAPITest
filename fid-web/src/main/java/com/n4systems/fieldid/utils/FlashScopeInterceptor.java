379e3ebe719aabc004a20e3d69a3f33f7cad881d
Neil Curzon <neil.curzon@fieldid.com>
2010-09-29 20:20:18 +0000

    Fixing issue with flash scope messages not display for actions that do redirects

--- fid-web/src/main/java/com/n4systems/fieldid/utils/FlashScopeInterceptor.java
+++ fid-web/src/main/java/com/n4systems/fieldid/utils/FlashScopeInterceptor.java
@@ -5,6 +5,7 @@ import org.apache.struts2.StrutsStatics;
 
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+import org.apache.struts2.dispatcher.ServletRedirectResult;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -18,7 +19,7 @@ public class FlashScopeInterceptor extends AbstractInterceptor implements Struts
         // for GET requests, we don't get two separate action invocations. This means that the flash scope messages
         // never get cleared up and we were actually displaying them on the next 2 pages. So we verify that this is
         // a post request. If not, we don't throw the flash scope messages into the session, because they will be available already for display
-        boolean storeSessionFlashMessages = "POST".equals(request.getMethod());
+        boolean requestIsPost = "POST".equals(request.getMethod());
 
         ActionInvocationWrapper action = new ActionInvocationWrapper(invocation);
         FlashScopeMarshaller marshaller = new FlashScopeMarshaller(action.getAction(), action.getSession().getHttpSession());
@@ -29,7 +30,7 @@ public class FlashScopeInterceptor extends AbstractInterceptor implements Struts
 
         marshaller.applyStoredFlashMessage();
 
-        if (storeSessionFlashMessages)
+        if (requestIsPost || invocation.getResult() instanceof ServletRedirectResult)
             marshaller.moveCurrentRequestFlashMessagesToFlashScope();
 
         return actionResponse;
