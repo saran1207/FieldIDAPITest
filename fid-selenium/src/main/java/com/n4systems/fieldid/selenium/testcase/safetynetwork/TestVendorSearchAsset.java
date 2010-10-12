097b5c9d026c98a43ba3f2b65370471efcf828ee
Roumen Pavlov <roumen.pavlov@fieldid.com>
2010-10-12 16:50:49 +0000

    Added another testcase for smart search.

--- fid-selenium/src/main/java/com/n4systems/fieldid/selenium/testcase/safetynetwork/TestVendorSearchAsset.java
+++ fid-selenium/src/main/java/com/n4systems/fieldid/selenium/testcase/safetynetwork/TestVendorSearchAsset.java
@@ -11,26 +11,46 @@ import com.n4systems.fieldid.selenium.safetynetwork.page.VendorConnectionProfile
 
 public class TestVendorSearchAsset extends PageNavigatingTestCase<VendorConnectionProfilePage> {
 
+	private static String PREASSIGNED_ASSET = "seaFitAsset";
+	private static String NON_PUBLISHED_ASSET = "nonPublishedAsset";
+	private static String NON_PREASSIGNED_ASSET = "nonPreassignedAsset";
+
 	@Override
 	protected VendorConnectionProfilePage navigateToPage() {
-        return startAsCompany("seafit").login().clickSafetyNetworkLink().selectVendorConnection("HALO");
-    }
-	
+		return startAsCompany("seafit").login().clickSafetyNetworkLink().selectVendorConnection("HALO");
+	}
+
 	@Test
 	public void test_search_with_no_result() throws Exception {
 		page.setAssetToSearchFor("g");
 		SafetyNetworkVendorAssetListPage result = page.clickSearchWithListResult();
 		assertFalse(result.hasAssetList());
 	}
-	
+
 	@Test
-	public void test_search_with_result() throws Exception {
-		page.setAssetToSearchFor("seaFitAsset");
+	public void test_simple_search_with_result() throws Exception {
+		page.setAssetToSearchFor(PREASSIGNED_ASSET);
 		AssetPage result = page.clickSearchWithSingleResult();
 
-		assertTrue(result.checkHeader("seaFitAsset"));
+		assertTrue(result.checkHeader(PREASSIGNED_ASSET));
 	}
-	
+
+	@Test
+	// i.e no customer set on this asset, only organization.
+	public void test_search_for_non_preassigned_asset() {
+		page.setAssetToSearchFor(NON_PREASSIGNED_ASSET);
+		AssetPage result = page.clickSearchWithSingleResult();
+
+		assertTrue(result.checkHeader(NON_PREASSIGNED_ASSET));
+	}
+
+	@Test
+	public void test_search_for_non_published_preassigned_asset() {
+		page.setAssetToSearchFor(NON_PUBLISHED_ASSET);
+		AssetPage result = page.clickSearchWithSingleResult();
+		assertFalse("Unpublished asset found...", result.checkHeader(NON_PUBLISHED_ASSET));
+	}
+
 	@Test
 	public void test_display_preassigned_assets() throws Exception {
 		SafetyNetworkVendorAssetListPage list = page.clickViewPreassignedAssets();
