68302f9344c34a74d381fc265644d824ed68e63e
Neil Curzon <neil.curzon@fieldid.com>
2010-11-05 13:36:36 +0000

    WEB-1807 Correcting table renaming migration

--- migration/db/migrations/201011041612_change_tables_from_inspection_to_event.rb
+++ migration/db/migrations/201011041612_change_tables_from_inspection_to_event.rb
@@ -1,16 +1,16 @@
 class ChangeTablesFromInspectionToEvent < ActiveRecord::Migration
 
   def self.up
-    #rename_table(:inspections, :events)
-    #rename_table(:inspectionsmaster, :masterevents)
-    #rename_table(:inspectionssub, :subevents)
-    #rename_table(:inspectionbooks, :eventbooks)
-    #rename_table(:inspectiongroups, :eventgroups)
-    #rename_table(:inspectionschedules, :eventschedules)
-    #rename_table(:inspectiontypes, :eventtypes)
-    #rename_table(:inspectiontypegroups, :eventtypegroups)
-    #rename_table(:inspectionsmaster_inspectionssub, :masterevents_subevents)
-    #rename_table(:associatedinspectiontypes, :associatedeventtypes)
+    rename_table(:inspections, :events)
+    rename_table(:inspectionsmaster, :masterevents)
+    rename_table(:inspectionssub, :subevents)
+    rename_table(:inspectionbooks, :eventbooks)
+    rename_table(:inspectiongroups, :eventgroups)
+    rename_table(:inspectionschedules, :eventschedules)
+    rename_table(:inspectiontypes, :eventtypes)
+    rename_table(:inspectiontypegroups, :eventtypegroups)
+    rename_table(:inspectionsmaster_inspectionssub, :masterevents_subevents)
+    rename_table(:associatedinspectiontypes, :associatedeventtypes)
 
     rename_table(:inspections_fileattachments, :events_fileattachments)
     rename_table(:inspections_infooptionmap, :events_infooptionmap)
@@ -23,6 +23,25 @@ class ChangeTablesFromInspectionToEvent < ActiveRecord::Migration
   end
 
   def self.down
+    rename_table(:events, :inspections)
+    rename_table(:masterevents, :inspectionsmaster)
+    rename_table(:subevents, :inspectionssub)
+    rename_table(:eventbooks, :inspectionbooks)
+    rename_table(:eventgroups, :inspectiongroups)
+    rename_table(:eventschedules, :inspectionschedules)
+    rename_table(:eventtypes, :inspectiontypes)
+    rename_table(:eventtypegroups, :inspectiontypegroups)
+    rename_table(:masterevents_subevents, :inspectionsmaster_inspectionssub)
+    rename_table(:associatedeventtypes, :associatedinspectiontypes)
+
+    rename_table(:events_fileattachments, :inspections_fileattachments)
+    rename_table(:events_infooptionmap, :inspections_infooptionmap)
+    rename_table(:eventtypes_criteriasections, :inspectiontypes_criteriasections)
+    rename_table(:eventtypes_infofieldnames, :inspectiontypes_infofieldnames)
+    rename_table(:eventtypes_supportedprooftests, :inspectiontypes_supportedprooftests)
+
+    rename_table(:notificationsettings_eventtypes, :notificationsettings_inspectiontypes)
+    rename_table(:catalogs_eventtypes, :catalogs_inspectiontypes)
   end
 
 end
