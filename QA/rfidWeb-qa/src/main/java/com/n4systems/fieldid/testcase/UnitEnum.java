18d61519a6cb44e14abfd6fc53f44c0a7744b13c
Neil Curzon <neil.curzon@fieldid.com>
2010-11-08 18:39:06 +0000

    Removing obsolete QA tree

--- QA/rfidWeb-qa/src/main/java/com/n4systems/fieldid/testcase/UnitEnum.java
+++
@@ -1,5 +0,0 @@
-package com.n4systems.fieldid.testcase;
-
-public enum UnitEnum {
-	undefined, Centimeter, Feet, Inches, Kilograms, kiloNewtons, Meters, Millimetres, Pounds, Ton, Tonne
-}
--- QA/rfidWeb-selenium/selenium-server.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-taskkill /f /t /im cmd.exe /fi "WINDOWTITLE eq selenium-server"
-SET WORKSPACE=C:\workspace\trunk\rfidWeb-selenium
-c:
-cd %WORKSPACE%\lib
-javaw -jar selenium-server.jar -firefoxProfileTemplate %WORKSPACE%\firefox
