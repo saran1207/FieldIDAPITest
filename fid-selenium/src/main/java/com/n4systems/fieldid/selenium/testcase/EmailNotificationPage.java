9f416cf4306b9492fd0bf209e910d92f9aa74406
Roumen Pavlov <roumen.pavlov@fieldid.com>
2011-02-08 14:34:28 +0000

    Some xpath changes.

--- fid-selenium/src/main/java/com/n4systems/fieldid/selenium/testcase/EmailNotificationPage.java
+++ fid-selenium/src/main/java/com/n4systems/fieldid/selenium/testcase/EmailNotificationPage.java
@@ -15,25 +15,25 @@ public class EmailNotificationPage extends FieldIDPage {
 	}
 
 	private boolean checkOnEmailNotificationsPage() {
-		return selenium.isElementPresent("//input[@id='notificationSettingUpdate_view_name']");
+		return selenium.isElementPresent("//input[@id='notificationSettingCreate_view_name']");
 	}
 	
 	public void setName(String name){
-		selenium.type("//input[@id='notificationSettingUpdate_view_name']", name);
+		selenium.type("//input[@id='notificationSettingCreate_view_name']", name);
 	}
 	
 	public void setUpcomingSchedulesDates(String from, String to){
-		selenium.check("input[@id='includeUpcoming']");
+		selenium.check("//input[@id='includeUpcoming']");
 		selenium.select("//select[@id='periodStart']", from);
 		selenium.select("//select[@id='periodEnd']", to);
 	}
 	
 	public void checkOverdue(){
-		selenium.check("//input[@id='notificationSettingUpdate_view_includeOverdue']");
+		selenium.check("//input[@id='notificationSettingCreate_view_includeOverdue']");
 	}
 	
 	public void checkFailed(){
-		selenium.check("//input[@id='notificationSettingUpdate_view_includeFailed']");
+		selenium.check("//input[@id='notificationSettingCreate_view_includeFailed']");
 	}
 	
 	//Filters
@@ -52,6 +52,14 @@ public class EmailNotificationPage extends FieldIDPage {
 	
 	public void setEmail(String email){
 		
+	}
+	
+	public void setDeliveryOptions(String frequency, boolean sendIfNoEventsOrSchedulesFound){
+		
+	}
+	
+	public void clickSave(){
+		selenium.click("//input[@id='notificationSettingCreate_label_save']");
 	}
 
 }
