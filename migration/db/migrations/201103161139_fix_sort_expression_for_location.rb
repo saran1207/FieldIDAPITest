97079f7b17bda4a4938b0c9bd5d579ba2aceec01
Neil Curzon <neil.curzon@fieldid.com>
2011-03-16 15:47:11 +0000

    WEB-1271 Fixing issue where sorting by location in jobs was broken.

--- migration/db/migrations/201103161139_fix_sort_expression_for_location.rb
+++ migration/db/migrations/201103161139_fix_sort_expression_for_location.rb
@@ -1,7 +1,7 @@
 class FixSortExpressionForLocation < ActiveRecord::Migration
 
   def self.up
-    execute ("update system_column_mappings set sort_expression = 'advancedLocation.freeformLocation' where path_expression = 'advancedLocation.fullName'")
+    execute ("update column_mappings set sort_expression = 'advancedLocation.freeformLocation' where path_expression = 'advancedLocation.fullName'")
   end
 
   def self.down
