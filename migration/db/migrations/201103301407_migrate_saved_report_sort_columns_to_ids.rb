ea2ff046d511a1319b9979cf47c02c53bee03628
Neil Curzon <neil.curzon@fieldid.com>
2011-03-31 20:03:40 +0000

    Fixing sort migration to use a default sort column when a bad column is in place in a saved report.

--- migration/db/migrations/201103301407_migrate_saved_report_sort_columns_to_ids.rb
+++ migration/db/migrations/201103301407_migrate_saved_report_sort_columns_to_ids.rb
@@ -1,3 +1,4 @@
+require 'column_mapping'
 require 'system_column_mapping'
 require 'column_mapping_group'
 require 'saved_report'
@@ -8,6 +9,8 @@ class MigrateSavedReportSortColumnsToIds < ActiveRecord::Migration
     add_column(:savedreports, :sort_column_id, :integer)
     add_foreign_key(:savedreports, :column_mappings, :source_column => :sort_column_id, :foreign_column => :id)
 
+    default_sort_column = ColumnMapping.find(:first, :conditions => {:name => 'event_search_date_performed'})
+
     event_column_groups = ColumnMappingGroup.find(:all, :conditions => { :report_type => "EVENT" }).collect { |e| e.id }
 
     saved_reports = SavedReport.find(:all)
@@ -15,10 +18,11 @@ class MigrateSavedReportSortColumnsToIds < ActiveRecord::Migration
     saved_reports.each do |report|
       sort_expr = report.sortcolumn
       mapping = ColumnMapping.find_by_sql("select c.* from system_column_mappings s, column_mappings c where s.column_id = c.id and c.group_id in (#{event_column_groups.join(',')}) and (c.sort_expression='#{sort_expr}' or (c.sort_expression is null and c.path_expression='#{sort_expr}'))").first
-      if mapping.nil?
-        raise "Could not find a way to migrate sort column for saved report #{report.id}: #{report.sortcolumn}"
+      if mapping.nil
+        report.sort_column_id = default_sort_column.id
+      else
+        report.sort_column_id = mapping.id
       end
-      report.sort_column_id = mapping.id
       report.save
     end
   end
