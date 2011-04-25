8349abad968321c34af9a2b42ceb01718323fa3d
Diana Lai <diana.lai@fieldid.com>
2011-04-25 15:47:06 +0000

    WEB-2115 Fixed incorrect filename for template

--- fid-web/src/main/java/com/n4systems/fieldid/actions/customers/ExampleCustomerExportAction.java
+++ fid-web/src/main/java/com/n4systems/fieldid/actions/customers/ExampleCustomerExportAction.java
@@ -74,7 +74,7 @@ public class ExampleCustomerExportAction extends AbstractDownloadAction {
 
 	@Override
 	public String getFileName() {
-		return ContentType.EXCEL.prepareFileName(getText("label.export_file"));
+		return ContentType.EXCEL.prepareFileName(getText("label.export_file.customer"));
 	}
 
 	@Override
