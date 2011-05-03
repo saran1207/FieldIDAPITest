756301036177cd3b571736d18cbea837d31d4c8c
Mark Frederiksen <mark.frederiksen@fieldid.com>
2011-05-03 21:12:42 +0000

    fixed table name in the clean notification script

--- migration/db/cleanScripts/200906081208_clean_notifications.rb
+++ migration/db/cleanScripts/200906081208_clean_notifications.rb
@@ -1,8 +1,8 @@
 class CleanNotifications < ActiveRecord::Migration
   def self.up
-	  execute "delete from notificationsettings_inspectiontypes"
+	  execute "delete from notificationsettings_eventtypes"
 	  execute "delete from notificationsettings_addresses"
-	  execute "delete from notificationsettings_producttypes"
+	  execute "delete from notificationsettings_assettypes"
 	  execute "delete from notificationsettings"
   end
   
