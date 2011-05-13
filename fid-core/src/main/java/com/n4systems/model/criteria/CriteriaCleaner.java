7a4d261513766aab3af3d62fa66525dd09cea66f
Neil Curzon <neil.curzon@fieldid.com>
2011-05-13 15:58:40 +0000

    WEB-2150
    Event type copying no longer removes select criteria options from the old event type.

--- fid-core/src/main/java/com/n4systems/model/criteria/CriteriaCleaner.java
+++ fid-core/src/main/java/com/n4systems/model/criteria/CriteriaCleaner.java
@@ -2,7 +2,9 @@ package com.n4systems.model.criteria;
 
 import java.util.ArrayList;
 
+import com.n4systems.model.ComboBoxCriteria;
 import com.n4systems.model.Criteria;
+import com.n4systems.model.SelectCriteria;
 import com.n4systems.model.Tenant;
 import com.n4systems.model.api.EntityWithTenantCleaner;
 import com.n4systems.util.ListHelper;
@@ -19,9 +21,26 @@ public class CriteriaCleaner extends EntityWithTenantCleaner<Criteria> {
 		
 		cleanRecommendations(criteria);
 		cleanDeficiencies(criteria);
+
+        if (criteria instanceof SelectCriteria) {
+            copySelectOptions((SelectCriteria) criteria);
+        }
+        if (criteria instanceof ComboBoxCriteria) {
+            copyComboOptions((ComboBoxCriteria) criteria);
+        }
 	}
 
-	private void cleanRecommendations(Criteria criteria) {
+    private void copyComboOptions(ComboBoxCriteria criteria) {
+        // Required so the options aren't moved into the new criteria
+        criteria.setOptions(ListHelper.copy(criteria.getOptions(), new ArrayList<String>()));
+    }
+
+    private void copySelectOptions(SelectCriteria criteria) {
+        // Required so the options aren't moved into the new criteria
+        criteria.setOptions(ListHelper.copy(criteria.getOptions(), new ArrayList<String>()));
+    }
+
+    private void cleanRecommendations(Criteria criteria) {
 		// Note: this is actually required so that hibernate does not move the old list to the new entity
 		// when it's being copied. -mf
 		criteria.setRecommendations(ListHelper.copy(criteria.getRecommendations(), new ArrayList<String>()));
