1dd194b666052dbd4b532defacc55cfec7105a29
Neil Curzon <neil.curzon@fieldid.com>
2011-05-25 14:14:57 +0000

    Fixing a possible race condition in selenium page fixture.

--- fid-selenium/src/main/java/com/n4systems/fieldid/selenium/pages/admin/AdminLoginPage.java
+++ fid-selenium/src/main/java/com/n4systems/fieldid/selenium/pages/admin/AdminLoginPage.java
@@ -3,10 +3,14 @@ package com.n4systems.fieldid.selenium.pages.admin;
 import com.n4systems.fieldid.selenium.pages.FieldIDPage;
 import com.thoughtworks.selenium.Selenium;
 
+import static org.junit.Assert.assertTrue;
+
 public class AdminLoginPage extends FieldIDPage {
 
     public AdminLoginPage(Selenium selenium) {
         super(selenium);
+        assertTrue("Expected to be on admin login page",  selenium.isElementPresent("//div[@id='header']//h1[.='Field ID Admin Console']"));
+        waitForElementToBePresent("//form[@id='loginForm']//input[@id='loginForm_username']", DEFAULT_TIMEOUT);
     }
 
     public AdminOrgsListPage login() {
