a09ec204d928697a1b5709db8ed74375bf28bed8
Diana Lai <diana.lai@fieldid.com>
2011-06-06 19:12:40 +0000

    Updating system pass for selenium tests

--- fid-selenium/src/main/java/com/n4systems/fieldid/selenium/pages/admin/AdminLoginPage.java
+++ fid-selenium/src/main/java/com/n4systems/fieldid/selenium/pages/admin/AdminLoginPage.java
@@ -14,7 +14,7 @@ public class AdminLoginPage extends FieldIDPage {
     }
 
     public AdminOrgsListPage login() {
-        return login("n4systems", "f0rM@t!!!");
+        return login("n4systems", "f0rM@t!!");
     }
 
     public AdminOrgsListPage login(String userName, String password) {
