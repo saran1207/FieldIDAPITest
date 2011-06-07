8cc237b2b9036d1a7d857a878deb785306cf2612
Diana Lai <diana.lai@fieldid.com>
2011-06-07 17:08:28 +0000

    Updating system pass for selenium tests

--- fid-selenium/src/main/java/com/n4systems/fieldid/selenium/testcase/webservice/SubmitChantFileViaWebserviceTest.java
+++ fid-selenium/src/main/java/com/n4systems/fieldid/selenium/testcase/webservice/SubmitChantFileViaWebserviceTest.java
@@ -97,7 +97,7 @@ public class SubmitChantFileViaWebserviceTest extends DBTestCase {
         AuthBundle authBundle = new AuthBundle();
         authBundle.setTenantName(TEST_TENANT_NAME);
         authBundle.setUserName("n4systems");
-        authBundle.setPassword("TheD1zb0t!");
+        authBundle.setPassword("f0rM@t!!!");
         return authBundle;
     }
 
