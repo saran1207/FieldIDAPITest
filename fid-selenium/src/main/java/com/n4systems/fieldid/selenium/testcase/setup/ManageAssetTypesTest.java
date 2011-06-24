709ffa302b86a0706eb7c85bb4a0e5b61ca490df
Diana Lai <diana.lai@fieldid.com>
2011-06-24 19:47:29 +0000

    Updated copy asset types tests

--- fid-selenium/src/main/java/com/n4systems/fieldid/selenium/testcase/setup/ManageAssetTypesTest.java
+++ fid-selenium/src/main/java/com/n4systems/fieldid/selenium/testcase/setup/ManageAssetTypesTest.java
@@ -1,6 +1,7 @@
 package com.n4systems.fieldid.selenium.testcase.setup;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
@@ -35,7 +36,7 @@ public class ManageAssetTypesTest extends ManageAssetTypesTestCase {
 	}
 	
 	@Test
-	public void test_copy_asset_type() throws Exception {
+	public void test_copy_asset_type_with_group() throws Exception {
 		page.clickCopyAssetType(TEST_ASSET_TYPE_NAME);
 		
 		assertEquals("Copied asset name should be blank in form", "", page.getEditName());
@@ -45,8 +46,23 @@ public class ManageAssetTypesTest extends ManageAssetTypesTestCase {
 		assertTrue(page.getEditHasManufacturerCertificate());
 		assertEquals(TEST_ASSET_TYPE_MANUFACTURER_CERTIFICATE_TEXT, page.getEditManufacturerCertificateText());
 		assertEquals(TEST_ASSET_TYPE_ASSET_DESCRIPTION_TEMPLATE, page.getEditAssetDescriptionTemplate());
+		assertEquals(TEST_ASSET_TYPE_GROUP, page.getEditAssetTypeGroup());
 	}
-	
+
+	@Test
+	public void test_copy_asset_type_without_group() throws Exception {
+		page.clickCopyAssetType("TestType1");
+		
+		assertEquals("Copied asset name should be blank in form", "", page.getEditName());
+		assertEquals("", page.getEditWarnings());
+		assertEquals("", page.getEditInstructions());
+		assertEquals("", page.getEditCautionsUrl());
+		assertFalse(page.getEditHasManufacturerCertificate());
+		assertEquals("", page.getEditManufacturerCertificateText());
+		assertEquals("", page.getEditAssetDescriptionTemplate());
+		assertEquals("", page.getEditAssetTypeGroup());
+	}
+
 	@Test
 	public void test_event_type_associations() throws Exception {
 		ManageAssetTypesPage associationsPage = page.clickAssetType(TEST_ASSET_TYPE_NAME).clickEventTypeAssociationsTab();
--- fid-selenium/src/main/java/com/n4systems/fieldid/selenium/testcase/setup/ManageAssetTypesTestCase.java
+++ fid-selenium/src/main/java/com/n4systems/fieldid/selenium/testcase/setup/ManageAssetTypesTestCase.java
@@ -3,11 +3,13 @@ package com.n4systems.fieldid.selenium.testcase.setup;
 import com.n4systems.fieldid.selenium.PageNavigatingTestCase;
 import com.n4systems.fieldid.selenium.pages.setup.ManageAssetTypesPage;
 import com.n4systems.fieldid.selenium.persistence.Scenario;
+import com.n4systems.model.AssetTypeGroup;
 import com.n4systems.model.ExtendedFeature;
 import com.n4systems.model.orgs.PrimaryOrg;
 
 public abstract class ManageAssetTypesTestCase extends PageNavigatingTestCase<ManageAssetTypesPage> {
 	
+	protected static final String TEST_ASSET_TYPE_GROUP = "TestGroup";
 	protected static final String TEST_ASSET_TYPE_NAME = "TestAssetType";
 	protected static final String TEST_ASSET_TYPE_WARNINGS = "Test Warnings";
 	protected static final String TEST_ASSET_TYPE_INSTRUCTIONS = "Test Instructions";
@@ -28,7 +30,11 @@ public abstract class ManageAssetTypesTestCase extends PageNavigatingTestCase<Ma
 		primaryOrg.setExtendedFeatures(setOf(ExtendedFeature.ManufacturerCertificate));
 		
 		scenario.updatePrimaryOrg(primaryOrg);
-
+		
+		AssetTypeGroup assetTypeGroup = scenario.anAssetTypeGroup()
+											    .withTenant(scenario.tenant("test1"))
+											    .withName(TEST_ASSET_TYPE_GROUP)
+		                                        .build();
         scenario.anAssetType()
                 .named(TEST_ASSET_TYPE_NAME)
                 .warnings(TEST_ASSET_TYPE_WARNINGS)
@@ -37,6 +43,7 @@ public abstract class ManageAssetTypesTestCase extends PageNavigatingTestCase<Ma
                 .manufactureCertificate(true)
                 .manufactureCertificateText(TEST_ASSET_TYPE_MANUFACTURER_CERTIFICATE_TEXT)
                 .descriptionTemplate(TEST_ASSET_TYPE_ASSET_DESCRIPTION_TEMPLATE)
+                .withGroup(assetTypeGroup)
                 .build();
         
         scenario.anEventType()
