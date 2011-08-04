792191793f350cc5601a2f95d0054c13f16cbb84
Dereck Dick <dereck.dick@fieldid.com>
2011-08-04 21:56:43 +0000

    WEB-2239, WEB-2250 - change to display address instead of lat/lng.

--- fid-web/src/main/webapp/javascript/googleMaps.js
+++ fid-web/src/main/webapp/javascript/googleMaps.js
@@ -2,6 +2,7 @@
 var googleMap = (function() {
 	 
 	var name = "FieldIdGoogleMaps";
+	
  
 	var initialize = function(id, latitude, longitude) {
 		var loc = new google.maps.LatLng(latitude, longitude);	
@@ -11,16 +12,28 @@ var googleMap = (function() {
 		};
 		var map = new google.maps.Map(document.getElementById(id), myOptions);  
 		map.setCenter(loc);
-		var coordinates = '(' + latitude + ' , ' + longitude + ')';
-		
+		var coordinates = '(' + latitude + ' , ' + longitude + ')';  /*default value just in case geocoder doesnt work */
+					
 		var marker = new google.maps.Marker({
-		    position: loc,
-		    map: map,
-		    title : coordinates
+			position: loc,
+			map: map,
+			title: coordinates
+		});	
+		
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode( {'latLng': loc}, function(results, status) {
+			if (status== google.maps.GeocoderStatus.OK) {				        
+				marker.setTitle(formatAddressString(results[0]));				      
+		 	}
 		});
+				
 		return map;		
 	};
- 
+
+	function formatAddressString(address) { 
+		return address.address_components[0].short_name + ' '  + address.address_components[1].short_name + ', ' + address.address_components[2].short_name;   
+	}
+	
 	return {
 		initialize: initialize
 	};
