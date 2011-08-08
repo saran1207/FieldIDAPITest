cf5e215795a2ee8305294fb65b123ad6641f8a39
Dereck Dick <dereck.dick@fieldid.com>
2011-08-08 15:14:38 +0000

    WEB-2244 - update system settings page.  still needs styling work.

---
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/setup/IdentifierOverridesPage.java
@@ -0,0 +1,77 @@
+package com.n4systems.fieldid.wicket.pages.setup;
+
+import java.util.List;
+
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import com.n4systems.fieldid.service.tenant.SystemSettingsService;
+import com.n4systems.fieldid.wicket.FieldIDSession;
+import com.n4systems.fieldid.wicket.components.FlatLabel;
+import com.n4systems.fieldid.wicket.components.feedback.FIDFeedbackPanel;
+import com.n4systems.fieldid.wicket.components.navigation.NavigationBar;
+import com.n4systems.fieldid.wicket.model.AssetTypesForTenantModel;
+import com.n4systems.fieldid.wicket.model.FIDLabelModel;
+import com.n4systems.fieldid.wicket.pages.FieldIDLoggedInPage;
+import com.n4systems.model.AssetType;
+
+@SuppressWarnings("serial")
+public class IdentifierOverridesPage extends FieldIDLoggedInPage {
+
+    @SpringBean
+    private SystemSettingsService systemSettingsService;
+    
+	public IdentifierOverridesPage(PageParameters params) {
+        super(params);
+        add(new IdentifierOverridesForm("identifierOverridesForm", new AssetTypesForTenantModel()));        
+    }
+
+    class IdentifierOverridesForm extends Form<List<AssetType>> { 
+
+    	public IdentifierOverridesForm(String id, IModel<List<AssetType>> model) { 
+    		super(id, model);
+    		
+            add(new FIDFeedbackPanel("feedbackPanel"));
+            
+		    add(new ListView<AssetType>("assetTypeOverrides", model) {
+		        @Override
+		        protected void populateItem(ListItem<AssetType> item) {
+		            item.add(new CheckBox("override", new PropertyModel<Boolean>(item.getModel(), "identifierOverridden")));
+		            item.add(new FlatLabel("assetTypeName", new PropertyModel<Boolean>(item.getModel(), "name")));
+		            item.add(new TextField<String>("identifierLabel", new PropertyModel<String>(item.getModel(), "identifierLabel")));
+		            item.add(new TextField<String>("identifierFormat", new PropertyModel<String>(item.getModel(), "identifierFormat")));
+		        }
+		    });
+            add(new Button("submitButton"));
+            add(new BookmarkablePageLink<Void>("cancelLink", SystemSettingsPage.class));		    
+    	}
+    	
+    	@Override
+    	protected void onSubmit() {
+        	systemSettingsService.saveSystemSettings(getModelObject());
+        	FieldIDSession.get().info(new FIDLabelModel("message.system_settings_updated").getObject());
+        	setResponsePage(SystemSettingsPage.class);
+ 		}
+    }
+
+    @Override
+    protected void addNavBar(String navBarId) {
+        add(new NavigationBar(navBarId));
+    }
+
+    @Override
+    protected Label createTitleLabel(String labelId) {
+        return new Label(labelId, new FIDLabelModel("title.identifier_overrides"));
+    }	
+	
+}
--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/setup/SystemSettingsPage.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/setup/SystemSettingsPage.java
@@ -1,17 +1,8 @@
 package com.n4systems.fieldid.wicket.pages.setup;
 
-import com.n4systems.fieldid.service.tenant.SystemSettingsService;
-import com.n4systems.fieldid.wicket.FieldIDSession;
-import com.n4systems.fieldid.wicket.components.FlatLabel;
-import com.n4systems.fieldid.wicket.components.feedback.FIDFeedbackPanel;
-import com.n4systems.fieldid.wicket.components.navigation.NavigationBar;
-import com.n4systems.fieldid.wicket.components.renderer.DateFormatSampleChoiceRenderer;
-import com.n4systems.fieldid.wicket.model.AssetTypesForTenantModel;
-import com.n4systems.fieldid.wicket.model.FIDLabelModel;
-import com.n4systems.fieldid.wicket.model.navigation.NavigationItemBuilder;
-import com.n4systems.fieldid.wicket.pages.FieldIDLoggedInPage;
-import com.n4systems.model.AssetType;
-import com.n4systems.model.tenant.SystemSettings;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
@@ -19,16 +10,22 @@ import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.resource.ContextRelativeResource;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.n4systems.fieldid.service.tenant.SystemSettingsService;
+import com.n4systems.fieldid.wicket.FieldIDSession;
+import com.n4systems.fieldid.wicket.components.FlatLabel;
+import com.n4systems.fieldid.wicket.components.feedback.FIDFeedbackPanel;
+import com.n4systems.fieldid.wicket.components.navigation.NavigationBar;
+import com.n4systems.fieldid.wicket.components.renderer.DateFormatSampleChoiceRenderer;
+import com.n4systems.fieldid.wicket.model.FIDLabelModel;
+import com.n4systems.fieldid.wicket.model.navigation.NavigationItemBuilder;
+import com.n4systems.fieldid.wicket.pages.FieldIDLoggedInPage;
+import com.n4systems.model.tenant.SystemSettings;
 
 public class SystemSettingsPage extends FieldIDLoggedInPage {
 
@@ -60,8 +57,6 @@ public class SystemSettingsPage extends FieldIDLoggedInPage {
 
     class SystemSettingsForm extends Form<SystemSettings> {
 
-        private IModel<List<AssetType>> assetTypesModel = new AssetTypesForTenantModel();
-
         public SystemSettingsForm(String id, SystemSettings data) {
             super(id, new CompoundPropertyModel<SystemSettings>(data));
 
@@ -70,28 +65,22 @@ public class SystemSettingsPage extends FieldIDLoggedInPage {
             add(new CheckBox("assignedTo"));
             add(new CheckBox("proofTestIntegration"));
             add(new CheckBox("manufacturerCertificate"));
+            add(new Image("gpsLogo", new ContextRelativeResource("/images/gps-logo.png")));
             add(new CheckBox("gpsCapture"));
             add(new DropDownChoice<String>("dateFormat", getDateFormats(), new DateFormatSampleChoiceRenderer()).setNullValid(false));
-            add(new TextField<String>("identifierFormat"));
+            add(new TextField<String>("identifierFormat")); 
             add(new TextField<String>("identifierLabel"));
 
+            add(new BookmarkablePageLink<Void>("overrides", IdentifierOverridesPage.class));
+            
             add(new Button("submitButton"));
             add(new BookmarkablePageLink<Void>("cancelLink", SettingsPage.class));
 
-            add(new ListView<AssetType>("assetTypeOverrides", assetTypesModel) {
-                @Override
-                protected void populateItem(ListItem<AssetType> item) {
-                    item.add(new CheckBox("override", new PropertyModel<Boolean>(item.getModel(), "identifierOverridden")));
-                    item.add(new FlatLabel("assetTypeName", new PropertyModel<Boolean>(item.getModel(), "name")));
-                    item.add(new TextField<String>("identifierLabel", new PropertyModel<String>(item.getModel(), "identifierLabel")));
-                    item.add(new TextField<String>("identifierFormat", new PropertyModel<String>(item.getModel(), "identifierFormat")));
-                }
-            });
         }
 
         @Override
         protected void onSubmit() {
-            systemSettingsService.saveSystemSettings(getModelObject(), assetTypesModel.getObject());
+            systemSettingsService.saveSystemSettings(getModelObject());
             FieldIDSession.get().info(new FIDLabelModel("message.system_settings_updated").getObject());
             setResponsePage(SystemSettingsPage.class);
         }
