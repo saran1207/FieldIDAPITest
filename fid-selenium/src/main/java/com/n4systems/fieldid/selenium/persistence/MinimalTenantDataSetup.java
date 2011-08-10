0509e507e1bc4daec37f4b9d5fbd6799d2588228
Neil Curzon <neil.curzon@fieldid.com>
2011-08-10 15:56:36 +0000

    Correcting typo in selenium test tenant user name

--- fid-selenium/src/main/java/com/n4systems/fieldid/selenium/persistence/MinimalTenantDataSetup.java
+++ fid-selenium/src/main/java/com/n4systems/fieldid/selenium/persistence/MinimalTenantDataSetup.java
@@ -87,7 +87,7 @@ public class MinimalTenantDataSetup {
 		user.setRegistered(true);
 		user.setPermissions(Permissions.SYSTEM);
 		user.setUserType(UserType.SYSTEM);
-		user.setUserID("n4ystems");
+		user.setUserID("n4systems");
 		user.setHashPassword("6e900ce5b6400c175a30f9f75987c95c4161608a"); //f0rM@t!!
 		user.setEmailAddress("at@dot.com");
 		user.setFirstName("N4");
