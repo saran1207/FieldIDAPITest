5cf0a5fa5f7b714dcd1b859c41ff06be44d7a7be
Diana Lai <diana.lai@fieldid.com>
2011-08-17 20:41:55 +0000

    Fixed broken tests for schedules

--- fid-selenium/src/main/java/com/n4systems/fieldid/selenium/testcase/schedules/SchedulesSearchTest.java
+++ fid-selenium/src/main/java/com/n4systems/fieldid/selenium/testcase/schedules/SchedulesSearchTest.java
@@ -4,19 +4,57 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
 
 import com.n4systems.fieldid.selenium.PageNavigatingTestCase;
 import com.n4systems.fieldid.selenium.pages.SchedulesSearchPage;
+import com.n4systems.fieldid.selenium.persistence.Scenario;
+import com.n4systems.model.Asset;
+import com.n4systems.model.AssetType;
+import com.n4systems.model.AssociatedEventType;
+import com.n4systems.model.EventSchedule;
+import com.n4systems.model.EventType;
+import com.n4systems.model.EventTypeGroup;
 
 
 public class SchedulesSearchTest extends PageNavigatingTestCase<SchedulesSearchPage> {
 
+	private static String COMPANY = "test1";
+	private static final String TEST_EVENT_TYPE1 = "Event Type 1";
+	private static final String SERIAL_NUMBER = "11111111";
+
+	@Override
+	public void setupScenario(Scenario scenario) {
+
+		EventTypeGroup group = scenario.anEventTypeGroup()
+									   .forTenant(scenario.tenant(COMPANY))
+								       .withName("Test Event Type Group")
+								 	   .build();
+
+        EventType eventType1 = scenario.anEventType()
+                                      .named(TEST_EVENT_TYPE1)
+                                      .withGroup(group)
+                                      .build();
+        
+        AssetType assetType = scenario.assetType(COMPANY, TEST_ASSET_TYPE_1);
+                
+        scenario.save(new AssociatedEventType(eventType1, assetType));    
+        
+        Asset asset = scenario.anAsset()
+                              .withOwner(scenario.primaryOrgFor(COMPANY))
+                              .withIdentifier(SERIAL_NUMBER)
+                              .ofType(assetType)
+                              .build();      
+        
+        scenario.save(new EventSchedule(asset, eventType1, new Date()));
+     }
+	
 	@Override
 	protected SchedulesSearchPage navigateToPage() {
-		return start().login().clickSchedulesLink();
+		return startAsCompany("test1").login().clickSchedulesLink();
 	}
 
 	@Test
@@ -27,7 +65,7 @@ public class SchedulesSearchTest extends PageNavigatingTestCase<SchedulesSearchP
 	}
 	
 	private List<String> getDefaultColumnHeaders() {
-		return Arrays.asList("Scheduled Date", "Status", "Days Past Due", "Serial Number", 
-				"Reference Number", "Customer Name", "Event Type", "Last Event Date", "Asset Type", "");
+		return Arrays.asList("Scheduled Date", "Status", "Days Past Due", "ID Number", "Asset Type", "Customer Name", "Location", 
+				"Event Type", "Last Event Date", "");
 	}
 }
