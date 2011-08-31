93e5659ef3dba7877d16acedc2a6acc4301616e8
Neil Curzon <neil.curzon@fieldid.com>
2011-08-31 14:24:28 +0000

    WEB-2227 WEB-2321 Merging export to excel fix into trunk

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/reporting/action/LegacyReportMassActionLink.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/reporting/action/LegacyReportMassActionLink.java
@@ -3,11 +3,14 @@ package com.n4systems.fieldid.wicket.pages.reporting.action;
 import com.n4systems.fieldid.viewhelpers.EventSearchContainer;
 import com.n4systems.model.search.EventReportCriteriaModel;
 import com.n4systems.fieldid.wicket.util.LegacyReportCriteriaStorage;
+import com.n4systems.util.ConfigContext;
+import com.n4systems.util.ConfigEntry;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.protocol.http.WebRequest;
 import org.apache.wicket.request.target.basic.RedirectRequestTarget;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 public class LegacyReportMassActionLink extends Link {
@@ -23,10 +26,15 @@ public class LegacyReportMassActionLink extends Link {
 
     @Override
     public void onClick() {
-        HttpSession session = ((WebRequest) getRequest()).getHttpServletRequest().getSession();
+        HttpServletRequest httpServletRequest = ((WebRequest) getRequest()).getHttpServletRequest();
+        HttpSession session = httpServletRequest.getSession();
+
         EventSearchContainer searchContainer = new LegacyReportCriteriaStorage().storeCriteria(reportCriteriaModel.getObject(), session);
         String formattedUrl = String.format(url, searchContainer.getSearchId());
-        getRequestCycle().setRequestTarget(new RedirectRequestTarget(formattedUrl));
+
+        String destination = ConfigContext.getCurrentContext().getString(ConfigEntry.SYSTEM_PROTOCOL) + "://" + httpServletRequest.getServerName() + httpServletRequest.getContextPath() + formattedUrl;
+
+        getRequestCycle().setRequestTarget(new RedirectRequestTarget(destination));
     }
 
 }
