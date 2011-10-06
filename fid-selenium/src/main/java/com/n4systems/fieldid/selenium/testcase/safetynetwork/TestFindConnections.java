85d69a822f760d9a8cab0e5bdb7367dd35e4fd3a
Neil Curzon <neil.curzon@fieldid.com>
2011-10-06 18:50:00 +0000

    Updating finding connections test to not rely on existing data.

--- fid-selenium/src/main/java/com/n4systems/fieldid/selenium/testcase/safetynetwork/TestFindConnections.java
+++ fid-selenium/src/main/java/com/n4systems/fieldid/selenium/testcase/safetynetwork/TestFindConnections.java
@@ -4,6 +4,7 @@ import com.n4systems.fieldid.selenium.PageNavigatingTestCase;
 import com.n4systems.fieldid.selenium.datatypes.Organization;
 import com.n4systems.fieldid.selenium.pages.SafetyNetworkPage;
 import com.n4systems.fieldid.selenium.pages.safetynetwork.FindConnectionResultsPage;
+import com.n4systems.fieldid.selenium.persistence.Scenario;
 import org.junit.Test;
 
 import java.util.List;
@@ -13,22 +14,24 @@ import static org.junit.Assert.*;
 public class TestFindConnections extends PageNavigatingTestCase<SafetyNetworkPage> {
 
     @Override
+    public void setupScenario(Scenario scenario) {
+        scenario.primaryOrgFor("test2").setName("The Second Selenium Test Organization");
+        scenario.primaryOrgFor("test2").setWebSite("news.bbc.co.uk");
+    }
+
+    @Override
     protected SafetyNetworkPage navigateToPage() {
-        return startAsCompany("n4").login().clickSafetyNetworkLink();
+        return start().login().clickSafetyNetworkLink();
     }
     
     @Test
     public void testFindConnections() throws Exception {
-        FindConnectionResultsPage resultsPage = page.findConnections("crane ma");
+        FindConnectionResultsPage resultsPage = page.findConnections("The Second Selenium Test Organization");
         List<Organization> orgs = resultsPage.getOrganizationSearchResults();
 
-        assertEquals(2, orgs.size());
+        assertEquals(1, orgs.size());
 
-        Organization aacm = findOrgNamed("All American Crane Maintenance", orgs);
-        assertEquals("http://www.aacm.net/", aacm.getCompanyWebsite());
-
-        Organization craneMasters = findOrgNamed("Crane Masters", orgs);
-        assertNull(craneMasters.getCompanyWebsite());
+        assertEquals("http://news.bbc.co.uk", orgs.get(0).getCompanyWebsite());
     }
 
     @Test
@@ -36,42 +39,6 @@ public class TestFindConnections extends PageNavigatingTestCase<SafetyNetworkPag
         FindConnectionResultsPage resultsPage = page.findConnections("This will have no results");
         List<Organization> orgs = resultsPage.getOrganizationSearchResults();
         assertEquals(0, orgs.size());
-    }
-
-    @Test
-    public void testFindConnectionsMoreThanOnePage() throws Exception {
-        FindConnectionResultsPage resultsPage = page.findConnections("inc");
-
-        assertEquals(2, resultsPage.getNumberOfPages());
-
-        List<Organization> orgs = resultsPage.getOrganizationSearchResults();
-
-        assertEquals(18, orgs.size());
-
-        assertNotNull(findOrgNamed("Key Constructors Inc.", orgs));
-        assertNotNull(findOrgNamed("Peintre inc", orgs));
-    }
-
-    @Test
-    public void testFindConnectionsNoHttpInWebsiteAddress() throws Exception {
-        FindConnectionResultsPage resultsPage = page.findConnections("West Coast Wire Rope");
-        List<Organization> orgs = resultsPage.getOrganizationSearchResults();
-
-        assertEquals(1, orgs.size());
-        Organization wcwr = orgs.iterator().next();
-
-        assertEquals("http://www.wcwr.com", wcwr.getCompanyWebsite());
-        assertEquals("West Coast Wire Rope", wcwr.getName());
-    }
-
-
-    public Organization findOrgNamed(String name, List<Organization> orgList) {
-        for (Organization org : orgList) {
-            if (name.equals(org.getName())) {
-                return org;
-            }
-        }
-        return null;
     }
 
 }
