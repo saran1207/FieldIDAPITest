086df0896848213871838ff97b589ae8bc9c91b2
Diana Lai <diana.lai@fieldid.com>
2011-10-27 20:02:58 +0000

    WEB-2416 added event auto scheduling to importer

--- fid-core/src/main/java/com/n4systems/exporting/AssetImporter.java
+++ fid-core/src/main/java/com/n4systems/exporting/AssetImporter.java
@@ -1,25 +1,32 @@
 package com.n4systems.exporting;
 
+import java.util.List;
+
 import com.n4systems.api.conversion.ConversionException;
 import com.n4systems.api.conversion.asset.AssetToModelConverter;
 import com.n4systems.api.model.AssetView;
 import com.n4systems.api.model.ExternalModelView;
 import com.n4systems.api.validation.Validator;
 import com.n4systems.api.validation.validators.AssetViewValidator;
+import com.n4systems.ejb.EventScheduleManager;
 import com.n4systems.exporting.io.MapReader;
 import com.n4systems.model.Asset;
+import com.n4systems.model.EventSchedule;
 import com.n4systems.persistence.Transaction;
 import com.n4systems.services.asset.AssetSaveService;
+import com.n4systems.util.ServiceLocator;
 
 public class AssetImporter extends AbstractImporter<AssetView> {
 	private final AssetSaveService saver;
 	private final AssetToModelConverter converter;
+	private EventScheduleManager eventScheduleManager;
+
 	
 	public AssetImporter(MapReader mapReader, Validator<ExternalModelView> validator, AssetSaveService saver, AssetToModelConverter converter) {
 		super(AssetView.class, mapReader, validator);
 		this.saver = saver;
 		this.converter = converter;
-		
+		this.eventScheduleManager = ServiceLocator.getEventScheduleManager();
 		validator.getValidationContext().put(AssetViewValidator.ASSET_TYPE_KEY, converter.getType());
 	}
 
@@ -27,7 +34,16 @@ public class AssetImporter extends AbstractImporter<AssetView> {
 	protected void importView(Transaction transaction, AssetView view) throws ConversionException {
 		Asset asset = converter.toModel(view, transaction);
 		saver.setAsset(asset);
-		saver.createWithoutHistory();
+		asset = saver.createWithoutHistory();
+		autoScheduleEvents(asset);
 	}
 
+	private void autoScheduleEvents(Asset asset) {
+		List<EventSchedule> eventSchedules = eventScheduleManager.getAutoEventSchedules(asset);
+		for (EventSchedule schedule: eventSchedules) {
+			if(schedule != null) {
+				eventScheduleManager.update( schedule );
+			}
+		}
+	}
 }
