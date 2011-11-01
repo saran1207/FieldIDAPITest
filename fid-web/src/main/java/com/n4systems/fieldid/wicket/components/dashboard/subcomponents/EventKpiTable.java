d61d4922ba30c3409f27878e3068b674fa2bb04c
Diana Lai <diana.lai@fieldid.com>
2011-11-01 21:04:50 +0000

    WEB-2366 Added configuration message to widget when no orgs are configured

---
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/components/dashboard/subcomponents/EventKpiTable.java
@@ -0,0 +1,64 @@
+package com.n4systems.fieldid.wicket.components.dashboard.subcomponents;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.odlabs.wiquery.ui.progressbar.ProgressBar;
+
+import com.n4systems.model.orgs.BaseOrg;
+import com.n4systems.services.reporting.DashboardReportingService;
+import com.n4systems.services.reporting.EventKpiRecord;
+
+public class EventKpiTable extends Panel {
+	
+	@SpringBean
+	private DashboardReportingService reportingService;
+
+
+	public EventKpiTable(String id, List<BaseOrg> orgList) {
+		super(id);
+		
+		add(new ListView<EventKpiRecord>("customerEventKpiList", getCustomerEventKPIs(orgList)) {
+			
+			@Override
+			protected void populateItem(ListItem<EventKpiRecord> item) {
+				EventKpiRecord eventKpiRecord = item.getModelObject();
+				
+				item.add(new Label("customerName", eventKpiRecord.getCustomer().getDisplayName()));
+				item.add(new Label("scheduledEvents", eventKpiRecord.getTotalScheduledEvents() + ""));
+
+				Long completedPercentage = getCompletedPercentage(eventKpiRecord);
+				ProgressBar completedSchedules;
+
+				item.add(completedSchedules = new ProgressBar("progressBar"));
+				completedSchedules.setValue(completedPercentage.intValue());
+				
+				item.add(new Label("completedPercentage", completedPercentage + "%"));
+				item.add(new Label("failedSchedules", eventKpiRecord.getFailed() + ""));
+			}
+
+		});
+	}
+	
+	private List<EventKpiRecord> getCustomerEventKPIs(List<BaseOrg> orgs) {
+		
+		List<EventKpiRecord> eventKpis = new ArrayList<EventKpiRecord>();
+		for(BaseOrg org: orgs) {
+			eventKpis.add(reportingService.getEventKpi(org));
+		}
+		return eventKpis;
+	}
+	
+	private Long getCompletedPercentage(EventKpiRecord record) {
+		if(record.getTotalScheduledEvents() > 0L)
+			return Math.round((record.getCompleted().doubleValue() * 100)/record.getTotalScheduledEvents().doubleValue());
+		else 
+			return 0L;
+	}
+
+}
--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/widgets/EventKpiWidget.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/widgets/EventKpiWidget.java
@@ -3,15 +3,14 @@ package com.n4systems.fieldid.wicket.pages.widgets;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.wicket.behavior.SimpleAttributeModifier;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.image.ContextImage;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.odlabs.wiquery.ui.progressbar.ProgressBar;
 
-import com.n4systems.fieldid.service.org.OrgService;
+import com.n4systems.fieldid.wicket.components.dashboard.subcomponents.EventKpiTable;
+import com.n4systems.fieldid.wicket.model.FIDLabelModel;
 import com.n4systems.fieldid.wicket.pages.widgets.config.EventKPIConfigPanel;
 import com.n4systems.fieldid.wicket.pages.widgets.config.WidgetConfigPanel;
 import com.n4systems.fieldid.wicket.util.AjaxCallback;
@@ -19,50 +18,27 @@ import com.n4systems.model.dashboard.WidgetDefinition;
 import com.n4systems.model.dashboard.widget.EventKPIWidgetConfiguration;
 import com.n4systems.model.dashboard.widget.WidgetConfiguration;
 import com.n4systems.model.orgs.BaseOrg;
-import com.n4systems.services.reporting.DashboardReportingService;
-import com.n4systems.services.reporting.EventKpiRecord;
 
 public class EventKpiWidget extends Widget {
 	
-	@SpringBean
-	private DashboardReportingService reportingService;
-	
-	@SpringBean
-	public OrgService orgService;
-
 	public EventKpiWidget(String id, WidgetDefinition widgetDefinition) {
         super(id, new Model<WidgetDefinition>(widgetDefinition));
 		setOutputMarkupId(true);
 		
-		add(new ListView<EventKpiRecord>("customerEventKpiList", getCustomerEventKPIs(getOrgList())) {
-
-			@Override
-			protected void populateItem(ListItem<EventKpiRecord> item) {
-				EventKpiRecord eventKpiRecord = item.getModelObject();
-				
-				item.add(new Label("customerName", eventKpiRecord.getCustomer().getDisplayName()));
-				item.add(new Label("scheduledEvents", eventKpiRecord.getTotalScheduledEvents() + ""));
-
-				Long completedPercentage = getCompletedPercentage(eventKpiRecord);
-				ProgressBar completedSchedules;
-
-				item.add(completedSchedules = new ProgressBar("progressBar"));
-				completedSchedules.setValue(completedPercentage.intValue());
-				
-				item.add(new Label("completedPercentage", completedPercentage + "%"));
-				item.add(new Label("failedSchedules", eventKpiRecord.getFailed() + ""));
-			}
-
-		});
+		List<BaseOrg> orgList = getOrgList();
+		ContextImage arrow;
+		add(arrow = new ContextImage("arrow", "images/dashboard/arrow.png"));
+		
+		if(orgList.isEmpty()) {
+			Label message= new Label("eventKpiContent", new FIDLabelModel("message.configure_widget"));
+			message.add(new SimpleAttributeModifier("class", "configureMessage"));
+			add(message);
+			arrow.setVisible(true);
+		} else {
+			add(new EventKpiTable("eventKpiContent", orgList));
+			arrow.setVisible(false);
+		}
 	}
-
-	private Long getCompletedPercentage(EventKpiRecord record) {
-		if(record.getTotalScheduledEvents() > 0L)
-			return Math.round((record.getCompleted().doubleValue() * 100)/record.getTotalScheduledEvents().doubleValue());
-		else 
-			return 0L;
-	}
-
 	
 	private List<BaseOrg> getOrgList() {
 		EventKPIWidgetConfiguration config = (EventKPIWidgetConfiguration) getWidgetDefinition().getObject().getConfig();
@@ -72,15 +48,6 @@ public class EventKpiWidget extends Widget {
 		}else {
 			return config.getOrgs();
 		}
-	}
-
-	private List<EventKpiRecord> getCustomerEventKPIs(List<BaseOrg> orgs) {
-		
-		List<EventKpiRecord> eventKpis = new ArrayList<EventKpiRecord>();
-		for(BaseOrg org: orgs) {
-			eventKpis.add(reportingService.getEventKpi(org));
-		}
-		return eventKpis;
 	}
 	
     @Override
