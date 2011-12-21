c714bc4d975ec602982baaf0a57e4128c6a5905e
Dereck Dick <dereck.dick@fieldid.com>
2011-12-21 20:18:05 +0000

    WEB-2542 - fix "non-smart" failed event loader.

--- fid-core/src/main/java/com/n4systems/model/event/FailedEventListLoader.java
+++ fid-core/src/main/java/com/n4systems/model/event/FailedEventListLoader.java
@@ -1,12 +1,8 @@
 package com.n4systems.model.event;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.persistence.EntityManager;
 
@@ -20,16 +16,14 @@ import com.n4systems.model.utils.PlainDate;
 import com.n4systems.persistence.loaders.ListLoader;
 import com.n4systems.util.DateHelper;
 import com.n4systems.util.persistence.QueryBuilder;
-import com.n4systems.util.persistence.WhereClause.ChainOp;
 import com.n4systems.util.persistence.WhereClauseFactory;
 import com.n4systems.util.persistence.WhereParameter.Comparator;
-import com.n4systems.util.persistence.WhereParameterGroup;
 import com.n4systems.util.time.Clock;
 
-public class FailedEventListLoader extends ListLoader<Event> {
+public class FailedEventListLoader extends ListLoader<Event>{
 	
-	protected  Clock clock;
-	protected SimpleFrequency frequency;
+	private Clock clock;
+	private SimpleFrequency frequency;
 	protected NotificationSetting setting;
 	
 	public FailedEventListLoader(SecurityFilter filter) {
@@ -38,55 +32,18 @@ public class FailedEventListLoader extends ListLoader<Event> {
 
 	@Override
 	protected List<Event> load(EntityManager em, SecurityFilter filter) {
-		// see WEB-2542 for details on how to handle failed, then passed events on same asset. 
-		QueryBuilder<Event> builder = createQueryBuilder(filter);
+		QueryBuilder<Event> builder = new QueryBuilder<Event>(Event.class, filter);
 		
-		WhereParameterGroup statusGroup = new WhereParameterGroup("statusgroup");		
-		statusGroup.addClause(WhereClauseFactory.create(Comparator.EQ, "failStatus", "status", Status.FAIL, null, ChainOp.OR));
-		statusGroup.addClause(WhereClauseFactory.create(Comparator.EQ, "passStatus", "status", Status.PASS, null, ChainOp.OR));		
-		builder.addWhere(statusGroup);		
-		
+		builder.addWhere(WhereClauseFactory.create("status", Status.FAIL));
 		builder.addWhere(Comparator.GE, "date", "date", getFromDate());
-		builder.addOrder("date");
 
 		if(setting != null) {
 			applyNotificationFilters(builder);
 		}
 		
+		builder.addOrder("date");
 		builder.getPostFetchPaths().addAll(Arrays.asList(Event.ALL_FIELD_PATHS));
-		List<Event> results = builder.getResultList(em);
-		
-		results = filterEarlierEventsForSameAsset(results);		
-
-		Collections.sort(results, new java.util.Comparator<Event>() {
-			@Override public int compare(Event a, Event b) {
-				return (int) (a.getDate().getTime()-b.getDate().getTime());
-			} 
-		});		
-		return results;
-	}
-
-	/*pkg protected so i can extract override in test*/
-	QueryBuilder<Event> createQueryBuilder(SecurityFilter filter) {
-		return new QueryBuilder<Event>(Event.class, filter);
-	}
-
-	private List<Event> filterEarlierEventsForSameAsset(List<Event> results) {
-		// precondition : list is sorted ascending by date.
-		final Map<String, Event> events = new HashMap<String, Event>();		
-		for (Event event:results) {			
-			String key = makeEventAssetKey(event);     
-			if (Status.FAIL.equals(event.getStatus())) {
-				events.put(key, event);
-			} else if (Status.PASS.equals(event.getStatus())) {
-				events.remove(key);
-			}
-		}
-		return new ArrayList<Event>(events.values());
-	}
-
-	private String makeEventAssetKey(Event event) {
-		return event.getType().getId()+":"+event.getAsset().getId();
+		return builder.getResultList(em);
 	}
 
 	protected void applyNotificationFilters(QueryBuilder<Event> builder) {
