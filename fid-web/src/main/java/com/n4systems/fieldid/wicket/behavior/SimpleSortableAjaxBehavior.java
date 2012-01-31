08a683ce2cfa420a8150d37db07d2b5197e3799c
Dereck Dick <dereck.dick@fieldid.com>
2012-01-31 22:41:31 +0000

    WEB-2604 - put in JS hook to turn on/off row highlighting as needed.

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/behavior/SimpleSortableAjaxBehavior.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/behavior/SimpleSortableAjaxBehavior.java
@@ -4,7 +4,8 @@ import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.odlabs.wiquery.ui.sortable.SortableAjaxBehavior;
 
-public abstract class SimpleSortableAjaxBehavior extends SortableAjaxBehavior {
+@SuppressWarnings("serial")
+public abstract class SimpleSortableAjaxBehavior<T extends Component> extends SortableAjaxBehavior<T> {
 
     @Override
     public void onReceive(Component sortedComponent, int index, Component parentSortedComponent, AjaxRequestTarget ajaxRequestTarget) {
--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/saveditems/ManageSavedItemsPage.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/saveditems/ManageSavedItemsPage.java
@@ -1,5 +1,36 @@
 package com.n4systems.fieldid.wicket.pages.saveditems;
 
+import java.util.Date;
+import java.util.List;
+
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.ajax.markup.html.form.AjaxCheckBox;
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.behavior.Behavior;
+import org.apache.wicket.markup.html.IHeaderResponse;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.RequiredTextField;
+import org.apache.wicket.markup.html.image.ContextImage;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.model.StringResourceModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.odlabs.wiquery.ui.sortable.SortableAjaxBehavior;
+
 import com.n4systems.fieldid.service.PersistenceService;
 import com.n4systems.fieldid.service.user.UserService;
 import com.n4systems.fieldid.wicket.FieldIDSession;
@@ -16,37 +47,14 @@ import com.n4systems.model.saveditem.SavedItem;
 import com.n4systems.model.saveditem.SavedReportItem;
 import com.n4systems.model.saveditem.SavedSearchItem;
 import com.n4systems.model.user.User;
-import org.apache.wicket.AttributeModifier;
-import org.apache.wicket.Component;
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.ajax.markup.html.form.AjaxButton;
-import org.apache.wicket.ajax.markup.html.form.AjaxCheckBox;
-import org.apache.wicket.behavior.Behavior;
-import org.apache.wicket.markup.html.IHeaderResponse;
-import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.RequiredTextField;
-import org.apache.wicket.markup.html.image.ContextImage;
-import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.LoadableDetachableModel;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
-import org.apache.wicket.model.StringResourceModel;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.odlabs.wiquery.ui.sortable.SortableAjaxBehavior;
 
-import java.util.Date;
-import java.util.List;
-
+@SuppressWarnings("serial")
 public class ManageSavedItemsPage extends FieldIDFrontEndPage {
 
+	private static final String HIGHLIGHT_REORDER_JS = "$('.list').delegate('tr', 'mousedown', function(event) { if(event.which==1) $(this).css('background-color','#eee'); } );";
+	private static final String UNHIGHLIGHT_REORDER_JS = "$('.list').delegate('tr', 'mouseup', function(event) { if(event.which==1) $(this).css('background-color', '#fff' ); } );";
+	    
+	
     @SpringBean
     private UserService userService;
 
@@ -54,7 +62,7 @@ public class ManageSavedItemsPage extends FieldIDFrontEndPage {
     private PersistenceService persistenceService;
 
     private WebMarkupContainer itemsListContainer;
-    private SortableAjaxBehavior sortableBehavior;
+    private SortableAjaxBehavior<WebMarkupContainer> sortableBehavior;
 
     private boolean reorderState = false;
     private boolean displayLastSearches;
@@ -84,7 +92,14 @@ public class ManageSavedItemsPage extends FieldIDFrontEndPage {
             }
         });
 
+        // if in "reorder" mode, then turn off row highlighting.
+        IModel<String> reorderModel = new AbstractReadOnlyModel<String>() {
+			@Override public String getObject() {
+				return reorderState ? "no-highlight" : "";
+			}        	
+        };
         itemsListContainer = new WebMarkupContainer("itemsListContainer");
+        itemsListContainer.add(new AttributeAppender("class", reorderModel, " "));        
         itemsListContainer.setOutputMarkupId(true);
         add(itemsListContainer);
 
@@ -146,9 +161,9 @@ public class ManageSavedItemsPage extends FieldIDFrontEndPage {
             }
         });
 
-        sortableBehavior = new SimpleSortableAjaxBehavior() {
+        sortableBehavior = new SimpleSortableAjaxBehavior<WebMarkupContainer>() {
             @Override
-            public void onUpdate(Component component, int index, AjaxRequestTarget target) {
+            public void onUpdate(WebMarkupContainer component, int index, AjaxRequestTarget target) {
                 SavedItem item = (SavedItem) component.getDefaultModelObject();
                 int oldIndex = savedItemsModel.getObject().indexOf(item);
                 
@@ -191,6 +206,8 @@ public class ManageSavedItemsPage extends FieldIDFrontEndPage {
     public void renderHead(IHeaderResponse response) {
         super.renderHead(response);
         response.renderCSSReference("style/newCss/component/manage_saved_items.css");
+        response.renderOnDomReadyJavaScript(HIGHLIGHT_REORDER_JS);
+        response.renderOnDomReadyJavaScript(UNHIGHLIGHT_REORDER_JS);
     }
 
     class EditItemNameForm extends Form {
