80ba281edd8583aa2e5224c1096dce0829181fe2
Kumana Jekeswaran <kumana.jekeswaran@fieldid.com>
2012-02-01 15:47:45 +0000

    Added more settings to ApiTenant that will be pulled down by IOS app.

--- fid-ws/src/main/java/com/n4systems/fieldid/ws/v1/resources/tenant/ApiTenant.java
+++ fid-ws/src/main/java/com/n4systems/fieldid/ws/v1/resources/tenant/ApiTenant.java
@@ -1,8 +1,21 @@
 package com.n4systems.fieldid.ws.v1.resources.tenant;
 
 public class ApiTenant {
+	private String serialNumberLabel;
 	private boolean usingAssignedTo;
+	private boolean usingJobSites;
+	private boolean usingAdvancedLocation;
+	private boolean usingOrderDetails;
+	private boolean usingGpsCapture;
+	
+	public void setSerialNumberLabel(String serialNumberLabel) {
+		this.serialNumberLabel = serialNumberLabel;
+	}
 
+	public String getSerialNumberLabel() {
+		return serialNumberLabel;
+	}
+	
 	public void setUsingAssignedTo(boolean usingAssignedTo) {
 		this.usingAssignedTo = usingAssignedTo;
 	}
@@ -10,4 +23,38 @@ public class ApiTenant {
 	public boolean isUsingAssignedTo() {
 		return usingAssignedTo;
 	}
+
+	public void setUsingJobSites(boolean usingJobSites) {
+		this.usingJobSites = usingJobSites;
+	}
+
+	public boolean isUsingJobSites() {
+		return usingJobSites;
+	}
+
+	public void setUsingAdvancedLocation(boolean usingAdvancedLocation) {
+		this.usingAdvancedLocation = usingAdvancedLocation;
+	}
+
+	public boolean isUsingAdvancedLocation() {
+		return usingAdvancedLocation;
+	}
+
+	public void setUsingOrderDetails(boolean usingOrderDetails) {
+		this.usingOrderDetails = usingOrderDetails;
+	}
+
+	public boolean isUsingOrderDetails() {
+		return usingOrderDetails;
+	}
+
+	public void setUsingGpsCapture(boolean usingGpsCapture) {
+		this.usingGpsCapture = usingGpsCapture;
+	}
+
+	public boolean isUsingGpsCapture() {
+		return usingGpsCapture;
+	}
+	
+	
 }
--- fid-ws/src/main/java/com/n4systems/fieldid/ws/v1/resources/tenant/ApiTenantResource.java
+++ fid-ws/src/main/java/com/n4systems/fieldid/ws/v1/resources/tenant/ApiTenantResource.java
@@ -9,7 +9,12 @@ public class ApiTenantResource extends ApiResource<ApiTenant, PrimaryOrg> {
 	@Override
 	public ApiTenant convertEntityToApiModel(PrimaryOrg primaryOrg) {
 		ApiTenant apiTenant = new ApiTenant();
+		apiTenant.setSerialNumberLabel(primaryOrg.getIdentifierLabel());
 		apiTenant.setUsingAssignedTo(primaryOrg.hasExtendedFeature(ExtendedFeature.AssignedTo));
+		apiTenant.setUsingJobSites(primaryOrg.hasExtendedFeature(ExtendedFeature.JobSites));
+		apiTenant.setUsingAdvancedLocation(primaryOrg.hasExtendedFeature(ExtendedFeature.AdvancedLocation));
+		apiTenant.setUsingOrderDetails(primaryOrg.hasExtendedFeature(ExtendedFeature.OrderDetails));
+		apiTenant.setUsingGpsCapture(primaryOrg.getTenant().getSettings().isGpsCapture());
 		return apiTenant;
 	}
 }
