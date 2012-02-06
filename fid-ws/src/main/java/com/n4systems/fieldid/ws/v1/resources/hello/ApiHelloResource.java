aa235a40defe936e2ba110ca7275791517c641e5
Mark Frederiksen <mark.frederiksen@fieldid.com>
2012-02-06 15:56:22 +0000

    IOS-85
    Hello now responds with string.

--- fid-ws/src/main/java/com/n4systems/fieldid/ws/v1/resources/hello/ApiHelloResource.java
+++ fid-ws/src/main/java/com/n4systems/fieldid/ws/v1/resources/hello/ApiHelloResource.java
@@ -3,6 +3,7 @@ package com.n4systems.fieldid.ws.v1.resources.hello;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
@@ -16,13 +17,15 @@ public class ApiHelloResource {
 	
 	@GET
 	@Consumes(MediaType.TEXT_PLAIN)
-	public void hello(
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello(
 			@QueryParam("tenant") String tenant,
 			@QueryParam("user") String user,
 			@QueryParam("version") String version,
 			@QueryParam("device") String device) {
 
 		logger.info(String.format("Hello: [%s:%s] [%s] [%s]", tenant, user, version, device));
+		return "Hello";
 	}
 	
 }
