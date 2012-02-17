15380ec03107588990a40a8a622c1fb8ee1d44ae
Dereck Dick <dereck.dick@fieldid.com>
2012-02-17 19:17:42 +0000

    WEB-2682 - tweak CSS w.r.t. panel sizes.

--- fid-web/src/main/webapp/javascript/fieldIdWide.js
+++ fid-web/src/main/webapp/javascript/fieldIdWide.js
@@ -6,32 +6,29 @@
 
 var fieldIdWidePage = (function() { 
 
-	var leftMenuWidth = '23%'; 
+	var leftMenuWidth = '325px'; 
 	var contentLeft = leftMenuWidth;
-	var contentWidth = '77%';
+	var contentWidth = '';
 	
 	var init = function(leftMenuToShow) {
 		$('.left-menu').show();		
 		$('#page .centre').css('width',contentWidth).css('marginLeft',contentLeft);			
-		$('.paginationWrapper').css('width',contentWidth).css('left',contentLeft);
 	};
 	
 	var showLeftMenu = function() {
 		var leftMenu = $('.left-menu');
 		if (!leftMenu.is(':visible')) {				
-			$('#page .centre').animate({width:contentWidth,marginLeft:contentLeft},200, 'linear', function() { leftMenu.show();} );
-			$('.paginationWrapper').css('width',contentWidth).css('left',contentLeft);
+			$('#page .centre').animate({marginLeft:contentLeft},200, 'linear', function() { leftMenu.show();} );
 		}		
 	}
 	
 	var hideLeftMenu = function() {
 		// make sure any pesky dialogs are removed first.
 		$('.locationSelection').remove(); 
-		$('.orgSelector').remove();  //should this be hide or remove??
+		$('.orgSelector').remove();
 		
 		$('.left-menu').hide(); 
-		$('#page .centre').animate({width:'100%',marginLeft:'0%'},200);
-		$('.paginationWrapper').css('width','100%').css('marginLeft','0');
+		$('#page .centre').animate({marginLeft:'0%'},200);
 	}
 			
 	return { 
