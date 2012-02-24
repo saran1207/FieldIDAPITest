882bc1acedcce5b431dc1f0c0e680835f54d5c55
Dereck Dick <dereck.dick@fieldid.com>
2012-02-24 20:04:09 +0000

    WEB-2697 - fix pagination bar.

--- fid-web/src/main/webapp/javascript/fieldIdWide.js
+++ fid-web/src/main/webapp/javascript/fieldIdWide.js
@@ -10,19 +10,8 @@ var fieldIdWidePage = (function() {
 	var contentLeft = leftMenuWidth;	
 	
 	var init = function(showLeftMenu) {
-		$(document).delegate('.asset-actions  .menu >  a', 'click', function() {
-			$('.asset-actions .menu-items').show();	
-			return false;
-		});		
-		// in an ideal world we would have a single top level div surrounding  all content instead of listing all (page,pageHeader...) in selector. 
-		$(document).delegate('"#page, #pageHeader', 'click', function(e) {
-			$('.asset-actions .menu-items').hide();					
-		});		
-		if (!showLeftMenu) {
-			initNoLeftMenu();
-		} else { 
-			initFilters();
-		}
+		addMenuHandlers();
+		initLeftMenu(showLeftMenu);
 	};
 	
 	var showLeftMenu = function() {	
@@ -31,7 +20,6 @@ var fieldIdWidePage = (function() {
 		if (!leftMenu.is(':visible')) {				
 			$('#page .centre').animate({marginLeft:contentLeft},220, 'linear', function() { leftMenu.show();} );
 		}
-		// need to restore "true" state of toggle button...
 	};
 
 	
@@ -40,9 +28,14 @@ var fieldIdWidePage = (function() {
 		$('#page .centre').animate({marginLeft:'0%'},220);
 	};
 			
-	var initNoLeftMenu = function() {
-		hideLeftImpl();
-		$('#page .centre').css('marginLeft','0%');
+	var initLeftMenu = function(showLeftMenu) {
+		if (showLeftMenu) {
+			// set filter toggle button to true (it's the default config panel to be shown).
+			$('.sub-menu .config .filters').addClass('true');	
+		} else {
+			hideLeftImpl();
+			$('#page .centre').css('marginLeft','0%');
+		}
 	};
 				
 	function hideLeftImpl() { 
@@ -50,10 +43,16 @@ var fieldIdWidePage = (function() {
 		$('.paginationWrapper').css('left','0px');		
 		$('#left-menu').hide(); 		
 	}
-	
-	// set filter toggle button to true (it's the default config panel to be shown).
-	function initFilters() {
-		$('.sub-menu .config .filters').addClass('true');				
+		
+	function addMenuHandlers() { 
+		$(document).delegate('.asset-actions  .menu >  a', 'click', function() {
+			$('.asset-actions .menu-items').show();	
+			return false;
+		});		
+		// in an ideal world we would have a single top level div surrounding  all content instead of listing all (page,pageHeader...) in selector. 
+		$(document).delegate('"#page, #pageHeader', 'click', function(e) {
+			$('.asset-actions .menu-items').hide();					
+		});		
 	}
 	
 	return { 
