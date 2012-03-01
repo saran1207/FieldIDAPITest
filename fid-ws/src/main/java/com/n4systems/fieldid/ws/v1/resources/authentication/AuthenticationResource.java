cb2d3097335b40e2e8946369e9a9e1f86247bcbc
Kumana Jekeswaran <kumana.jekeswaran@fieldid.com>
2012-03-01 17:18:32 +0000

    IOS-53
    IOS Passcode Authentication

--- fid-ws/src/main/java/com/n4systems/fieldid/ws/v1/resources/authentication/AuthenticationResource.java
+++ fid-ws/src/main/java/com/n4systems/fieldid/ws/v1/resources/authentication/AuthenticationResource.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,6 +25,7 @@ import com.n4systems.services.SecurityContext;
 @Path("/authenticate")
 @Component
 public class AuthenticationResource extends FieldIdPersistenceService {
+	private static Logger logger = Logger.getLogger(AuthenticationResource.class);
 
 	@Autowired protected UserService userService;
 	@Autowired protected ApiUserResource apiUserResource;
@@ -38,11 +40,36 @@ public class AuthenticationResource extends FieldIdPersistenceService {
 			@FormParam("user") String userId, 
 			@FormParam("password") String password) {
 		
+		logger.info("Standard authentication for " + tenantName + " for " + userId);
+		
 		if (tenantName == null || userId == null || password == null) {
 			throw new ForbiddenException();
 		}
 
 		User user = userService.authenticateUserByPassword(tenantName, userId, password);
+		return authenticateUser(user);
+	}
+	
+	@POST
+	@Path("passcode")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Transactional(readOnly = true)
+	public ApiUser authenticate(
+			@FormParam("tenant") String tenantName,
+			@FormParam("passcode") String passcode) {
+		
+		logger.info("Passcode authentication for " + tenantName);
+		
+		if (tenantName == null || passcode == null) {
+			throw new ForbiddenException();
+		}
+		
+		User user = userService.authenticateUserBySecurityCard(tenantName, passcode);
+		return authenticateUser(user);
+	}
+	
+	private ApiUser authenticateUser(User user) {
 		if (user == null) {
 			throw new ForbiddenException();
 		}
