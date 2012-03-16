e41d20605632de084b0df45d6677f608c3399d28
Neil Curzon <neil.curzon@fieldid.com>
2012-03-16 18:48:42 +0000

    WEB-2790
    Attempt at fixing reporting mass action links.

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/components/search/results/MassActionLink.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/components/search/results/MassActionLink.java
@@ -1,5 +1,6 @@
 package com.n4systems.fieldid.wicket.components.search.results;
 
+import org.apache.log4j.Logger;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.IModel;
@@ -7,6 +8,8 @@ import org.apache.wicket.model.IModel;
 import java.lang.reflect.Constructor;
 
 public class MassActionLink<P extends WebPage> extends Link {
+
+    private static final Logger logger = Logger.getLogger(MassActionLink.class);
 
     private Class<P> pageClass;
     private Object model;
@@ -21,8 +24,9 @@ public class MassActionLink<P extends WebPage> extends Link {
     public void onClick() {
         try {
             final Constructor<P> constructor = pageClass.getConstructor(IModel.class);
-            getRequestCycle().setResponsePage(constructor.newInstance(model));
+            setResponsePage(constructor.newInstance(model));
         } catch (Exception e) {
+            logger.error("error instantiating mass action page save name as page", e);
             throw new RuntimeException(e);
         }
     }
