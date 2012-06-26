5cdc17a673383e1db2a4e5cfe8a7ffca8f44a10b
Neil Curzon <neil.curzon@fieldid.com>
2012-06-26 15:39:28 +0000

    Fixing ContentTypeUtil to work with PNG images with upper case extension.

--- fid-core/src/main/java/com/n4systems/util/ContentTypeUtil.java
+++ fid-core/src/main/java/com/n4systems/util/ContentTypeUtil.java
@@ -5,7 +5,7 @@ import javax.activation.FileTypeMap;
 public class ContentTypeUtil {
 
     public static String getContentType(String fileName) {
-        if (fileName != null && fileName.endsWith(".png")) {
+        if (fileName != null && fileName.toLowerCase().endsWith(".png")) {
             return "image/png";
         } else {
             return FileTypeMap.getDefaultFileTypeMap().getContentType(fileName);
