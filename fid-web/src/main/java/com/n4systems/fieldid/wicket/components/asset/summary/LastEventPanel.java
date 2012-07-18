5322eb3320c23d57384560aab4fa3c9d4ca8ba60
Diana Lai <diana.lai@fieldid.com>
2012-07-18 15:17:56 +0000

    WEB-3053 Updated last event performed panel

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/components/asset/summary/LastEventPanel.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/components/asset/summary/LastEventPanel.java
@@ -2,17 +2,16 @@ package com.n4systems.fieldid.wicket.components.asset.summary;
 
 import com.n4systems.fieldid.service.asset.AssetService;
 import com.n4systems.fieldid.wicket.FieldIDSession;
-import com.n4systems.fieldid.wicket.components.NonWicketIframeLink;
-import com.n4systems.fieldid.wicket.components.NonWicketLink;
+import com.n4systems.fieldid.wicket.components.asset.events.table.EventActionsCell;
 import com.n4systems.model.Asset;
 import com.n4systems.model.Event;
 import com.n4systems.model.Status;
 import com.n4systems.util.FieldidDateFormatter;
-import org.apache.wicket.AttributeModifier;
-import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.image.ContextImage;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import java.text.DateFormat;
@@ -34,30 +33,24 @@ public class LastEventPanel extends Panel {
         Event lastEvent = assetService.findLastEvents(asset, FieldIDSession.get().getSessionUser().getSecurityFilter());
 
         if(lastEvent != null) {
-            String eventType = lastEvent.getType().getName();
             String lastEventDate = new FieldidDateFormatter(lastEvent.getDate(), FieldIDSession.get().getSessionUser(), true, true).format();
             Status status = lastEvent.getStatus();
 
-            WebMarkupContainer result;
-            add(result = new WebMarkupContainer("result"));
-            Label statusLabel;
-            result.add(statusLabel = new Label("lastEventResult", status.getDisplayName()));
+            ContextImage image;
+
             if (status.equals(Status.PASS)) {
-                result.add(new AttributeModifier("class", "passColor"));
+                add(image = new ContextImage("resultIcon", "images/event-completed-pass.png"));
             } else if (status.equals(Status.FAIL)) {
-                result.add(new AttributeModifier("class", "failColor"));
+                add(image = new ContextImage("resultIcon", "images/event-completed-fail.png"));
             } else {
-                result.add(new AttributeModifier("class", "naColor"));
+                add(image = new ContextImage("resultIcon", "images/event-completed-na.png"));
             }
             
             add(new Label("lastEventDate", lastEventDate));
-            add(new Label("lastEventType", eventType));
+            add(new Label("lastEventType", lastEvent.getType().getName()));
+            add(new Label("performedBy", lastEvent.getPerformedBy().getDisplayName()));
+            add(new EventActionsCell("actions", Model.of(lastEvent)));
 
-            String viewEventUrl = "aHtml/iframe/event.action?uniqueID=" + lastEvent.getID() + "&assetId=" + asset.getId();
-            add(new NonWicketIframeLink("viewLastEventLink", viewEventUrl, false, 600, 600 ));
-
-            String printEventUrl = "file/downloadEventCert.action?uniqueID="+ lastEvent.getID() + "&reportType=INSPECTION_CERT";
-            add(new NonWicketLink("printLastEventLink", printEventUrl));
         }
     }
 }
