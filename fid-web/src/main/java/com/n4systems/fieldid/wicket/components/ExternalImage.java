1c70ec16918ee0058365006eb531c18bec869951
Diana Lai <diana.lai@fieldid.com>
2012-07-19 14:56:49 +0000

    WEB-3074 Added logic to hide image if url is invalid

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/components/ExternalImage.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/components/ExternalImage.java
@@ -4,16 +4,28 @@ import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.WebComponent;
 
+import java.net.URL;
+
 public class ExternalImage extends WebComponent {
 
     public ExternalImage(String id, String imageUrl) {
         super(id);
         add(new AttributeModifier("src", imageUrl));
-	    setVisible(!(imageUrl==null || imageUrl.equals("")));
+	    setVisible(imageUrl != null && !imageUrl.equals("") && isValidImage(imageUrl));
     }
 
     protected void onComponentTag(ComponentTag tag) {
             super.onComponentTag(tag);
             checkComponentTag(tag, "img");
+    }
+    
+    private boolean isValidImage(String imageUrl){
+        URL url = null;
+        try {
+            url = new URL(imageUrl);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
