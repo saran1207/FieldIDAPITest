5ad657b1a0c30f836f8aba547e7835cc4d4bade4
Neil Curzon <neil.curzon@fieldid.com>
2012-08-08 16:13:17 +0000

    Fixing case of loading asset counts for an archived org.

--- fid-ws/src/main/java/com/n4systems/fieldid/ws/v1/resources/assetcount/ApiAssetCountResource.java
+++ fid-ws/src/main/java/com/n4systems/fieldid/ws/v1/resources/assetcount/ApiAssetCountResource.java
@@ -1,18 +1,5 @@
 package com.n4systems.fieldid.ws.v1.resources.assetcount;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.n4systems.fieldid.service.FieldIdPersistenceService;
 import com.n4systems.fieldid.ws.v1.exceptions.NotFoundException;
 import com.n4systems.fieldid.ws.v1.resources.model.ListResponse;
@@ -20,6 +7,13 @@ import com.n4systems.model.Asset;
 import com.n4systems.model.orgs.BaseOrg;
 import com.n4systems.model.security.OwnerAndDownFilter;
 import com.n4systems.util.persistence.QueryBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @Path("assetCount")
@@ -38,12 +32,8 @@ public class ApiAssetCountResource extends FieldIdPersistenceService {
 			builder.setCountSelect();
 
             BaseOrg org = null;
-            // This allows a division user to load its parent customer so that we can get an asset count for the customers screen on the mobile
-            if (currentUserOwner.isDivision() && currentUserOwner.getParent().getId().equals(orgId)) {
-                org = persistenceService.findNonSecure(BaseOrg.class, orgId);
-            } else {
-                org = persistenceService.find(BaseOrg.class, orgId);
-            }
+
+            org = persistenceService.findUsingTenantOnlySecurityWithArchived(BaseOrg.class, orgId);
 
 			if (org == null) {
 				throw new NotFoundException("Organization", orgId);
