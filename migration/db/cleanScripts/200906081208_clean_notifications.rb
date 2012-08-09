ed5b7474c2b4b54b5c3843ae7936899bcdc084a2
Neil Curzon <neil.curzon@fieldid.com>
2012-08-09 21:05:18 +0000

    Fixing clean script to include send saved item schedules email addresses.

--- migration/db/cleanScripts/200906081208_clean_notifications.rb
+++ migration/db/cleanScripts/200906081208_clean_notifications.rb
@@ -4,6 +4,8 @@ class CleanNotifications < ActiveRecord::Migration
 	  execute "delete from notificationsettings_addresses"
 	  execute "delete from notificationsettings_assettypes"
 	  execute "delete from notificationsettings"
+    execute "delete from send_saved_item_schedules_emails"
+    execute "delete from send_saved_item_schedules"
   end
   
   def self.down
