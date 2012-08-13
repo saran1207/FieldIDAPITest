ce1f55ca8111d410def3ce66e035a8474ad50d3c
Dereck Dick <dereck.dick@fieldid.com>
2012-08-13 20:55:14 +0000

    WEB-3188 : phase three of work widget.  mark days if they have an event.

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/components/Agenda.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/components/Agenda.java
@@ -39,65 +39,87 @@ public class Agenda extends Panel  {
     private @SpringBean DateService dateService;
     private @SpringBean EventService eventService;
 
-    private static final String CALL_WICKET_JS = "function upateAgenda() { " +
-            " var wcall = wicketAjaxGet('%s',function(){},function(){}); }";
+    private static final String INIT_CALENDAR_JS = "calendar.init('%s', %s);";
+//    private static final String CALL_WICKET_JS = "function updateAgenda(month, year) { " +
+//            " var wcall = wicketAjaxGet(getUrl('%s',month,year),function(){},function(){});" +
+//            " }";
 
-    private Calendar calendar;
+    private WebMarkupContainer calendar;
     private Date from = dateService.nowAsDate();
     private Date to = null;
-    private ListView<Event> listView;
 
     private WebMarkupContainer dayByDayView;
-    private boolean showCalendar = false;
+    private boolean showCalendar = true;
     private AbstractDefaultAjaxBehavior behavior;
+    private ListView<EventDay> listView;
 
     public Agenda(String id) {
         super(id);
 
+        // TODO : change this to simple javascript, not AJAX
         add(new CheckBox("showCalendar", new PropertyModel<Boolean>(this, "showCalendar")).add(new OnChangeAjaxBehavior() {
             @Override protected void onUpdate(AjaxRequestTarget target) {
-                updateViews();
+                calendar.setVisible(showCalendar);
                 target.add(dayByDayView);
             }
         }));
         add(dayByDayView = createDayByDayView());
-        updateViews();
+
+        behavior = new AbstractDefaultAjaxBehavior() {
+            protected void respond(final AjaxRequestTarget target) {
+                target.add(dayByDayView);
+            }
+        };
+        calendar.setOutputMarkupPlaceholderTag(true).setVisible(true);
+
     }
 
     private WebMarkupContainer createDayByDayView() {
         final WebMarkupContainer container = new WebMarkupContainer("dayByDayView");
-        calendar = new Calendar("calendar", new PropertyModel<Date>(this, "from"));
+        calendar = new WebMarkupContainer("calendar", new PropertyModel<Date>(this, "from"));
 
-        ListView<EventDay> list = new ListView<EventDay>("days", new EventDayModel()) {
+        listView = new ListView<EventDay>("days", new EventDayModel()) {
             @Override protected void populateItem(ListItem<EventDay> item) {
                 item.add(new EventDayPanel("day", item.getModelObject()));
             }
         };
-        container.add(list);
-        behavior = new AbstractDefaultAjaxBehavior() {
-            protected void respond(final AjaxRequestTarget target) {
-                target.add(container);
-            }
-        };
 
-        container.add(behavior);
-        container.add(calendar);
-        container.setOutputMarkupId(true).setOutputMarkupPlaceholderTag(true);
+        container.add(listView);
+        container.add(calendar.setOutputMarkupId(true));
+        container.setOutputMarkupId(true);
         return container;
     }
 
     @Override
     public void renderHead(IHeaderResponse response) {
         response.renderCSSReference("style/component/agenda.css");
-        response.renderOnDomReadyJavaScript(String.format(CALL_WICKET_JS, behavior.getCallbackUrl()));
-    }
 
-    private void updateViews() {
-        calendar.setVisible(showCalendar);
+//        List<WorkSummaryRecord> workData = eventService.getMontlyWorkSummary(dateService.now());
+        String workJson = "{year:2012, month:3, events:[0,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4]}";
+        response.renderOnLoadJavaScript(String.format(INIT_CALENDAR_JS, calendar.getMarkupId(), workJson));
+
+        // CAVEAT : the reason a special (datepicker only) version of jquery ui was brought in
+        //  is because if you referenced the entire ui library it would conflict the use of some
+        //  wiquery ui things.  (AutoComplete in this case).
+        // the best situation would be just to have all components use the predefined wiquery js references
+        //  i.e. renderJavaScriptReference(CoreUIJavaScriptResourceReference.get());
+        response.renderJavaScriptReference("javascript/jquery-ui-1.8.20.no-autocomplete.min.js");
+//        response.renderJavaScript(String.format(CALL_WICKET_JS, behavior.getCallbackUrl()), "updateAgenda");
+
+        response.renderCSSReference("style/jquery-redmond/jquery-ui-1.8.13.custom.css");
+        response.renderJavaScriptReference("javascript/jquery-ui-timepicker-addon.js");
+        response.renderJavaScriptReference("javascript/component/agenda.js");
+
     }
 
 
     class EventDayModel extends LoadableDetachableModel<List<EventDay>> {
+
+        private int size = 0;
+        @Override
+        public void detach() {
+            size = 0;
+        }
 
         @Override
         protected List<EventDay> load() {
@@ -118,7 +140,12 @@ public class Agenda extends Panel  {
             }
             ArrayList<EventDay> list = new ArrayList<EventDay>(map.values());
             sort(list);
+            size  = list.size();
             return list;
+        }
+
+        public int getSize() {
+            return size;
         }
     }
 
@@ -126,8 +153,8 @@ public class Agenda extends Panel  {
         public EventDayPanel(String id, EventDay eventDay) {
             super(id, "dayPanel", Agenda.this);
             add(new Label("date", DateUtil.getDayString(eventDay.getDate())));
-            add(new Label("meta", Model.of(eventDay.getDate().getDayOfMonth())));
-            listView = new ListView<Event>("list", eventDay.events) {
+            add(new Label("meta", getMetaData(eventDay)));
+            ListView<Event> listView = new ListView<Event>("list", eventDay.events) {
                 @Override protected void populateItem(ListItem<Event> item) {
                     Event event = item.getModelObject();
                     String image = event.isCompleted() ? "images/gps-icon-small.png" : "images/gps-recorded.png";
@@ -140,6 +167,10 @@ public class Agenda extends Panel  {
                 }
             };
             add(listView);
+        }
+
+        private Model<String> getMetaData(EventDay eventDay) {
+            return Model.of(eventDay.getDate().getDayOfMonth()+"");
         }
     }
 
@@ -160,6 +191,10 @@ public class Agenda extends Panel  {
             return date;
         }
 
+        public int getEventCount() {
+            return events.size();
+        }
+
         @Override
         public int compareTo(EventDay o) {
             if (o==null) {
@@ -168,4 +203,6 @@ public class Agenda extends Panel  {
             return (int) (date.toDate().getTime() - o.getDate().toDate().getTime());
         }
     }
+
+
 }
--- fid-web/src/main/webapp/javascript/component/agenda.js
+++ fid-web/src/main/webapp/javascript/component/agenda.js
@@ -6,22 +6,35 @@ var calendar = (function() {
 	/*
 	 * public methods exposed.
 	 * defaults to highlighting "today's" events.
+	 * eventDays contains array of events per day.
 	 */
-	var init = function(id) {
-		$('#'+id).datepicker(
+	var init = function(id, eventDays) {
+		var calendar = $('#'+id);
+		calendar.datepicker(
 			{
 				onSelect:select,
 				onChangeMonthYear: changeMonthYear
 			});
+		highlightEventfulDays(calendar,eventDays.events);
 	};
+
+	function highlightEventfulDays(calendar,eventDays) {
+		calendar.find('eventful').each(function(index) { $(this).remove(); });
+		calendar.find('table td a.ui-state-default').each(function(index) {
+			if (eventDays[index]>0) {
+				$(this).prepend('<p class="eventful">&#8226;</p>');
+			}
+		});
+	}
 
 	function changeMonthYear(year, month, inst) {
 		alert(year + ' ' + month);
 	}
 
 	function select(dateText,inst) {
-		var date = new Date(dateText);
-		filter(date.getUTCDate());
+		inst.inline = false;
+		var day = inst.selectedDay.replace(new RegExp('<(p|P)(.*)?>'),'');
+		filter(day);
 	}
 
 	function filter(dayOfMonth) {
