5e06295e417c5fc385421a96561514ca704223e0
Diana Lai <diana.lai@fieldid.com>
2012-10-04 19:37:45 +0000

    WEB-3374 Fixed broken migration

--- fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201210041131_AddNotesColumnToReporting.sql
+++ fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201210041131_AddNotesColumnToReporting.sql
@@ -1,4 +1,4 @@
 INSERT INTO column_mappings(created, modified, label, name, path_expression, sortable, default_order, group_id)
-VALUES(now(), now(), 'label.action_notes', 'event_search_action_notes', 'notes', TRUE, 10170, 1)
+VALUES(now(), now(), 'label.action_notes', 'event_search_action_notes', 'notes', TRUE, 10170, 1);
 
 INSERT into system_column_mappings(column_id) values((SELECT id FROM column_mappings WHERE name ='event_search_action_notes'));
