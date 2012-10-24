d14de61995d61c724f5d6e2053f8cbea9b2331b9
Diana Lai <diana.lai@fieldid.com>
2012-10-24 19:59:44 +0000

    WEB-3386 Updated migration to rename search path expression

--- fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201210151119_RenameEventNextDateToDueDate.sql
+++ fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201210151119_RenameEventNextDateToDueDate.sql
@@ -1,3 +1,5 @@
 ALTER TABLE masterevents CHANGE nextDate dueDate datetime;
 
 UPDATE column_mappings SET path_expression='dueDate' WHERE name='event_search_scheduled_date';
+
+UPDATE column_mappings SET path_expression = 'dueDate' WHERE name = 'event_schedule_nextdate';
