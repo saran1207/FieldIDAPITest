97055b532a79ed8f3544ebdbc414f581fc2f9d33
Neil Curzon <neil.curzon@fieldid.com>
2012-11-19 22:30:32 +0000

    WEB-3469 Merging fix to trunk

--- fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201211191141_FixOpenEventsWithCompletedDates.sql
+++ fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201211191141_FixOpenEventsWithCompletedDates.sql
@@ -1,5 +1,5 @@
 create temporary table completed_open_events_that_have_results select me.event_id as id from masterevents me, criteriaresults res where event_state  = "OPEN" and completeddate is not null and res.event_id = me.event_id group by me.event_id having count(*)>0;
 
-update masterevents me set completeddate = null,performedby_id = null,result="VOID" where completeddate is not null and event_state="OPEN" and me.event_id not in (select id from completed_open_events_that_have_results);
+update masterevents me set completeddate = null,performedby_id = null,status="VOID" where completeddate is not null and event_state="OPEN" and me.event_id not in (select id from completed_open_events_that_have_results);
 
 drop temporary table completed_open_events_that_have_results;
