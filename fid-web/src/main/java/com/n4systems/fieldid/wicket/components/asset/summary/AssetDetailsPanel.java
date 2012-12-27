0ba5c6e8173b2212cc61ae7d737850c11c37f6e2
Diana Lai <diana.lai@fieldid.com>
2012-12-27 16:27:48 +0000

    WEB-3540 Updated created/modified info on asset summary

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/components/asset/summary/AssetDetailsPanel.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/components/asset/summary/AssetDetailsPanel.java
@@ -14,14 +14,17 @@ public class AssetDetailsPanel extends Panel {
         
         Asset asset = model.getObject();
 
+        String created = new FieldIdDateFormatter(asset.getCreated(), FieldIDSession.get().getSessionUser(), false, true).format();
+        String modified = new FieldIdDateFormatter(asset.getModified(), FieldIDSession.get().getSessionUser(), false, true).format();
+        String identified = new FieldIdDateFormatter(asset.getIdentified(), FieldIDSession.get().getSessionUser(), false, false).format();
+
         add(new Label("referenceNumber", new PropertyModel<Object>(asset, "customerRefNumber")));
         add(new Label("rfidNumber", new PropertyModel<Object>(asset, "rfidNumber")));
-        add(new Label("identifiedBy", new PropertyModel<Object>(asset, "identifiedBy.fullName")));
+        add(new Label("createdby", new PropertyModel<Object>(asset, "createdBy.fullName")));
+        add(new Label("created", created));
         add(new Label("modifiedBy", new PropertyModel<Object>(asset, "modifiedBy.fullName")));
+        add(new Label("modified", modified));
         add(new Label("visibility", new PropertyModel<Object>(asset, "published")));
-        
-        String identified = new FieldIdDateFormatter(asset.getIdentified(), FieldIDSession.get().getSessionUser(), false, false).format();
-        
         add(new Label("identified", identified));
     }
 }
