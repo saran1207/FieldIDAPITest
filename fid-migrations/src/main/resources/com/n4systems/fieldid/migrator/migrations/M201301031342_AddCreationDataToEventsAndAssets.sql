c516f5107a047273493030d59337419593c903dc
Neil Curzon <neil.curzon@fieldid.com>
2013-01-10 16:12:32 +0000

    WEB-3558
    Combining migrations that alter the masterevents and assets tables.

--- fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201301031342_AddCreationDataToEventsAndAssets.sql
+++ fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201301031342_AddCreationDataToEventsAndAssets.sql
@@ -1,5 +1,3 @@
-ALTER TABLE masterevents ADD COLUMN created_platform VARCHAR(200);
-ALTER TABLE masterevents ADD COLUMN modified_platform VARCHAR(200);
+ALTER TABLE masterevents ADD COLUMN created_platform_type VARCHAR(30), ADD COLUMN modified_platform_type VARCHAR(30), ADD COLUMN created_platform VARCHAR(200), ADD COLUMN modified_platform VARCHAR(200);
 
-ALTER TABLE assets ADD COLUMN created_platform VARCHAR(200);
-ALTER TABLE assets ADD COLUMN modified_platform VARCHAR(200);
+ALTER TABLE assets ADD COLUMN created_platform_type VARCHAR(30), ADD COLUMN modified_platform_type VARCHAR(30), ADD COLUMN created_platform VARCHAR(200), ADD COLUMN modified_platform VARCHAR(200);
--- fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201301041337_AddPlatformTypeDataToAssetsAndEvents.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE masterevents ADD COLUMN created_platform_type VARCHAR(30), ADD COLUMN modified_platform_type VARCHAR(30);
-
-ALTER TABLE assets ADD COLUMN created_platform_type VARCHAR(30), ADD COLUMN modified_platform_type VARCHAR(30);
