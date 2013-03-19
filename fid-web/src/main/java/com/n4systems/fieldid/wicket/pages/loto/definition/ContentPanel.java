aef71c1382cd9a6b472f22f41e78834f2af0940e
Dereck Dick <dereck.dick@fieldid.com>
2013-03-19 18:53:06 +0000

    WEB-3684 : add image header.

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/loto/definition/ContentPanel.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/loto/definition/ContentPanel.java
@@ -1,11 +1,16 @@
 package com.n4systems.fieldid.wicket.pages.loto.definition;
 
+import com.google.common.collect.Lists;
 import com.n4systems.fieldid.service.PersistenceService;
 import com.n4systems.fieldid.wicket.components.image.EditableImageList;
 import com.n4systems.model.common.EditableImage;
+import com.n4systems.model.procedure.ProcedureDefinition;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
@@ -16,16 +21,20 @@ public class ContentPanel extends Panel {
     private @SpringBean PersistenceService persistenceService;
 
     private List<EditableImage> images;
+    List<ProcedureDefinition> procedures = Lists.newArrayList();
 
     public ContentPanel(String id) {
         super(id);
 
-        // for debugging only....
+        // for debugging only...
        // images = persistenceService.findAll(EditableImage.class);
         //...............
 
         setOutputMarkupPlaceholderTag(true);
         add(new EditableImageList("images", new ImageModel() ));
+
+        //add(new ProcedureList("procedures", new PropertyModel(this,"procedures")));
+
         add(new AjaxLink("cancel") {
             @Override
             public void onClick(AjaxRequestTarget target) {
@@ -52,6 +61,18 @@ public class ContentPanel extends Panel {
 //            return Lists.newArrayList();
             List<EditableImage> all = persistenceService.findAll(EditableImage.class);
             return all;
+        }
+    }
+
+    class ProcedureList extends ListView<ProcedureDefinition> {
+
+        public ProcedureList(String id, IModel<List<? extends ProcedureDefinition>> model) {
+            super(id, model);
+        }
+
+        @Override
+        protected void populateItem(ListItem<ProcedureDefinition> item) {
+
         }
     }
 
