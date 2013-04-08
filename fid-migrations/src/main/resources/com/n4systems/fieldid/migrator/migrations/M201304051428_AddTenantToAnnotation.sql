a18a4d210710c71dca4a70ddaed249c61e5df49d
Dereck Dick <dereck.dick@fieldid.com>
2013-04-08 14:00:46 +0000

    fix migrations...column already there.

--- fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201304051428_AddTenantToAnnotation.sql
+++ fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201304051428_AddTenantToAnnotation.sql
@@ -1,7 +1,3 @@
-alter table image_annotation add column tenant_id bigint;
-alter table image_annotation add constraint fk_image_annotation_tenant foreign key (tenant_id) references tenants(id);
-alter table image_annotation drop column direction;
-
 alter table image_annotation add column createdby bigint default null;
 alter table image_annotation add column modifiedby bigint default null;
 
