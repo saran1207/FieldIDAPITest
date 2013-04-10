4667ce9f5853f32ec46bab53317b82a1b1fb23e1
Neil Curzon <neil.curzon@fieldid.com>
2013-04-10 21:13:07 +0000

    Re-add tenant id migration.

--- fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201304051428_AddTenantToAnnotation.sql
+++ fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201304051428_AddTenantToAnnotation.sql
@@ -1,3 +1,6 @@
+alter table image_annotation add column tenant_id bigint;
+alter table image_annotation add constraint fk_image_annotation_tenant foreign key (tenant_id) references tenants(id);
+
 alter table image_annotation add column createdby bigint default null;
 alter table image_annotation add column modifiedby bigint default null;
 
@@ -6,4 +9,3 @@ alter table image_annotation add column modified datetime not null;
 
 alter table image_annotation add constraint fk_annotation_created_user foreign key (createdby) references users (id);
 alter table image_annotation add constraint fk_annotation_modified_user foreign key (modifiedby) references users (id);
-
