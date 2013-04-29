1ff753096670977f05e6af22181b6c559fd68151
Scott Usher <scott.usher@fieldid.com>
2013-04-29 20:42:05 +0000

    WEB-3736

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/loto/definition/PrintProductSummary.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/loto/definition/PrintProductSummary.java
@@ -1,13 +1,5 @@
 package com.n4systems.fieldid.wicket.pages.loto.definition;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Scott Usher
- * Date: 4/19/13
- * Time: 3:41 PM
- * To change this template use File | Settings | File Templates.
- */
-
 import com.n4systems.fieldid.wicket.ComponentWithExternalHtml;
 import com.n4systems.fieldid.wicket.util.ProxyModel;
 import com.n4systems.model.procedure.IsolationPoint;
@@ -52,18 +44,20 @@ public class PrintProductSummary extends Panel {
 
                 if(iPt != null && StringUtils.isNotEmpty(iPt.getDeviceDefinition().getFreeformDescription())) {
                     deviceCount++;
-                }
+            }
             }
 
         }
 
         //lockout points
-        //add(new Label("lockoutPoints", ProxyModel.of(model, lockoutPts)));
-        add(new Label("lockoutPoints", Model.of("5")));
+        add(new Label("lockoutPoints", ProxyModel.of(model, lockoutPts)));
+
+        // number of isolation pts
+        //add(new Label("lockoutPoints", ProxyModel.of(model, on(ProcedureDefinition.class).getIsolationPoints().size())));
 
         //device count
-        //add(new Label("deviceCount", ProxyModel.of(model, deviceCount)));
-        add(new Label("deviceCount", Model.of("3")));
+        add(new Label("deviceCount", ProxyModel.of(model, deviceCount)));
+        //add(new Label("deviceCount", Model.of("3")));
 
         //warnings
         add(new Label("warnings", ProxyModel.of(model, on(ProcedureDefinition.class).getWarnings())));
