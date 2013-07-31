601ce4fea9aa8ef5d88bcc4fd955fa7133190cc0
Neil Curzon <neil.curzon@fieldid.com>
2013-07-31 17:41:14 +0000

    WEB-3970
    Updating tooltip to match styling guideline

--- fid-web/src/main/webapp/javascript/trends/trending.js
+++ fid-web/src/main/webapp/javascript/trends/trending.js
@@ -1,3 +1,6 @@
 var plainTrendsTooltip = function(item, options, total) {
-    return item.datapoint[1];
+    var dataIndex = item.dataIndex;
+    var criteriaName = item.series.xaxis.ticks[dataIndex].label;
+    var count = item.datapoint[1];
+    return "<span class='count'>"+count+"</span>"+"<span class='criteriaName'>"+criteriaName+"</span>";
 };
