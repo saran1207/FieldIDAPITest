6db87ead5e420132083e9388fbc1040917473474
Dereck Dick <dereck.dick@fieldid.com>
2013-08-02 20:21:47 +0000

    WEB-4009 : use ajax to hide popup.

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/components/popup/Popup.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/components/popup/Popup.java
@@ -2,34 +2,34 @@ package com.n4systems.fieldid.wicket.components.popup;
 
 import com.google.common.base.Preconditions;
 import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxEventBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.image.ContextImage;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
-import org.odlabs.wiquery.core.events.Event;
-import org.odlabs.wiquery.core.events.MouseEvent;
-import org.odlabs.wiquery.core.events.WiQueryEventBehavior;
-import org.odlabs.wiquery.core.javascript.JsScope;
-import org.odlabs.wiquery.ui.core.JsScopeUiEvent;
 
 public class Popup extends Panel {
+
+    public static final String CONTENT_ID = "content";
 
     public Popup(String id) {
         super(id);
         setOutputMarkupPlaceholderTag(true);
         add(new ContextImage("close", "images/x-black.gif").add(createClosePopupBehavior()));
-        add(createContent("content"));
+        add(createContent(CONTENT_ID));
         add(new AttributeAppender("class", Model.of("popup")," "));
     }
 
     public Behavior createClosePopupBehavior() {
-        return new WiQueryEventBehavior(new Event(MouseEvent.CLICK) {
-            @Override public JsScope callback() {
-                return JsScopeUiEvent.quickScope("$('#" + getMarkupId() + "').hide();");
+        return new AjaxEventBehavior("onclick") {
+            @Override protected void onEvent(AjaxRequestTarget target) {
+                Popup.this.setVisible(false);
+                target.add(Popup.this);
             }
-        });
+        };
     }
 
     protected WebMarkupContainer createContent(String id) {
@@ -37,8 +37,9 @@ public class Popup extends Panel {
     }
 
     public void setContent(Component component) {
-        Preconditions.checkArgument(component.getId().equals("content"));
+        Preconditions.checkArgument(component.getId().equals(CONTENT_ID));
         replace(component);
+        component.setOutputMarkupId(true);
     }
 
 }
