cf7a7608f01f33ad49d9279ffe167d88cece748a
Scott Usher <scott.usher@fieldid.com>
2013-08-19 19:01:43 +0000

    WEB-4041 - Oops found under My Account - Email Notifications

--- fid-web/src/main/java/com/n4systems/fieldid/validators/RFCEmailValidator.java
+++ fid-web/src/main/java/com/n4systems/fieldid/validators/RFCEmailValidator.java
@@ -4,20 +4,50 @@ import com.n4systems.api.validation.validators.EmailValidator;
 import com.opensymphony.xwork2.validator.ValidationException;
 import com.opensymphony.xwork2.validator.validators.FieldValidatorSupport;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RFCEmailValidator extends FieldValidatorSupport {
     @Override
     public void validate(Object object) throws ValidationException {
         String fieldName = getFieldName();
-        String value = null;
+        String val = null;
+        Object value = null;
+        List<String> values = new ArrayList<String>();
         try {
-            value = (String)this.getFieldValue(fieldName, object);
+            value = this.getFieldValue(fieldName, object);
+
+            if (value instanceof List) {
+                values = (List<String>)value;
+                validateMatch(values, fieldName, object);
+
+            } else {
+                val = (String)value;
+                validateMatch(val, fieldName, object);
+            }
+
+
         } catch ( ClassCastException e ) {
             // do nothing pass the null to the validator.  this way Longs can also use this validator.
         }
 
-        if((value !=null && !EmailValidator.EMAIL_REGEX.matcher(value).matches() && !value.isEmpty())) {
+
+    }
+
+    private void validateMatch(String value, String fieldName, Object object) {
+        if((!EmailValidator.EMAIL_REGEX.matcher(value).matches() && !value.isEmpty())) {
             addFieldError(fieldName, object);
             return;
         }
     }
+
+    private void validateMatch(List<String> values, String fieldName, Object object) {
+        for (String value: values) {
+            if((!EmailValidator.EMAIL_REGEX.matcher(value).matches() && !value.isEmpty())) {
+                addFieldError(fieldName, object);
+                return;
+            }
+        }
+    }
+
 }
