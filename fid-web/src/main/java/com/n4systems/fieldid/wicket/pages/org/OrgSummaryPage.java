5b393c26c2bfe190454553c300fce1f0b442958a
ddick <derek.dick@fieldid.com>
2013-11-08 14:23:34 -0500

    WEB-4290 : add merge,archive,delete menu items.

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/org/OrgSummaryPage.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/org/OrgSummaryPage.java
@@ -4,16 +4,20 @@ import com.google.common.collect.Lists;
 import com.n4systems.fieldid.service.org.PlaceService;
 import com.n4systems.fieldid.wicket.components.GoogleMap;
 import com.n4systems.fieldid.wicket.components.MultiSelectDropDownChoice;
+import com.n4systems.fieldid.wicket.components.NonWicketLink;
 import com.n4systems.fieldid.wicket.components.addressinfo.AddressPanel;
 import com.n4systems.fieldid.wicket.components.renderer.EventTypeChoiceRenderer;
 import com.n4systems.fieldid.wicket.model.EntityModel;
+import com.n4systems.fieldid.wicket.model.FIDLabelModel;
 import com.n4systems.fieldid.wicket.pages.FieldIDFrontEndPage;
+import com.n4systems.fieldid.wicket.pages.setup.org.OrgViewPage;
 import com.n4systems.fieldid.wicket.util.ProxyModel;
 import com.n4systems.model.Address;
 import com.n4systems.model.EventType;
 import com.n4systems.model.GpsLocation;
 import com.n4systems.model.builders.EventTypeBuilder;
 import com.n4systems.model.orgs.BaseOrg;
+import com.n4systems.model.orgs.CustomerOrg;
 import org.apache.wicket.Component;
 import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.ajax.AjaxRequestTarget;
@@ -41,6 +45,11 @@ import static ch.lambdaj.Lambda.on;
 // terminology note : you might also think of this as "places" page.  we might want to refactor code to have common words at some point.
 public class OrgSummaryPage extends FieldIDFrontEndPage {
 
+    interface FormPanel {
+        Component submit(AjaxRequestTarget target);
+        Component error(AjaxRequestTarget target);
+    }
+
     private static final String RIGHT_PANEL_ID = "right";
     private static final String LEFT_PANEL_ID = "left";
 
@@ -56,6 +65,8 @@ public class OrgSummaryPage extends FieldIDFrontEndPage {
     private Component left;
     private Component right;
     private GoogleMap map;
+    private Component deletePanel;
+    private Component archivePanel;
 
     public OrgSummaryPage(PageParameters params) {
         init(params.get("id").toLong());
@@ -88,6 +99,11 @@ public class OrgSummaryPage extends FieldIDFrontEndPage {
                         switchRightPanel(getEditPanel(), target);
                     }
                 })
+                .add(new AjaxLink("archive") {
+                    @Override public void onClick(AjaxRequestTarget target) {
+                        switchRightPanel(getArhcivePanel(), target);
+                    }
+                })
                 .add(new AjaxLink("recurring") {
                     @Override public void onClick(AjaxRequestTarget target) {
                         switchRightPanel(getEditRecurringPanel(), target);
@@ -96,6 +112,11 @@ public class OrgSummaryPage extends FieldIDFrontEndPage {
                 .add(new AjaxLink("eventTypes") {
                     @Override public void onClick(AjaxRequestTarget target) {
                         switchRightPanel(getEditEventTypesPanel(), target);
+                    }
+                })
+                .add( new NonWicketLink("merge", "mergeCustomers.action?uniqueID="+model.getObject().getId()) {
+                    @Override public boolean isVisible() {
+                        return model.getObject() instanceof CustomerOrg;
                     }
                 });
 
@@ -127,6 +148,13 @@ public class OrgSummaryPage extends FieldIDFrontEndPage {
                 });
 
         return actions.add(startMenu.setRenderBodyOnly(true),editMenu.setRenderBodyOnly(true),scheduleMenu.setRenderBodyOnly(true));
+    }
+
+    private Component getArhcivePanel() {
+        if (archivePanel==null) {
+            archivePanel = new ArchivePanel();
+        }
+        return archivePanel;
     }
 
     private Component getSummaryPanel() {
@@ -180,19 +208,19 @@ public class OrgSummaryPage extends FieldIDFrontEndPage {
 
     @Override
     public void renderHead(IHeaderResponse response) {
+        super.renderHead(response);
         // for starters, i am basing this page on asset summary page.  later on need to either refactor common css or create my own file.
         response.renderCSSReference("style/newCss/asset/asset.css");
     }
 
-
-    private Component createSubmitCancelButtons(String id) {
+    private Component createSubmitCancelButtons(String id, final FormPanel formPanel) {
         return new Fragment(id,"saveCancelButtons",OrgSummaryPage.this)
                 .add(new AjaxSubmitLink("submit") {
                     @Override protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-                        switchRightPanel(getSummaryPanel(),target);
+                        formPanel.submit(target);
                     }
                     @Override protected void onError(AjaxRequestTarget target, Form<?> form) {
-                        switchRightPanel(getSummaryPanel(),target);
+                        formPanel.submit(target);
                     }
                 })
                 .add(new AjaxLink("cancel") {
@@ -217,7 +245,7 @@ public class OrgSummaryPage extends FieldIDFrontEndPage {
     }
 
 
-    class EditPanel extends Fragment {
+    class EditPanel extends Fragment implements FormPanel {
         IModel<BaseOrg> model;
         Address address = new Address("111 queen st east, toronto");
 
@@ -225,7 +253,7 @@ public class OrgSummaryPage extends FieldIDFrontEndPage {
             super(RIGHT_PANEL_ID, "edit", OrgSummaryPage.this);
             add(new Form("form")
 //            add(new TextField("address", ProxyModel.of(model,on(BaseOrg.getLocation().getAddress()))));
-                .add(createSubmitCancelButtons("buttons"))
+                .add(createSubmitCancelButtons("buttons",this))
                 .add(new TextField("type", Model.of("commercial")))
                 .add(new TextField("status", Model.of("sold")))
                 .add(new TextField("name", Model.of("joe smith")))
@@ -234,16 +262,32 @@ public class OrgSummaryPage extends FieldIDFrontEndPage {
                 .add(new TextField("fax", Model.of("964 745 3528")))
                 .add(new AddressPanel("address", new PropertyModel(this,"address")).withExternalMap(map.getJsVar())));
         }
+
+        @Override public Component submit(AjaxRequestTarget target) {
+            return this;
+        }
+
+        @Override public Component error(AjaxRequestTarget target) {
+            return this;
+        }
     }
 
-    class SummaryPanel extends Fragment {
+    class SummaryPanel extends Fragment implements FormPanel {
         public SummaryPanel() {
             super(RIGHT_PANEL_ID, "summary", OrgSummaryPage.this);
             setOutputMarkupId(true);
         }
+
+        @Override public Component submit(AjaxRequestTarget target) {
+            return this;
+        }
+
+        @Override public Component error(AjaxRequestTarget target) {
+            return this;
+        }
     }
 
-    class EditEventTypesPanel extends Fragment {
+    class EditEventTypesPanel extends Fragment implements FormPanel {
         private List<EventType> types = Lists.newArrayList();
 
         public EditEventTypesPanel() {
@@ -252,21 +296,59 @@ public class OrgSummaryPage extends FieldIDFrontEndPage {
             add(new Form("form")
 // TODO DD                    .add(new MultiSelectDropDownChoice<EventType>("types", ProxyModel.of(model, on(BaseOrg.class).getEventTypes()), getEventTypes(), new EventTypeChoiceRenderer()))
                     .add(new MultiSelectDropDownChoice<EventType>("types", new PropertyModel<List<EventType>>(this, "types"), getEventTypes(), new EventTypeChoiceRenderer()))
-                    .add(createSubmitCancelButtons("buttons")));
+                    .add(createSubmitCancelButtons("buttons",this)));
         }
 
         private List<EventType> getEventTypes() {
             return placeService.getEventTypesFor(model.getObject());
         }
+        @Override public Component submit(AjaxRequestTarget target) {
+            // TODO DD : save event types
+            switchRightPanel(getSummaryPanel(),target);
+            return this;
+        }
+
+        @Override public Component error(AjaxRequestTarget target) {
+            return this;
+        }
     }
 
-    class EditRecurringPanel extends Fragment {
+    class EditRecurringPanel extends Fragment implements FormPanel {
         public EditRecurringPanel() {
             super(RIGHT_PANEL_ID, "editRecurringEvents", OrgSummaryPage.this);
             setOutputMarkupId(true);
             add(new Form("form")
-                    .add(createSubmitCancelButtons("buttons")));
+                    .add(createSubmitCancelButtons("buttons",this)));
         }
+        @Override public Component submit(AjaxRequestTarget target) {
+            // TODO : save recurring events stuff
+            switchRightPanel(getSummaryPanel(),target);
+            return this;
+        }
+
+        @Override public Component error(AjaxRequestTarget target) {
+            return this;
+        }
+    }
+
+    class ArchivePanel extends Fragment implements FormPanel {
+        public ArchivePanel() {
+            super(RIGHT_PANEL_ID,"archivePanel",OrgSummaryPage.this);
+            setOutputMarkupId(true);
+            add(new Form("form")
+                    .add(new Label("confirm", new FIDLabelModel("message.confirm_archive_place",model.getObject().getName())))
+                    .add(createSubmitCancelButtons("buttons",this)));
+        }
+        @Override public Component submit(AjaxRequestTarget target) {
+            // TODO DD: archive it.
+            setResponsePage(OrgViewPage.class);
+            return this;
+        }
+
+        @Override public Component error(AjaxRequestTarget target) {
+            return this;
+        }
+
     }
 
 }
