416db1065ef371caed665fa0dd2f8f34b1f85df2
dianalai <diana.lai@fieldid.com>
2013-12-10 15:13:02 -0500

    WEB-4353 Added picker to the events table open actions

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/components/org/events/table/OpenActionsCell.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/components/org/events/table/OpenActionsCell.java
@@ -6,8 +6,10 @@ import com.n4systems.fieldid.wicket.FieldIDSession;
 import com.n4systems.fieldid.wicket.components.NonWicketLink;
 import com.n4systems.fieldid.wicket.components.action.ActionDetailsPage;
 import com.n4systems.fieldid.wicket.components.modal.DialogModalWindow;
+import com.n4systems.fieldid.wicket.components.schedule.SchedulePicker;
 import com.n4systems.fieldid.wicket.model.EntityModel;
 import com.n4systems.fieldid.wicket.model.FIDLabelModel;
+import com.n4systems.fieldid.wicket.model.eventtype.EventTypesForPlaceModel;
 import com.n4systems.fieldid.wicket.model.navigation.PageParametersBuilder;
 import com.n4systems.fieldid.wicket.pages.FieldIDFrontEndPage;
 import com.n4systems.fieldid.wicket.pages.asset.AssetEventsPage;
@@ -15,14 +17,13 @@ import com.n4systems.fieldid.wicket.pages.event.CloseEventPage;
 import com.n4systems.model.CriteriaResult;
 import com.n4systems.model.PlaceEvent;
 import com.n4systems.model.ThingEvent;
+import com.n4systems.model.orgs.BaseOrg;
 import org.apache.wicket.AttributeModifier;
-import org.apache.wicket.Component;
 import org.apache.wicket.Page;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
-import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
@@ -34,40 +35,29 @@ public class OpenActionsCell extends Panel {
     @SpringBean private PersistenceService persistenceService;
 
     private ModalWindow modalWindow;
-    private Component/*SchedulePicker*/ schedulePicker;
+    private SchedulePicker<PlaceEvent> schedulePicker;
 
     public OpenActionsCell(String id, final IModel<PlaceEvent> eventModel, final Panel eventDisplayPanel) {
         super(id);
 
+        final PlaceEvent schedule = eventModel.getObject();
+
         add(modalWindow = createModalWindow(eventModel, eventDisplayPanel));
-/*
-        IModel<List<Event>> eventTypesModel = createEventTypesModelForEvent(eventModel);
-*/
-/*        add(schedulePicker = new SchedulePicker("schedulePickerWindow", eventModel, eventTypesModel, new EventJobsForTenantModel()) {
+
+        add(schedulePicker = new SchedulePicker<PlaceEvent>("schedulePickerWindow", eventModel, new EventTypesForPlaceModel(new PropertyModel<BaseOrg>(eventModel, "place"))) {
             { setSaveButtonLabel(new FIDLabelModel("label.save")); }
             @Override
             protected void onPickComplete(AjaxRequestTarget target) {
                 persistenceService.update(eventModel.getObject());
                 target.add(eventDisplayPanel);
             }
-        });*/
-        add(schedulePicker = new WebMarkupContainer("schedulePickerWindow").setVisible(false));
+        });
 
-                setVisible(FieldIDSession.get().getSessionUser().hasAccess("createevent") && FieldIDSession.get().getSessionUser().hasAccess("editevent"));
-        
-        final PlaceEvent schedule = eventModel.getObject();
-
-
-        // NOTE TO DIANA : this is now a PlaceEvent so no reference to asset will exist.
-        String startAction = "TO BE DONE";//"selectEventAdd.action?scheduleId=" + schedule.getId() + "&assetId=" + schedule.getAsset().getId() + "&type=" + schedule.getType().getId();
+        //TODO This should link to the perform event page
+        String startAction = "TO BE DONE";
         add(new NonWicketLink("startLink", startAction, new AttributeModifier("class", "btn-secondary")));
 
-        add(new Link("closeLink") {
-            @Override
-            public void onClick() {
-                setResponsePage(new CloseEventPage(PageParametersBuilder.uniqueId(schedule.getId()), (FieldIDFrontEndPage) getPage()));
-            }
-        });
+        add(new BookmarkablePageLink<CloseEventPage>("closeLink", CloseEventPage.class, PageParametersBuilder.uniqueId(schedule.getId())));
 
         add(new AjaxLink<Void>("deleteLink") {
             @Override
@@ -99,19 +89,12 @@ public class OpenActionsCell extends Panel {
         AjaxLink<Void> editLink = new AjaxLink<Void>("editLink") {
             @Override
             public void onClick(AjaxRequestTarget target) {
-                //schedulePicker.show(target);
+                schedulePicker.show(target);
             }
         };
+        editLink.setVisible(FieldIDSession.get().getSessionUser().hasAccess("createevent") && FieldIDSession.get().getSessionUser().hasAccess("editevent"));
         add(editLink);
     }
-
-/*    private IModel<List<ThingEventType>> createEventTypesModelForEvent(IModel<ThingEvent> eventModel) {
-        if (eventModel.getObject().getType() == null || !eventModel.getObject().getType().getGroup().isAction()) {
-            return new EventTypesForAssetTypeModel(new PropertyModel<AssetType>(eventModel, "asset.type"));
-        } else {
-            return new ActionTypesForTenantModel();
-        }
-    }*/
 
     private DialogModalWindow createModalWindow(final IModel<PlaceEvent> eventModel, final Panel eventDisplayPanel) {
         DialogModalWindow dialogWindow = new DialogModalWindow("modalWindow");
