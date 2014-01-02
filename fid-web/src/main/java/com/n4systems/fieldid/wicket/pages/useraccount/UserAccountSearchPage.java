1206293dc5233f6687aab6b3754403ba36ec387f
Scott Usher <scott.usher@fieldid.com>
2014-01-02 14:12:06 -0500

    WEB-4391 Page Styling - Forgot Username page

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/useraccount/UserAccountSearchPage.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/useraccount/UserAccountSearchPage.java
@@ -1,20 +1,34 @@
 package com.n4systems.fieldid.wicket.pages.useraccount;
 
+import com.n4systems.fieldid.service.amazon.S3Service;
 import com.n4systems.fieldid.service.user.SendForgotUserEmailService;
 import com.n4systems.fieldid.service.user.UserService;
+import com.n4systems.fieldid.wicket.components.ExternalImage;
+import com.n4systems.fieldid.wicket.components.feedback.FIDFeedbackPanel;
+import com.n4systems.fieldid.wicket.components.feedback.TopFeedbackPanel;
 import com.n4systems.fieldid.wicket.model.FIDLabelModel;
 import com.n4systems.fieldid.wicket.pages.FieldIDWicketPage;
 import com.n4systems.model.user.User;
+import com.n4systems.services.reporting.CriteriaTrendsResultCountRecord;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.IHeaderResponse;
+import org.apache.wicket.markup.html.WebComponent;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.RequiredTextField;
+import org.apache.wicket.markup.html.image.ContextImage;
+import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.validation.validator.EmailAddressValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.net.URI;
 import java.util.List;
@@ -23,8 +37,17 @@ public class UserAccountSearchPage extends FieldIDWicketPage {
 
     @SpringBean
     protected UserService userService;
+
     @SpringBean
     protected SendForgotUserEmailService sendForgotUserEmailService;
+
+    @SpringBean
+    private S3Service s3Service;
+
+    private FIDFeedbackPanel feedbackPanel;
+    private TopFeedbackPanel topFeedbackPanel;
+
+
 
     public UserAccountSearchPage() {
     }
@@ -32,16 +55,38 @@ public class UserAccountSearchPage extends FieldIDWicketPage {
     @Override
     protected void onInitialize() {
         super.onInitialize();
-        add(new FeedbackPanel("feedbackPanel"));
+
+        feedbackPanel = new FIDFeedbackPanel("feedbackPanel");
+        feedbackPanel.setOutputMarkupId(true);
+
+        topFeedbackPanel = new TopFeedbackPanel("topFeedbackPanel");
+        topFeedbackPanel.setOutputMarkupId(true);
+
+        add(feedbackPanel);
+        add(topFeedbackPanel);
+
         add(new GetUsernameForm("GetUsernameForm"));
+        add(new StaticImage("tenantLogo", new Model<String>(s3Service.getBrandingLogoURL().toString())).setEscapeModelStrings(false));
+        add(new ContextImage("password-icon", "/fieldid/images/password-icon.png"));
+        add(new ContextImage("logo-icon", "/fieldid/images/logo-icon.png"));
+
     }
 
     @Override
     public void renderHead(IHeaderResponse response) {
         super.renderHead(response);
 
-        response.renderCSSReference("pageStyles/login.css");
-        response.renderJavaScriptReference("jquery/watermark/jquery.watermark.js");
+        response.renderCSSReference("style/pageStyles/login.css");
+        response.renderCSSReference("newCss/layout/layout.css");
+        response.renderCSSReference("style/branding/default.css");
+
+        response.renderCSSReference("style/colorbox.css");
+        response.renderCSSReference("style/fieldid.css");
+        response.renderCSSReference("style/public.css");
+        response.renderCSSReference("style/reset.css");
+        response.renderCSSReference("style/site_wide.css");
+
+        response.renderCSSReference("style/jquery-redmond/jquery-ui-1.8.13.custom.css");
 
     }
 
@@ -59,6 +104,7 @@ public class UserAccountSearchPage extends FieldIDWicketPage {
         return URI.create(getServletRequest().getRequestURL().toString()).resolve(getServletRequest().getContextPath() + "/");
     }
 
+
     class GetUsernameForm extends Form {
 
         private String userEmail;
@@ -68,11 +114,27 @@ public class UserAccountSearchPage extends FieldIDWicketPage {
 
             RequiredTextField rField = new RequiredTextField<String>("userEmail", new PropertyModel<String>(this, "userEmail"));
             rField.setRequired(true);
-            rField.setLabel(new FIDLabelModel("label.useremail"));
             rField.add(EmailAddressValidator.getInstance());
             add(rField);
 
-            add(new Button("sendUsername"));
+            add(new AjaxSubmitLink("sendUsername") {
+                @Override
+                protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                    String email = getUserEmail();
+
+                    if (null != email && email.length() > 0) {
+                        sendUsername(email);
+                    }
+                    target.add(feedbackPanel, topFeedbackPanel);
+                }
+
+                @Override
+                protected void onError(AjaxRequestTarget target, Form<?> form) {
+                    target.add(feedbackPanel, topFeedbackPanel);
+                }
+            });
+
+
 
             add(new Link<Void>("cancel") {
                 @Override
@@ -82,15 +144,7 @@ public class UserAccountSearchPage extends FieldIDWicketPage {
             });
         }
 
-        @Override
-        protected void onSubmit() {
-            String email = getUserEmail();
 
-            if (null != email && email.length() > 0) {
-                sendUsername(email);
-            }
-
-        }
 
         public String getUserEmail() {
             return userEmail;
@@ -98,6 +152,18 @@ public class UserAccountSearchPage extends FieldIDWicketPage {
 
         public void setUserEmail(String userEmail) {
             this.userEmail = userEmail;
+        }
+    }
+    static class StaticImage extends WebComponent {
+        public StaticImage(String id, IModel<String> urlModel) {
+            super( id, urlModel );
+        }
+
+        @Override
+        protected void onComponentTag(ComponentTag tag) {
+            super.onComponentTag( tag );
+            checkComponentTag( tag, "img" );
+            tag.put( "src", getDefaultModelObjectAsString() );
         }
     }
 
