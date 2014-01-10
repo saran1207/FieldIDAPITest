9117e88aa64107c10f9a6358b6b4b8d79447ed4b
dianalai <diana.lai@fieldid.com>
2014-01-10 15:03:33 -0500

    Fix class loading problem

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/components/org/events/DueDateState.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/components/org/events/DueDateState.java
@@ -1,13 +1,12 @@
 package com.n4systems.fieldid.wicket.components.org.events;
 
 
-import com.n4systems.fieldid.wicket.model.FIDLabelModel;
 import com.n4systems.model.api.DisplayEnum;
 
 public enum DueDateState implements DisplayEnum
 {
-    UPCOMING(new FIDLabelModel("label.upcoming").getObject()),
-    OVERDUE(new FIDLabelModel("label.overdue").getObject());
+    UPCOMING("label.upcoming"),
+    OVERDUE("label.overdue");
 
     private String label;
 
