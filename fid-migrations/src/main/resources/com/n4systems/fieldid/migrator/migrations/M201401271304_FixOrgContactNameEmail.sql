83525391bc436b49fc6e78640ce94ffc00222cda
ddick <derek.dick@fieldid.com>
2014-01-27 15:35:24 -0500

    WEB-4538 : combine alter stmts into one.

--- fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201401271304_FixOrgContactNameEmail.sql
+++ fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201401271304_FixOrgContactNameEmail.sql
@@ -1,4 +1,3 @@
-alter table org_base modify contactemail varchar(255);
-alter table org_base modify contactname varchar(255);
+alter table org_base modify contactemail varchar(255), modify contactname varchar(255);
 update org_base ob join org_customer oc on ob.id = oc.org_id set ob.contactemail = oc.contactemail, ob.contactname = oc.contactname;
 
