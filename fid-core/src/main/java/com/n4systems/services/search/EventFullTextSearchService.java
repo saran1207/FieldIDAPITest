ca534ec3c8ec26fb00e588bbf93f1e8bf7978c02
dianalai <diana.lai@fieldid.com>
2014-02-27 14:19:38 -0500

    WEB-4398 Fixed secondary org filter

--- fid-core/src/main/java/com/n4systems/services/search/EventFullTextSearchService.java
+++ fid-core/src/main/java/com/n4systems/services/search/EventFullTextSearchService.java
@@ -2,11 +2,13 @@ package com.n4systems.services.search;
 
 import com.n4systems.model.Tenant;
 import com.n4systems.model.orgs.BaseOrg;
+import com.n4systems.services.search.field.AssetIndexField;
 import com.n4systems.services.search.field.EventIndexField;
 import com.n4systems.services.search.field.IndexField;
 import com.n4systems.services.search.writer.EventIndexWriter;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.MatchAllDocsQuery;
 import org.apache.lucene.search.NumericRangeQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -27,8 +29,14 @@ public class EventFullTextSearchService extends FullTextSearchService {
     @Override
     protected void addSecondaryOrgSecurity(BaseOrg owner, BooleanQuery securityQuery) {
         Long id = owner.getId();  // secondary can see primary, and this secondary
-        securityQuery.add(new BooleanClause(NumericRangeQuery.newLongRange("_secondaryOrgId", id, id, true, true), BooleanClause.Occur.SHOULD));
-        securityQuery.add(NumericRangeQuery.newLongRange(EventIndexField.SECONDARY_ID.getField(), 0L, null, true, true), BooleanClause.Occur.MUST_NOT);  // tricky way of saying "should be null"
+
+        BooleanQuery secondaryIsNullQuery = new BooleanQuery();
+
+        secondaryIsNullQuery.add(new MatchAllDocsQuery(), BooleanClause.Occur.MUST);
+        secondaryIsNullQuery.add(NumericRangeQuery.newLongRange(AssetIndexField.SECONDARY_ID.getField(), 0L, null, true, true), BooleanClause.Occur.MUST_NOT);
+
+        securityQuery.add(new BooleanClause(NumericRangeQuery.newLongRange(AssetIndexField.SECONDARY_ID.getField(), id, id, true, true), BooleanClause.Occur.SHOULD));
+        securityQuery.add(secondaryIsNullQuery, BooleanClause.Occur.SHOULD);
     }
 
     @Override
