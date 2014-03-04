a8655f695d885f1c32006f6f7a2798b627e62da3
Mark Frederiksen <mfrederiksen@gmail.com>
2014-03-04 17:38:16 -0500

    Final fix for googleMaps

--- fid-web/src/main/webapp/javascript/googleMaps.js
+++ fid-web/src/main/webapp/javascript/googleMaps.js
@@ -1,24 +1,24 @@
  /**
- * requires....
- *  <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+ * requires.... 
+ *  <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script> 
  *  <@n4.includeScript src="googleMaps.js"/>
- *
- * e.g.
- *
+ *  
+ * e.g. 
+ * 
  * simplest usage for a map with a single marker on it =
  *  googleMapFactory.createAndShowWithLocation('mapCanvas',44, -79)
- * ...given <div id='mapCanvas'/> exists and...
+ * ...given <div id='mapCanvas'/> exists and... 
  * 44/-79 is the latitude/longitude of the marker.
  * note that you MUST style the div with the desired height/width.  a map will not affect its containers size - it merely fills it up.
- *
+ * 
  * to add more markers and/or customize them...
- *
+ * 
  *  var map = googleMapFactory.create('mapCanvas');
  *	map.makeMarker = googleMapFactory.makeMarkerForStatus;     // factory to create custom markers based on status  (PASS status in this case).
  *  map.addLocation(44,-79, "hello show this in infowindow", "pass");   // content = "hello..." which pops up when user clicks on marker.  PASS = status
  *  map.show();
- *
- * make sure you don't call map.show() unless you know the element ('mapCanvas' in this case) has been parsed into the document.
+ * 
+ * make sure you don't call map.show() unless you know the element ('mapCanvas' in this case) has been parsed into the document.  
  * if not, delay the show call until onLoad().
  */
 
@@ -29,12 +29,32 @@ var googleMapFactory = (function() {
 		 mapTypeId:google.maps.MapTypeId.ROADMAP
 	 };
 
-	 var create = function(elementId) {
-		 if (!elementId) {
+	 var create = function(options, lat, lng) {
+		 // for legacy callers, needs to be fixed in next release
+		 if (typeof options === "string") {
+			options = {
+				id: options,
+				latitude: lat,
+				longitude: lng
+			}
+		 }
+		
+		 if (!options.id) {
 		 	throw "you must specify id for GoogleMap element";
 		 }
-
-		 return googleMap(elementId, defaultOptions);
+		 if (options.latitude && options.longitude) {
+			 options.center = new google.maps.LatLng(options.latitude,options.longitude);
+		 }
+		
+		 if (typeof options.zoom === "undefined") {
+			options.zoom = defaultOptions.zoom;
+		 }
+		 if (typeof options.mapTypeId === "undefined") {
+			options.mapTypeId = defaultOptions.mapTypeId;
+		  }
+		 
+		 
+		 return googleMap(options.id, options);
 	 };
 
 	 var createAndShow = function(opts) {
@@ -182,10 +202,10 @@ var googleMapFactory = (function() {
 
 
 	/**
-	 * map object returned by factory
-	 */
+	 * map object returned by factory 
+	 */	
 	function googleMap(mapId, opt) {
-
+		
 		/* private methods and properties */
 		var id = mapId;
 		var map = '';
@@ -196,29 +216,29 @@ var googleMapFactory = (function() {
 		var currBounds;
 		var currZoom;
 		var timeout;
-
+		
 		function showInfoWindow(marker, map) {
 			if (this.updateContentWithAddress && !marker.address) {
 				new google.maps.Geocoder().geocode( {'latLng': marker.position}, function(results, status) {
 					if (status== google.maps.GeocoderStatus.OK) {
-						marker.address = formatAddressString(results[0]);
-						marker.content = marker.content ? marker.content+'<br>' + marker.address : marker.address;
+						marker.address = formatAddressString(results[0]);						
+						marker.content = marker.content ? marker.content+'<br>' + marker.address : marker.address; 
 					}
 					showInfoWindowImpl(marker,map);
 				});
 			}
 			else if (marker.content) {
 				showInfoWindowImpl(marker,map);
-			}
+			}			
 		}
 
 		function showInfoWindowImpl(marker, map) {
 			if (infowindow) infowindow.close();
 			infowindow =  new google.maps.InfoWindow();
-			infowindow.setContent(marker.content);
+			infowindow.setContent(marker.content);	
 			infowindow.open(map,marker);
 		}
-
+		
 		function formatAddressString(address) {
 			var result = '';
 			if (address.address_components[0]) {
@@ -230,7 +250,7 @@ var googleMapFactory = (function() {
 			if (address.address_components[2]) {
 				result += ',' + address.address_components[2].short_name;
 			}
-			return result;
+			return result;   
 		}
 
 		function mapChanged() {
@@ -280,7 +300,7 @@ var googleMapFactory = (function() {
 		}
 
 		return {
-
+			
 			makeMarker : function(loc) {
 				var desc = loc.desc;
 				var marker = new google.maps.Marker({
@@ -295,7 +315,7 @@ var googleMapFactory = (function() {
 			addLocation : function(latitude,longitude,desc) {
 				var loc = new google.maps.LatLng(latitude,longitude);
 				if (desc) { loc.desc = desc; }
-				locations.push(loc);
+				locations.push(loc);  				
 				loc.args =  [].slice.call(arguments, 2); /* attach any additional args possibly passed to location. may be used by makeMarker() */
 				if (infowindow) infowindow.close();
 				return loc;
@@ -330,7 +350,7 @@ var googleMapFactory = (function() {
 					return;
 				}
 				var element = document.getElementById(id);
-				if (!element) {
+				if (!element) { 
 					throw "can't find element '" + id + "' for google map.";
 				}
 				map = new google.maps.Map(element, options);
@@ -371,7 +391,7 @@ var googleMapFactory = (function() {
 			}
 
 		};
-
+		
 	}
 
 	function prefixed(prefix,url) {
@@ -395,7 +415,7 @@ var googleMapFactory = (function() {
 				position: loc
 			});
 		}
-
+		
 		var icon = '';
 		if (status.toLowerCase()=='pass') {
 			icon = prefixed(prefix,'images/marker-images/greenMapIcon.png');
@@ -407,20 +427,20 @@ var googleMapFactory = (function() {
 				new google.maps.Size(32,32),
 				new google.maps.Point(0,0),
 				new google.maps.Point(16,32)
-		);
-
+		);		
+		
 		var shadow = new google.maps.MarkerImage(
 				  prefixed(prefix,'images/marker-images/mapIconShadow.png'),
 				  new google.maps.Size(52,32),
 				  new google.maps.Point(0,0),
 				  new google.maps.Point(16,32)
 				);
-
+		
 		var shape = {
 				  coord: [19,0,20,1,21,2,22,3,23,4,24,5,24,6,24,7,24,8,24,9,24,10,24,11,24,12,23,13,23,14,22,15,21,16,20,17,20,18,19,19,19,20,18,21,18,22,17,23,17,24,17,25,17,26,16,27,16,28,16,29,16,30,16,31,14,31,14,30,14,29,14,28,14,27,14,26,13,25,13,24,13,23,12,22,12,21,12,20,11,19,10,18,10,17,9,16,8,15,7,14,7,13,6,12,6,11,6,10,6,9,6,8,6,7,6,6,7,5,7,4,8,3,9,2,10,1,11,0,19,0],
 				  type: 'poly'
 				};
-
+		
 		var marker =  new google.maps.Marker({
 			  draggable: false,
 			  icon: image,
@@ -430,7 +450,7 @@ var googleMapFactory = (function() {
 			});
 
 		marker.content = content;
-		return marker;
+		return marker;		
 	};
 
 
@@ -499,17 +519,17 @@ var googleMapFactory = (function() {
 
 
 
-
-	return {
+		
+	return { 
 		create : create,
 		createAndShow : createAndShow,
 		makeMarkerForStatus : makeMarkerForStatus,
 		createAutoCompleteAddress : createAutoCompleteAddress
 	};
 
-
+	
 })();
 
 
 
-
+		
