4511903db79502c082c5edebd8c7ce08cc4e8e15
rrana <ruppi.rana@gmail.com>
2014-05-02 11:30:30 -0400

    WEB-4756 Adding null check when looking for primary and secondary unit of measure values.

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/components/event/criteria/view/UnitOfMeasureCriteriaResultPanel.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/components/event/criteria/view/UnitOfMeasureCriteriaResultPanel.java
@@ -12,13 +12,13 @@ public class UnitOfMeasureCriteriaResultPanel extends Panel{
         super(id);
 
         boolean hasPrimaryUnit = ((UnitOfMeasureCriteria) resultModel.getObject().getCriteria()).getPrimaryUnit() != null;
-        boolean hasPrimaryValue = !resultModel.getObject().getPrimaryValue().equals("");
+        boolean hasPrimaryValue = (resultModel.getObject().getPrimaryValue() != null) && !resultModel.getObject().getPrimaryValue().equals("");
 
         add(new Label("primaryUnit", new PropertyModel<String>(resultModel, "primaryValue")).setVisible(hasPrimaryUnit && hasPrimaryValue));
         add(new Label("primaryUnitLabel", new PropertyModel<String>(resultModel, "criteria.primaryUnit.name")).setVisible(hasPrimaryUnit && hasPrimaryValue));
 
         boolean hasSecondaryUnit = ((UnitOfMeasureCriteria) resultModel.getObject().getCriteria()).getSecondaryUnit() != null;
-        boolean hasSecondaryValue = !resultModel.getObject().getSecondaryValue().equals("");
+        boolean hasSecondaryValue = (resultModel.getObject().getSecondaryValue() != null) && !resultModel.getObject().getSecondaryValue().equals("");
 
         add(new Label("secondaryUnit", new PropertyModel<String>(resultModel, "secondaryValue")).setVisible(hasSecondaryUnit && hasSecondaryValue));
         add(new Label("secondaryUnitLabel", new PropertyModel<String>(resultModel, "criteria.secondaryUnit.name")).setVisible(hasSecondaryUnit && hasSecondaryValue));
