e0d41fdab72d281edf2dc3674620f301358d5ae1
dianalai <dl2587gi>
2014-05-14 14:17:20 -0400

    WEB-4776 Finished work on incomplete page

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/identify/LimitedEditAsset.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/identify/LimitedEditAsset.java
@@ -1,56 +1,83 @@
 package com.n4systems.fieldid.wicket.pages.identify;
 
 import com.n4systems.fieldid.service.PersistenceService;
-import com.n4systems.fieldid.wicket.components.org.AutoCompleteOrgPicker;
-import com.n4systems.fieldid.wicket.model.EntityModel;
+import com.n4systems.fieldid.wicket.components.org.OrgLocationPicker;
+import com.n4systems.fieldid.wicket.model.FIDLabelModel;
 import com.n4systems.fieldid.wicket.model.navigation.PageParametersBuilder;
-import com.n4systems.fieldid.wicket.pages.FieldIDFrontEndPage;
+import com.n4systems.fieldid.wicket.pages.FieldIDTemplatePage;
 import com.n4systems.fieldid.wicket.pages.asset.AssetSummaryPage;
 import com.n4systems.fieldid.wicket.pages.assetsearch.components.ModalLocationPicker;
 import com.n4systems.fieldid.wicket.util.ProxyModel;
 import com.n4systems.model.Asset;
+import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.SubmitLink;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import static ch.lambdaj.Lambda.on;
 
-public class LimitedEditAsset extends FieldIDFrontEndPage {
+public class LimitedEditAsset extends FieldIDTemplatePage {
 
     @SpringBean
     private PersistenceService persistenceService;
 
     public LimitedEditAsset(PageParameters params) {
         Long id = params.get("id").toLongObject();
+        Asset asset = persistenceService.find(Asset.class, id);
 
-        add(new EditAssetCustomerInformationForm("editCustomerInformationForm", new EntityModel<Asset>(Asset.class, id)));
+        add(new EditAssetCustomerInformationForm("editCustomerInformationForm", Model.of(asset)));
     }
 
+    @Override
+    protected Component createTitleLabel(String labelId) {
+        return new Label(labelId, new FIDLabelModel("label.edit_asset"));
+    }
+
+
     class EditAssetCustomerInformationForm extends Form<Asset> {
-        public EditAssetCustomerInformationForm(String id, IModel<Asset> model) {
+
+        private ModalLocationPicker locationPicker;
+        private OrgLocationPicker ownerPicker;
+
+        public EditAssetCustomerInformationForm(String id, final IModel<Asset> model) {
             super(id, model);
 
-            final ModalLocationPicker locationPicker = new ModalLocationPicker("locationPicker", ProxyModel.of(model, on(Asset.class).getAdvancedLocation()));
+            locationPicker = new ModalLocationPicker("locationPicker", ProxyModel.of(model, on(Asset.class).getAdvancedLocation()));
             add(locationPicker);
             locationPicker.setOwner(model.getObject().getOwner());
 
-            AutoCompleteOrgPicker ownerPicker = new AutoCompleteOrgPicker("ownerPicker", ProxyModel.of(model, on(Asset.class).getOwner())) {
-                { withAutoUpdate(true); }
-                @Override
-                protected void onUpdate(AjaxRequestTarget target, String hiddenInput, String fieldInput) {
-                    locationPicker.setOwner(getModelObject());
+            ownerPicker = new OrgLocationPicker("ownerPicker", new PropertyModel(model, "owner")) {
+                @Override protected void onChanged(AjaxRequestTarget target) {
+                    locationPicker.setOwner(getOwner());
+
+                    target.add(locationPicker);
                 }
-            };
-            add(ownerPicker);
 
-            add(new TextField<String>("referenceNumber", ProxyModel.of(model, on(Asset.class).getCustomerRefNumber())));
-            add(new TextField<String>("purchaseOrder", ProxyModel.of(model, on(Asset.class).getPurchaseOrder())));
+                @Override protected void onError(AjaxRequestTarget target, RuntimeException e) { }
+            }.withAutoUpdate();
+            add(ownerPicker.setRequired(true).setLabel(new FIDLabelModel("label.owner")));
 
+
+            add(new TextField<String>("referenceNumber", new PropertyModel<String>(model, "customerRefNumber")));
+            add(new TextField<String>("purchaseOrder", new PropertyModel<String>(model, "purchaseOrder")));
+
+            add(new SubmitLink("saveLink"));
             add(new BookmarkablePageLink<Void>("cancelLink", AssetSummaryPage.class, PageParametersBuilder.uniqueId(model.getObject().getId())));
+        }
+
+        @Override
+        protected void onSubmit() {
+            Asset asset = getModel().getObject();
+            persistenceService.update(asset);
+            setResponsePage(AssetSummaryPage.class, PageParametersBuilder.uniqueId(asset.getId()));
         }
     }
 
