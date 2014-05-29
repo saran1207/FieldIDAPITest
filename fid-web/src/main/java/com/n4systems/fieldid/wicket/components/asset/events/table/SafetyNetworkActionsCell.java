b89f660df585ddfb1ea9d76fc61326317ef15712
rrana <ruppi.rana@gmail.com>
2014-05-29 14:51:48 -0400

    WEB-4560 Fixed the following Safety Network Issues:  Reporting results, Reporting Results actions, Event Summary list, Event Summary actions, Traceability tab.

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/components/asset/events/table/SafetyNetworkActionsCell.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/components/asset/events/table/SafetyNetworkActionsCell.java
@@ -1,10 +1,9 @@
 package com.n4systems.fieldid.wicket.components.asset.events.table;
 
-import com.n4systems.fieldid.wicket.model.navigation.PageParametersBuilder;
-import com.n4systems.fieldid.wicket.pages.event.ThingEventSummaryPage;
+import com.n4systems.fieldid.wicket.components.NonWicketLink;
 import com.n4systems.model.ThingEvent;
 import com.n4systems.model.WorkflowState;
-import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 
@@ -19,7 +18,8 @@ public class SafetyNetworkActionsCell extends Panel {
 
         Long eventId = model.getObject().getId();
 
-        add(new BookmarkablePageLink<ThingEventSummaryPage>("viewLink", ThingEventSummaryPage.class, PageParametersBuilder.id(eventId)));
+        String viewAction = "event.action?uniqueID=" + eventId;
+        add(new NonWicketLink("viewLink", viewAction, new AttributeModifier("class", "mattButton")));
     }
 
 }
