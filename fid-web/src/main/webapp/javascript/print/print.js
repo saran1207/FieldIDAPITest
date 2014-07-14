835d687b586f25d549def824dec9cb1e943810d1
Dave Addison <dave.addison@fieldid.com>
2014-07-14 11:34:36 -0400

    WEB-4927 Updated styling and logic to control page breaks to LOTO print form

--- fid-web/src/main/webapp/javascript/print/print.js
+++ fid-web/src/main/webapp/javascript/print/print.js
@@ -1,32 +1,24 @@
 $( document ).ready(function() {
-	var page = $('<div class="print-form-page"></div>').appendTo('.print-form');
-	$('#source .data-container-header').clone().appendTo(page);
-	var currentDataContainer = $('<ul class="data-container"></ul>').appendTo(page);
+    var page = $('<div class="print-form-page"></div>').appendTo('body');
+    $('#source .data-container-header').clone().appendTo(page);
+    var currentDataContainer = $('<ul class="data-container"></ul>').appendTo(page);
 
-	var counter = 0;
 
-	$('.data-item').each( function( index, elem ){
-		var withElem = counter + $(elem).height();
+    $('.data-item').each( function( index, elem ){
+        $(elem).appendTo(currentDataContainer);
 
-		if( $(elem).hasClass('short-form-steps') ){
-			withElem = withElem + 10;
-		}
+        if( $(currentDataContainer).outerHeight(true) > 540 ){
+            $('#source .data-container-footer').clone().appendTo(page);
 
-		if( withElem < 500){
-			counter = withElem;
-			$(elem).appendTo(currentDataContainer);
+            page = $('<div class="print-form-page"></div>').appendTo('body');
+            $('#source .data-container-header').clone().appendTo(page);
+            currentDataContainer = $('<ul class="data-container"></ul>').appendTo(page);
 
-		} else {
-			counter = $(elem).height();
-			$('#source .data-container-footer').clone().appendTo(page);
-			
-			page = $('<div class="print-form-page"></div>').appendTo('.print-form');
-			$('#source .data-container-header').clone().appendTo(page);
-			currentDataContainer = $('<ul class="data-container"></ul>').appendTo(page);
-			
-			$(elem).appendTo(currentDataContainer);
-		}
-	});
-	$('#source .data-container-footer').clone().appendTo(page);
-	$('#source').hide();
+            $(elem).appendTo(currentDataContainer);
+        }
+    });
+    $('#source .data-container-footer').clone().appendTo(page);
+   // $('#source').hide();
+
+    $('.data-container').height(595);
 });
