1cbbec50504d5c1e3dc6c27886912446308dc574
Jordan Heath <jordan.heath@fieldid.com>
2014-07-28 16:34:23 -0400

    WEB-4866 The user is now confronted with a page allowing them to choose whether or not they want to proceed with unpublishing.  Proceeding with an unpublish action will result in all scheduled occurrences of the LOTO Procedure or Audits of said Procedure.  Once the schedules have been removed, the definition itself gets unpublished. As a last step, the user is navigated to the Previously Published Procedure Definition list.
    If the user clicks "Cancel" then they are immediately navigated back to the Published Procedure Definitions list page.

---
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/loto/UnpublishProcedureDefinitionPage.java
@@ -0,0 +1,50 @@
+package com.n4systems.fieldid.wicket.pages.loto;
+
+import com.n4systems.fieldid.service.procedure.ProcedureDefinitionService;
+import com.n4systems.fieldid.wicket.pages.FieldIDTemplatePage;
+import com.n4systems.model.procedure.ProcedureDefinition;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+/**
+ * This page allows the user to confirm whether or not they are prepared to archive all Procedures and Procedure Audit
+ * events related to the Procedure Definition being unpublished.
+ *
+ * Created by Jordan Heath on 28/07/14.
+ */
+public class UnpublishProcedureDefinitionPage extends FieldIDTemplatePage {
+
+    @SpringBean private ProcedureDefinitionService procedureDefinitionService;
+
+    private ProcedureDefinition procedureDefinition;
+
+    public UnpublishProcedureDefinitionPage(PageParameters pageParameters) {
+        super(pageParameters);
+
+        Long procedureDefinitionId = pageParameters.get("procedureDefinitionId").toLong();
+
+        procedureDefinition = procedureDefinitionService.getProcedureDefinitionById(procedureDefinitionId);
+
+        Form<Void> confirmUnpublishForm = new Form<Void>("confirmForm") {
+            @Override
+            protected void onSubmit() {
+                procedureDefinitionService.unpublishProcedureDefinition(procedureDefinition);
+                setResponsePage(new PreviouslyPublishedListAllPage());
+            }
+        };
+
+        confirmUnpublishForm.add(new Link("cancelLink") {
+            @Override
+            public void onClick() {
+                setResponsePage(new PublishedListAllPage());
+            }
+        });
+
+        add(confirmUnpublishForm);
+    }
+}
