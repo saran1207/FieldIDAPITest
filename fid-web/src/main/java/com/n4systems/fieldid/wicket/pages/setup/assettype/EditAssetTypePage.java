120fa192a75697adb814f60a7329c6effc4aa2f0
dianalai <dl2587gi>
2014-07-30 11:46:35 -0400

    WEB-4954 Added logic for hiding inspections related tabs

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/setup/assettype/EditAssetTypePage.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/setup/assettype/EditAssetTypePage.java
@@ -1,9 +1,13 @@
 package com.n4systems.fieldid.wicket.pages.setup.assettype;
 
+import com.google.common.collect.Lists;
 import com.n4systems.fieldid.wicket.components.navigation.NavigationBar;
+import com.n4systems.fieldid.wicket.model.navigation.NavigationItem;
 import com.n4systems.fieldid.wicket.model.navigation.PageParametersBuilder;
 import com.n4systems.model.AssetType;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import java.util.List;
 
 import static com.n4systems.fieldid.wicket.model.navigation.NavigationItemBuilder.aNavItem;
 
@@ -24,13 +28,18 @@ public class EditAssetTypePage extends AddAssetTypePage {
     @Override
     protected void addNavBar(String navBarId) {
         assetTypeId = assetType.getObject().getId();
-        add(new NavigationBar(navBarId,
-                aNavItem().label("nav.view_all").page(AssetTypeListPage.class).build(),
-                aNavItem().label("nav.edit").page(EditAssetTypePage.class).params(PageParametersBuilder.uniqueId(assetTypeId)).build(),
-                aNavItem().label("nav.event_type_associations").page(EventTypeAssociationsPage.class).params(PageParametersBuilder.uniqueId(assetTypeId)).build(),
-                aNavItem().label("nav.schedules").page(AssetTypeSchedulesPage.class).params(PageParametersBuilder.uniqueId(assetTypeId)).build(),
-                aNavItem().label("nav.add").page(AddAssetTypePage.class).onRight().build()
-        ));
+        List<NavigationItem> navItems = Lists.newArrayList();
+
+        navItems.add(aNavItem().label("nav.view_all").page(AssetTypeListPage.class).build());
+        navItems.add(aNavItem().label("nav.edit").page(EditAssetTypePage.class).params(PageParametersBuilder.uniqueId(assetTypeId)).build());
+
+        if(getTenant().getSettings().isInspectionsEnabled()) {
+            navItems.add(aNavItem().label("nav.event_type_associations").page(EventTypeAssociationsPage.class).params(PageParametersBuilder.uniqueId(assetTypeId)).build());
+            navItems.add(aNavItem().label("nav.schedules").page(AssetTypeSchedulesPage.class).params(PageParametersBuilder.uniqueId(assetTypeId)).build());
+        }
+        navItems.add(aNavItem().label("nav.add").page(AddAssetTypePage.class).onRight().build());
+
+        add(new NavigationBar(navBarId, navItems.toArray(new NavigationItem[0])));
     }
 
     @Override
