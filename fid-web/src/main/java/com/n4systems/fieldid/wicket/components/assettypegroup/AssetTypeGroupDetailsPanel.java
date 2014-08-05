543a2b551d648a0f22420f40d5d53fdea6076a79
tracyshi <tracy.shi@fieldid.com>
2014-08-05 10:38:00 -0400

    WEB-4967 Modified view asset type group page.

---
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/components/assettypegroup/AssetTypeGroupDetailsPanel.java
@@ -0,0 +1,19 @@
+package com.n4systems.fieldid.wicket.components.assettypegroup;
+
+import com.n4systems.model.AssetTypeGroup;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+
+/**
+ * Created by tracyshi on 2014-08-01.
+ */
+public class AssetTypeGroupDetailsPanel extends Panel {
+
+    public AssetTypeGroupDetailsPanel(String id, AssetTypeGroup assetTypeGroup) {
+        super(id);
+        add(new Label("name", new PropertyModel<String>(assetTypeGroup, "displayName")));
+    }
+
+}
--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/setup/assettypegroup/ViewAssetTypeGroupPage.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/setup/assettypegroup/ViewAssetTypeGroupPage.java
@@ -1,10 +1,91 @@
 package com.n4systems.fieldid.wicket.pages.setup.assettypegroup;
 
+import com.n4systems.fieldid.service.asset.AssetTypeGroupService;
+import com.n4systems.fieldid.service.asset.AssetTypeService;
+import com.n4systems.fieldid.wicket.FieldIDSession;
+import com.n4systems.fieldid.wicket.components.FlatLabel;
+import com.n4systems.fieldid.wicket.components.assettypegroup.AssetTypeGroupDetailsPanel;
+import com.n4systems.fieldid.wicket.components.assettypegroup.GroupAssetTypeListPanel;
+import com.n4systems.fieldid.wicket.components.navigation.NavigationBar;
+import com.n4systems.fieldid.wicket.model.DayDisplayModel;
+import com.n4systems.fieldid.wicket.model.FIDLabelModel;
+import com.n4systems.fieldid.wicket.model.navigation.PageParametersBuilder;
 import com.n4systems.fieldid.wicket.pages.FieldIDTemplatePage;
+import com.n4systems.fieldid.wicket.pages.setup.AssetsAndEventsPage;
+import com.n4systems.fieldid.wicket.pages.setup.assettype.EditAssetTypePage;
+import com.n4systems.model.AssetType;
+import com.n4systems.model.AssetTypeGroup;
+import org.apache.wicket.Component;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+import static com.n4systems.fieldid.wicket.model.navigation.NavigationItemBuilder.aNavItem;
 
 /**
  * Created by tracyshi on 2014-07-31.
  */
 public class ViewAssetTypeGroupPage extends FieldIDTemplatePage {
+
+    @SpringBean
+    private AssetTypeGroupService assetTypeGroupService;
+
+    private Long assetTypeGroupId;
+    private AssetTypeGroup assetTypeGroup;
+    private AssetTypeGroupDetailsPanel detailsPanel;
+    private GroupAssetTypeListPanel assetTypeListPanel;
+    private WebMarkupContainer noAssetTypesUnderGroup;
+
+    public ViewAssetTypeGroupPage(PageParameters params) {
+        assetTypeGroupId = params.get("uniqueID").toLong();
+        assetTypeGroup = assetTypeGroupService.getGroupById(assetTypeGroupId);
+        detailsPanel = new AssetTypeGroupDetailsPanel("assetTypeGroupDetailsPanel", assetTypeGroup);
+        add(detailsPanel);
+        detailsPanel.setOutputMarkupId(true);
+
+        assetTypeListPanel = new GroupAssetTypeListPanel("groupAssetTypeListPanel", assetTypeGroupId);
+        add(assetTypeListPanel);
+        noAssetTypesUnderGroup.setOutputMarkupPlaceholderTag(true);
+        noAssetTypesUnderGroup.setVisible(assetTypeListPanel.isEmpty());
+
+    }
+
+    @Override
+    protected void addNavBar(String navBarId) {
+        NavigationBar navBar = new NavigationBar(navBarId,
+                aNavItem().label("nav.view_all").page(AssetTypeGroupListPage.class).build(),
+                aNavItem().label("nav.view").page(ViewAssetTypeGroupPage.class).params(PageParametersBuilder.uniqueId(assetTypeGroupId)).build(),
+                aNavItem().label("nav.edit").page(EditAssetTypeGroupPage.class).build());
+        add(navBar);
+    }
+
+    @Override
+    protected Component createTitleLabel(String labelId) {
+        return new Label(labelId, new FIDLabelModel("title.view_asset_type_group"));
+    }
+
+    @Override
+    protected Component createBackToLink(String linkId, String linkLabelId) {
+        BookmarkablePageLink<Void> pageLink = new BookmarkablePageLink<Void>(linkId, AssetsAndEventsPage.class);
+        pageLink.add(new FlatLabel(linkLabelId, new FIDLabelModel("label.back_to_setup")));
+        return pageLink;
+    }
+
+
+
+
+
 
 }
