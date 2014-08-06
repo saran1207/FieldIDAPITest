890d53b33aeab89fb823e905566c60ffe8f3adc7
Ali R <ali.rezafard@inpwrd.com>
2014-08-06 16:13:47 -0400

    WEB-4985 Drop unused prooftest columns from thing_events and masterevents tables

---
+++ fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201408060321_RemoveProoftestColumns.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `thing_events` DROP COLUMN `peakloadduration`, DROP COLUMN `duration`, DROP COLUMN `peakload`, DROP COLUMN `prooftesttype`;
+ALTER TABLE `masterevents` DROP COLUMN `peakloadduration`, DROP COLUMN `duration`, DROP COLUMN `peakload`, DROP COLUMN `prooftesttype`;
