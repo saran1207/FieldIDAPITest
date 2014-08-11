56464fa5fcdf8c210c545ce57ec3ec234cb60645
dianalai <dl2587gi>
2014-08-11 14:52:24 -0400

    WEB-4969 Fixed last login column

--- fid-core/src/main/java/com/n4systems/fieldid/service/user/UserService.java
+++ fid-core/src/main/java/com/n4systems/fieldid/service/user/UserService.java
@@ -56,6 +56,11 @@ public class UserService extends FieldIdPersistenceService {
 
     public Long countUsers(UserListFilterCriteria criteria) {
         QueryBuilder<User> builder = createUserQueryBuilder(criteria);
+
+        if (!getCurrentUser().getGroups().isEmpty()) {
+            return Long.valueOf(new ArrayList<User>(ThreadLocalInteractionContext.getInstance().getVisibleUsers()).size());
+        }
+
         return persistenceService.count(builder);
     }
 
