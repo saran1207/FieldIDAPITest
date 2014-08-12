5646056e98b52dea76698c52537155d56a70f516
tracyshi <tracy.shi@fieldid.com>
2014-08-12 15:54:38 -0400

    WEB-4967 Added 'Edit' link text on view asset type group page.

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/components/assettypegroup/AssetTypeGroupDetailsPanel.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/components/assettypegroup/AssetTypeGroupDetailsPanel.java
@@ -1,7 +1,10 @@
 package com.n4systems.fieldid.wicket.components.assettypegroup;
 
+import com.n4systems.fieldid.wicket.model.navigation.PageParametersBuilder;
+import com.n4systems.fieldid.wicket.pages.setup.assettypegroup.EditAssetTypeGroupPage;
 import com.n4systems.model.AssetTypeGroup;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
@@ -13,6 +16,7 @@ public class AssetTypeGroupDetailsPanel extends Panel {
 
     public AssetTypeGroupDetailsPanel(String id, AssetTypeGroup assetTypeGroup) {
         super(id);
+        add(new BookmarkablePageLink<Void>("editLink", EditAssetTypeGroupPage.class, PageParametersBuilder.uniqueId(assetTypeGroup.getId())));
         add(new Label("name", new PropertyModel<String>(assetTypeGroup, "displayName")));
     }
 
