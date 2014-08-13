b4d8712d36b3a31bd0efa4514db28d021b87b8b1
tracyshi <tracy.shi@fieldid.com>
2014-08-13 10:37:27 -0400

    WEB-4967 Updated setup menu to link to the new wicket page.

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/FieldIDTemplatePage.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/FieldIDTemplatePage.java
@@ -27,6 +27,7 @@ import com.n4systems.fieldid.wicket.pages.search.AdvancedEventSearchPage;
 import com.n4systems.fieldid.wicket.pages.setup.*;
 import com.n4systems.fieldid.wicket.pages.setup.assetstatus.AssetStatusListAllPage;
 import com.n4systems.fieldid.wicket.pages.setup.assettype.AssetTypeListPage;
+import com.n4systems.fieldid.wicket.pages.setup.assettypegroup.AssetTypeGroupListPage;
 import com.n4systems.fieldid.wicket.pages.setup.columnlayout.ColumnsLayoutPage;
 import com.n4systems.fieldid.wicket.pages.setup.comment.CommentTemplateListPage;
 import com.n4systems.fieldid.wicket.pages.setup.eventbook.EventBooksListAllPage;
@@ -289,6 +290,7 @@ public class FieldIDTemplatePage extends FieldIDAuthenticatedPage implements UIC
         WebMarkupContainer container = new WebMarkupContainer("assetsEventsSubMenuContainer");
         container.add(new BookmarkablePageLink("eventStatusListLink", EventStatusListPage.class));
         container.add(new BookmarkablePageLink("eventBooksListLink", EventBooksListAllPage.class));
+        container.add(new BookmarkablePageLink("assetTypeGroupsListLink", AssetTypeGroupListPage.class));
         container.add(new BookmarkablePageLink("assetTypesList", AssetTypeListPage.class));
         container.add(new BookmarkablePageLink("assetStatusList", AssetStatusListAllPage.class));
         container.add(new BookmarkablePageLink("priorityCodeListLink", PriorityCodePage.class).setVisible(getSecurityGuard().isInspectionsEnabled()));
