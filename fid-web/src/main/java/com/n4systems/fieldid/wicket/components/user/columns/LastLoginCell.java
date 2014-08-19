3072cfa0e0c6431f2f9943bc30cc4324728ac681
dianalai <dl2587gi>
2014-08-19 10:31:18 -0400

    WEB-4969 Enabled sorting on last login

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/components/user/columns/LastLoginCell.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/components/user/columns/LastLoginCell.java
@@ -6,8 +6,9 @@ import com.n4systems.model.user.User;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 
+import java.util.Date;
 import java.util.TimeZone;
 
 public class LastLoginCell extends Panel {
@@ -15,7 +16,7 @@ public class LastLoginCell extends Panel {
     public LastLoginCell(String id, IModel<User> model) {
         super(id, model);
         TimeZone timeZone = FieldIDSession.get().getSessionUser().getTimeZone();
-        add(new Label("lastLogin", new DayDisplayModel(Model.of(model.getObject().getLastLogin())).includeTime().withTimeZone(timeZone)));
+        add(new Label("lastLogin", new DayDisplayModel(new PropertyModel<Date>(model, "lastLogin")).includeTime().withTimeZone(timeZone)));
     }
 
 }
