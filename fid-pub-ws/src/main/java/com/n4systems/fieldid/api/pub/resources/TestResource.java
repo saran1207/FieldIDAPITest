406084d1fbbb299f543ff5007e3937e012c4d24d
Kirill Ternovsky <kirillternovsky@kternovsky.local>
2014-09-19 15:01:49 -0400

    Remove tenant dependency

--- fid-pub-ws/src/main/java/com/n4systems/fieldid/api/pub/resources/TestResource.java
+++ fid-pub-ws/src/main/java/com/n4systems/fieldid/api/pub/resources/TestResource.java
@@ -1,7 +1,6 @@
 package com.n4systems.fieldid.api.pub.resources;
 
 import com.n4systems.fieldid.service.FieldIdPersistenceService;
-import com.n4systems.model.Tenant;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,8 +10,7 @@ public class TestResource extends FieldIdPersistenceService {
 
 	@GET
 	public String test() {
-		Tenant t = persistenceService.findNonSecure(Tenant.class, 15511493L);
-		return "Hello World: " + t.getName() + " V1";
+		return "Hello World";
 	}
 
 }
