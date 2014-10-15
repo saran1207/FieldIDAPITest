e4ea462a9e6b0661492aedebda753b380d3b8bc5
Kirill Ternovsky <kirillternovsky@kternovsky.local>
2014-10-15 13:50:19 -0400

    standardize pulling out serialized names

--- fid-pub-ws/src/main/java/com/n4systems/fieldid/api/pub/protocolbuffers/google/GoogleProtobufUtils.java
+++ fid-pub-ws/src/main/java/com/n4systems/fieldid/api/pub/protocolbuffers/google/GoogleProtobufUtils.java
@@ -1,7 +1,19 @@
 package com.n4systems.fieldid.api.pub.protocolbuffers.google;
 
-/**
- * Created by kirillternovsky on 2014-10-14.
- */
-public class GoogleProtobufUtils {
+import com.google.protobuf.Descriptors;
+
+import java.util.Map;
+
+public final class GoogleProtobufUtils {
+
+    private GoogleProtobufUtils() {}
+
+    public static String getSerializedFieldName(Descriptors.FieldDescriptor descriptor) {
+        for(Map.Entry<Descriptors.FieldDescriptor, Object> opt : descriptor.getOptions().getAllFields().entrySet()) {
+            if(opt.getKey().getName().equals("serialized_name"))
+                return (String)opt.getValue();
+        }
+
+        return descriptor.getName();
+    }
 }
