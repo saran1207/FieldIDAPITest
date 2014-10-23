f1a865c7720bfc79a964568847275e32e170b6be
Mark Frederiksen <mfrederiksen@gmail.com>
2014-10-23 14:42:21 -0400

    Added oauth token generation on create

--- fid-core/src/main/java/com/n4systems/model/security/KeyPair.java
+++ fid-core/src/main/java/com/n4systems/model/security/KeyPair.java
@@ -3,9 +3,22 @@ package com.n4systems.model.security;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.UUID;
 
 @Embeddable
 public class KeyPair implements Serializable {
+
+	public static KeyPair generate() {
+		KeyPair kp = new KeyPair();
+		kp.setSecret(generateKey());
+		kp.setKey(generateKey());
+		return kp;
+	}
+
+	private static String generateKey() {
+		return UUID.randomUUID().toString().replaceAll("-", "");
+	}
+
 	@Column(name="key", nullable = false, length = 32)
 	private String key;
 
@@ -26,5 +39,9 @@ public class KeyPair implements Serializable {
 
 	public void setKey(String key) {
 		this.key = key;
+	}
+
+	public boolean isSet() {
+		return (secret != null && key != null);
 	}
 }
