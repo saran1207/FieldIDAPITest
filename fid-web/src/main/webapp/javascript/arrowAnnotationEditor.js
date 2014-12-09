ae30fef5891067390d2b83524986e49c9a4422b4
Dave Addison <daddison@mlock.com>
2014-12-09 11:11:03 -0500

    WEB-5354 - Added server calls (wicketAjaxGet) for arrowAnnotationEditor.js + General code cleanup to make it more readable + Moved element checks that disable the editor the initialization function

--- fid-web/src/main/webapp/javascript/arrowAnnotationEditor.js
+++ fid-web/src/main/webapp/javascript/arrowAnnotationEditor.js
@@ -19,114 +19,144 @@
         arrow: {
             head: {'x':null, 'y':null},
             tail: {'x':null, 'y':null}
+        },
+        
+        style: {
+            size: 5,
+            color: '#ff0000',
         }
     };
     
     function Plugin(element, params){
-        this.editor = $.extend({}, defaults, params);
-        this.editor.arrow.element = null;
-        this.editor.arrow.size = 5;
-        this.editor.arrow.color = '#ff0000';
-        this.editor.index = 0;
-        this.editor.state = ['empty', 'headSelected', 'tailSelected'];
+        // Check for required elements withing the svg
+        this.annotations = element.getElementsByClassName('annotations')[0];
+        if(!this.annotations){
+            return;
+        }
 
+        this.pluginParams = $.extend({}, defaults, params);
+        
+        this.index = 0;
+        this.state = ['empty', 'headSelected', 'tailSelected'];
+
+        this.arrow = this.pluginParams.arrow;
+        this.arrow.element = null;
+        
+        this.style = this.pluginParams.style;
+        
         this.element = element;
         this.width   = $(element).width();
         this.height  = $(element).height();
-        
 
         // Check for existing arrow values
-        if( this.editor.arrow.head.x !== null &&
-            this.editor.arrow.head.y !== null &&
-            this.editor.arrow.tail.x !== null &&
-            this.editor.arrow.tail.y !== null ){
+        if( this.arrow.head.x !== null &&
+            this.arrow.head.y !== null &&
+            this.arrow.tail.x !== null &&
+            this.arrow.tail.y !== null ){
             
             // Update editor state
-            this.editor.index = this.editor.state.length -1;
+            this.index = this.state.length -1;
             this.renderArrow();
         }
         
-        // Set arrow head color (can't seem to limit the scope to the calling element, so using 'document')
-        var poly = this.element.getElementsByClassName('arrowAnnotationEditor-arrow-poly');
+        // Set arrow head color
+        var poly = this.element.getElementsByClassName('arrow-poly');
         for(var i = 0, len = poly.length; i < len; i++ ){
-            poly[i].setAttributeNS(null, 'fill', this.editor.arrow.color);
+            poly[i].setAttributeNS(null, 'fill', this.style.color);
         }
         
         $(element).on('mousemove', $.proxy(this.move,   this));
-        $(element).on('mousedown', $.proxy(this.action, this));
+        $(element).on('mousedown', $.proxy(this.click, this));
     };
 
+    Plugin.prototype.updateArrowOnServer = function(){
+        var url = new String(this.apiUrl) +
+            '&x='+  this.arrow.head.x +
+            '&y='+  this.arrow.head.y +
+            '&x2='+ this.arrow.tail.x +
+            '&y2='+ this.arrow.tail.y;
+        wicketAjaxGet(url, function(){}, function(){});
+    }    
+
+    // Prepare values for storing in the DB
+    Plugin.prototype.calculateX = function(value){
+        return value / this.width;
+    };
+    Plugin.prototype.calculateY = function(value){
+        return value / this.height;
+    };
+    
     // Track mouse move after the head point has been selected
     Plugin.prototype.move = function(event){
-        if( this.editor.state[ this.editor.index] === 'headSelected' ){
+        if( this.state[ this.index] === 'headSelected' ){
             // Store tail based on current mouse position
-            this.editor.arrow.tail.x = this.width  / event['offsetX'];
-            this.editor.arrow.tail.y = this.height / event['offsetY'];
+            this.arrow.tail.x = this.calculateX( event['offsetX'] );
+            this.arrow.tail.y = this.calculateY( event['offsetY'] );
 
-            if( this.editor.arrow.element !== null ){
-                this.editor.arrow.element.setAttributeNS(null, 'x2', event['offsetX']);
-                this.editor.arrow.element.setAttributeNS(null, 'y2', event['offsetY']);
+            if( this.arrow.element !== null ){
+                this.arrowAttribute('x2', event['offsetX']);
+                this.arrowAttribute('y2', event['offsetY']);
             }
         }
     };
     
-    // State machine for tracking (click) events
-    Plugin.prototype.action = function(event){
+    // State machine for tracking click events
+    Plugin.prototype.click = function(event){
 
         // Update editor state
-        this.editor.index = ++this.editor.index % this.editor.state.length;
+        this.index = ++this.index % this.state.length;
 
-        switch( this.editor.state[this.editor.index] ){
+        switch( this.state[this.index] ){
                 
             case 'empty':
                 // Clear internal data structure
-                this.editor.arrow.head.x = null;
-                this.editor.arrow.head.y = null;
-                this.editor.arrow.tail.x = null;
-                this.editor.arrow.tail.y = null;
+                this.arrow.head.x = null;
+                this.arrow.head.y = null;
+                this.arrow.tail.x = null;
+                this.arrow.tail.y = null;
 
                 // Clear annotations group (visual rendering)
-                var el = this.element.getElementsByClassName('annotations')[0];
-                if(el){
-                    $(el).empty();
-                }
+                $(this.annotations).empty();
                 break;
         
             case 'headSelected':
                 // Store head based on click position
-                this.editor.arrow.head.x = event['offsetX'] / this.width;
-                this.editor.arrow.head.y = event['offsetY'] / this.height;
-
+                this.arrow.head.x = this.calculateX( event['offsetX'] );
+                this.arrow.head.y = this.calculateY( event['offsetY'] );
+                
                 // Start the tail at the same location as the head (so it grows out from the head)
-                this.editor.arrow.tail.x = event['offsetX'] / this.width;
-                this.editor.arrow.tail.y = event['offsetY'] / this.height;
+                this.arrow.tail.x = this.calculateX( event['offsetX'] );
+                this.arrow.tail.y = this.calculateY( event['offsetY'] );
                 
                 this.renderArrow();
                 break;
 
             case 'tailSelected':
+                this.updateArrowOnServer();
                 break;
         }
     };
-
+    
+    // Helper to set attributesNS on the arrow element
+    Plugin.prototype.arrowAttribute = function(key, value){
+        this.arrow.element.setAttributeNS(null, key, value);
+    };
+    
     Plugin.prototype.renderArrow = function(){
-        var el = this.element.getElementsByClassName('annotations')[0];
-        if(el){
-            // Draw svg arrow based on arrow.head and tail
-            // Namedspaced functions are required for xml based svg (aka jquery wont work)
-            this.editor.arrow.element = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-            
-            this.editor.arrow.element.setAttributeNS(null, 'x1', this.width  * this.editor.arrow.head.x);
-            this.editor.arrow.element.setAttributeNS(null, 'y1', this.height * this.editor.arrow.head.y);
-            this.editor.arrow.element.setAttributeNS(null, 'x2', this.width  * this.editor.arrow.tail.x);
-            this.editor.arrow.element.setAttributeNS(null, 'y2', this.height * this.editor.arrow.tail.y);
-            this.editor.arrow.element.setAttributeNS(null, 'stroke', this.editor.arrow.color);
-            this.editor.arrow.element.setAttributeNS(null, 'stroke-width', this.editor.arrow.size + 'px');
-            this.editor.arrow.element.setAttributeNS(null, 'marker-start', 'url(#arrow)');
+        // Draw svg arrow based on arrow.head and tail
+        // Namedspaced functions are required for xml based svg (aka jquery wont work)
+        this.arrow.element = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+        
+        this.arrowAttribute('x1', this.width  * this.arrow.head.x);
+        this.arrowAttribute('y1', this.height * this.arrow.head.y);
+        this.arrowAttribute('x2', this.width  * this.arrow.tail.x);
+        this.arrowAttribute('y2', this.height * this.arrow.tail.y);
+        this.arrowAttribute('stroke',       this.style.color);
+        this.arrowAttribute('stroke-width', this.style.size + 'px');
+        this.arrowAttribute('marker-start', 'url(#arrow)');
 
-            // Append svg arrow to annotations group
-            el.appendChild(this.editor.arrow.element);
-        }
+        // Append svg arrow to annotations group
+        this.annotations.appendChild(this.arrow.element);
     };
     
     // Generic jquery plugin creation / destroy
