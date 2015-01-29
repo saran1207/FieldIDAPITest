c0afdfb210870edd2603e694ed2cc2105d3c4ce8
Mark Frederiksen <mfrederiksen@gmail.com>
2015-01-29 13:50:35 -0500

    Improved exception handling

--- fid-pub-ws/src/main/java/com/n4systems/fieldid/api/pub/filters/CatchAllExceptionMapper.java
+++ fid-pub-ws/src/main/java/com/n4systems/fieldid/api/pub/filters/CatchAllExceptionMapper.java
@@ -3,6 +3,7 @@ package com.n4systems.fieldid.api.pub.filters;
 import com.n4systems.fieldid.service.SecurityContextInitializer;
 import org.apache.log4j.Logger;
 
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -20,7 +21,12 @@ public class CatchAllExceptionMapper implements ExceptionMapper<Throwable> {
 		 */
 		SecurityContextInitializer.resetSecurityContext();
 
-		logger.error("Uncaught exception in webservice", exception);
-		return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.TEXT_PLAIN_TYPE).build();
+		logger.error("Exception in webservice", exception);
+
+		if (exception instanceof WebApplicationException) {
+			return ((WebApplicationException) exception).getResponse();
+		} else {
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.TEXT_PLAIN_TYPE).build();
+		}
 	}
 }
