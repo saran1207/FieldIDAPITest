de952b8602656bc82e762061e4f11b4e3875bede
Mark Frederiksen <mfrederiksen@gmail.com>
2015-02-05 14:16:42 -0500

    Added CORS filter to public API

---
+++ fid-pub-ws/src/main/java/com/n4systems/fieldid/api/pub/filters/CorsFilter.java
@@ -0,0 +1,27 @@
+package com.n4systems.fieldid.api.pub.filters;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+	@Override
+	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+			MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+			headers.add("Access-Control-Allow-Origin", "*");
+			headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+			headers.add("Access-Control-Allow-Headers",
+							"Content-Type, " +
+							"X-APPINFO-DEVICE, " +
+							"X-APPINFO-DEVICETYPE, " +
+							"X-APPINFO-PLATFORM, " +
+							"X-APPINFO-OSVERSION, " +
+							"X-APPINFO-APPVERSION, " +
+							"X-APPINFO-NOTES");
+	}
+}
