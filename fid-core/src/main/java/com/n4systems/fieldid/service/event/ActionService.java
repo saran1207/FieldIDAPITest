e49a690c2c72ccaf7c77658aa75f11ef3aabc82d
dianalai <dlai@mlock.com>
2015-04-22 12:12:33 -0400

    WEB-5588 merging missing change from 2015.2.0

--- fid-core/src/main/java/com/n4systems/fieldid/service/event/ActionService.java
+++ fid-core/src/main/java/com/n4systems/fieldid/service/event/ActionService.java
@@ -2,11 +2,17 @@ package com.n4systems.fieldid.service.event;
 
 import com.n4systems.fieldid.service.FieldIdPersistenceService;
 import com.n4systems.model.ActionEventType;
+import com.n4systems.model.Event;
+import com.n4systems.model.notification.AssigneeNotification;
 import com.n4systems.util.persistence.QueryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class ActionService extends FieldIdPersistenceService {
+
+    @Autowired
+    private NotifyEventAssigneeService notifyEventAssigneeService;
 
     public List<ActionEventType> getActionTypes() {
         QueryBuilder<ActionEventType> query = createUserSecurityBuilder(ActionEventType.class);
@@ -14,4 +20,15 @@ public class ActionService extends FieldIdPersistenceService {
         return persistenceService.findAll(query);
     }
 
+    public <T extends Event> T update(T actionEvent) {
+        if(actionEvent.isSendEmailOnUpdate() && actionEvent.getAssigneeOrDateUpdated()) {
+            if(!notifyEventAssigneeService.notificationExists(actionEvent)) {
+                AssigneeNotification assigneeNotification = new AssigneeNotification();
+                assigneeNotification.setEvent(actionEvent);
+                persistenceService.save(assigneeNotification);
+                actionEvent.setAssigneeNotification(assigneeNotification);
+            }
+        }
+        return persistenceService.update(actionEvent);
+    }
 }
