45d1b6189dbb502e046a9372f38e678542a34864
dianalai <dlai@mlock.com>
2015-05-12 10:23:48 -0400

    WEB-5601 Fixed test

--- fid-core/src/test/java/com/n4systems/util/mail/iCalAttachmentBuilderTest.java
+++ fid-core/src/test/java/com/n4systems/util/mail/iCalAttachmentBuilderTest.java
@@ -17,14 +17,17 @@ public class iCalAttachmentBuilderTest {
     public void testICalAttachmentBuilder() {
         ICalAttachmentBuilder builder = ICalAttachmentBuilder.anICalAttachment()
                 .withSubject("Work Items Assigned")
-                .withStartDate(date);
+                .withStartDate(date)
+                .withAllDayEvent(true);
 
         byte[] bytes = builder.build();
 
         String result = new String(bytes);
 
+        System.out.println(result);
+
         assertTrue(result.contains("SUMMARY;LANGUAGE=en-us:Work Items Assigned"));
-        assertTrue(result.contains("DTSTART:20150101T050000Z"));
+        assertTrue(result.contains("DTSTART;VALUE=DATE:20150101"));
     }
 
 }
