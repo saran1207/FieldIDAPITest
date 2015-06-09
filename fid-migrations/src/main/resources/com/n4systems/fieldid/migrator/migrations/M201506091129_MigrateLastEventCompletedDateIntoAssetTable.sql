41ec5090615c9e5fd1397bc3e0f4ac3943cd77d9
Jordan Heath <jordan.heath@fieldid.com>
2015-06-09 11:30:59 -0400

    WEB-5604 Changing the migration for last event completed date... the query was rubbish.

--- fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201506091129_MigrateLastEventCompletedDateIntoAssetTable.sql
+++ fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201506091129_MigrateLastEventCompletedDateIntoAssetTable.sql
@@ -1,10 +1,9 @@
 UPDATE assets
 INNER JOIN (
-SELECT DISTINCT a.id as asset_id, me.completedDate as completed_date
+SELECT DISTINCT a.id as asset_id, MAX(me.completedDate) as completed_date
 FROM assets a
 INNER JOIN thing_events te ON a.id = te.asset_id
 INNER JOIN masterevents me ON te.id = me.event_id
-GROUP BY a.id
-HAVING MAX(me.completedDate)) s
+GROUP BY a.id) s
 ON s.asset_id = assets.id
 SET assets.last_event_completed_date = s.completed_date;
