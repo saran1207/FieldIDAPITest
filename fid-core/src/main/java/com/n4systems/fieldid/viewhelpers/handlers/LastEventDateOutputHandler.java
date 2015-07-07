37438c861599ce69ff36da19c294cb4b315b7a9b
Jordan Heath <jordan.heath@fieldid.com>
2015-07-07 16:35:11 -0400

    WEB-5682 Proper fix for the poor localization of the Last Event Date field.  At first, I thought the issue was that I hadn't checked in code (I hadn't, it turns out), but this code would have simply moved the Localization issue to other controls, where the effect would have been doubling the offset of any given timezone when applied to the Last Event Date.  This fix adds a new WebOutputHandler for the Last Event Date field and sets that Output Handler via a Migration script.

---
+++ fid-core/src/main/java/com/n4systems/fieldid/viewhelpers/handlers/LastEventDateOutputHandler.java
@@ -0,0 +1,39 @@
+package com.n4systems.fieldid.viewhelpers.handlers;
+
+import com.n4systems.fieldid.service.download.TableGenerationContext;
+import com.n4systems.fieldid.service.download.WebOutputHandler;
+import com.n4systems.util.DateHelper;
+import com.n4systems.util.FieldIdDateFormatter;
+import com.n4systems.util.time.DateUtil;
+
+import java.util.Date;
+
+/**
+ * This WebOutputHandler is used to PROPERLY localize the Last Event Date column for Assets in the Asset Search.
+ *
+ * For some reason, localization isn't working as expected in the DateTimeHandler, so this will fix the issue without
+ * impacting the multitude of other places DateTimeHandler is used.
+ *
+ * Created by Jordan Heath on 2015-07-07.
+ */
+public class LastEventDateOutputHandler extends WebOutputHandler {
+    public LastEventDateOutputHandler(TableGenerationContext contextProvider) {
+        super(contextProvider);
+    }
+
+    @Override
+    public String handleWeb(Long entityId, Object value) {
+        String cellString = "";
+        if(value instanceof Date) {
+            value = DateHelper.localizeDate((Date) value, contextProvider.getTimeZone());
+            cellString = new FieldIdDateFormatter(((Date)value), contextProvider, true, !DateUtil.isMidnight((Date) value)).format();
+        }
+
+        return cellString;
+    }
+
+    @Override
+    public Object handleExcel(Long entityId, Object value) {
+        return value;
+    }
+}
