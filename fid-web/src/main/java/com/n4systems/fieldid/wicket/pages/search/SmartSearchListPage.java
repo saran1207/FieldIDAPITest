a48f20da4b524105f200eacd2a4b19ddee3bb134
rrana <ruppi.rana@gmail.com>
2015-07-23 15:41:26 -0400

    WEB-5709 Fixing the smart search to route to the asset summary page when the result is only a single asset.

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/search/SmartSearchListPage.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/search/SmartSearchListPage.java
@@ -1,16 +1,19 @@
 package com.n4systems.fieldid.wicket.pages.search;
 
+import com.n4systems.fieldid.service.asset.AssetService;
 import com.n4systems.fieldid.wicket.components.feedback.FIDFeedbackPanel;
 import com.n4systems.fieldid.wicket.components.search.results.SmartSearchListPanel;
 import com.n4systems.fieldid.wicket.data.SmartSearchDataProvider;
 import com.n4systems.fieldid.wicket.model.FIDLabelModel;
 import com.n4systems.fieldid.wicket.pages.FieldIDTemplatePage;
+import com.n4systems.fieldid.wicket.pages.asset.AssetSummaryPage;
 import com.n4systems.model.Asset;
 import org.apache.wicket.Component;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.markup.html.IHeaderResponse;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import java.util.List;
 
@@ -18,6 +21,9 @@ import java.util.List;
  * Created by rrana on 2015-06-22.
  */
 public class SmartSearchListPage extends FieldIDTemplatePage {
+
+    @SpringBean
+    private AssetService assetService;
 
     private SmartSearchListPanel smartSearchListPanel;
     private SmartSearchDataProvider dataProvider;
@@ -39,6 +45,11 @@ public class SmartSearchListPage extends FieldIDTemplatePage {
     public SmartSearchListPage(PageParameters params) {
         super();
         this.searchTerm = params.get("searchTerm").toString();
+
+        if(assetService.findExactAssetSizeByIdentifiersForNewSmartSearch(searchTerm) == 1) {
+            Asset smartSearchAsset = assetService.findExactAssetByIdentifiersForNewSmartSearch(searchTerm).get(0);
+            setResponsePage(new AssetSummaryPage(smartSearchAsset));
+        }
     }
 
     @Override
