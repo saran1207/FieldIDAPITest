62e4ba8af6181a7d2c392fad3ebc5885336d8647
Kirill Ternovsky <kirill.ternovsky@fieldid.com>
2015-08-29 12:06:09 -0400

    Add type filter on "latest org" query
    This allows the front-end to be a little dumber with data processing, as there
    is no longer a possibility of inserting an org before its parent is in the local
    DB, which means we no longer need to cache a whoel ton of orgs (Intelligrated,
    I'm looking at you) in memory before being able to insert.
    
    Also, thanks Jordan!

--- fid-mobile-ws/src/main/java/com/n4systems/fieldid/ws/v2/resources/setupdata/org/ApiOrgResource.java
+++ fid-mobile-ws/src/main/java/com/n4systems/fieldid/ws/v2/resources/setupdata/org/ApiOrgResource.java
@@ -2,17 +2,34 @@ package com.n4systems.fieldid.ws.v2.resources.setupdata.org;
 
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.n4systems.fieldid.service.amazon.S3Service;
+import com.n4systems.fieldid.ws.v2.resources.ApiModelHeader;
+import com.n4systems.fieldid.ws.v2.resources.model.DateParam;
 import com.n4systems.fieldid.ws.v2.resources.setupdata.SetupDataResourceReadOnly;
 import com.n4systems.model.AddressInfo;
 import com.n4systems.model.orgs.BaseOrg;
+import com.n4systems.model.orgs.PrimaryOrg;
+import com.n4systems.model.orgs.SecondaryOrg;
+import com.n4systems.model.orgs.CustomerOrg;
+import com.n4systems.model.orgs.DivisionOrg;
+import com.n4systems.util.persistence.NewObjectSelect;
+import com.n4systems.util.persistence.QueryBuilder;
+import com.n4systems.util.persistence.WhereClauseFactory;
+import com.n4systems.util.persistence.WhereParameter;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -21,12 +38,53 @@ import java.util.regex.Pattern;
 @Path("org")
 public class ApiOrgResource extends SetupDataResourceReadOnly<ApiOrg, BaseOrg> {
 	private static Logger logger = Logger.getLogger(ApiOrgResource.class);
+	/* Not sure if it's better to make the super's field protected */
+	private final String idField;
 
     @Autowired
     private S3Service s3Service;
 
 	public ApiOrgResource() {
 		super(BaseOrg.class, true);
+		this.idField = "id";
+	}
+
+	@GET
+	@Path("query/latest/{type}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Transactional(readOnly = true)
+	public List<ApiModelHeader> queryLatestByType(@PathParam("type") String type, @QueryParam("since") DateParam since) {
+		Class<?> targetClass;
+
+		switch(type)
+		{
+			case "primary":
+				targetClass = PrimaryOrg.class;
+				break;
+			case "secondary":
+				targetClass = SecondaryOrg.class;
+				break;
+			case "customer":
+				targetClass = CustomerOrg.class;
+				break;
+			case "division":
+				targetClass = DivisionOrg.class;
+				break;
+			default:
+				return new ArrayList<ApiModelHeader>();
+		}
+
+		QueryBuilder<ApiModelHeader> query = new QueryBuilder<>(targetClass, securityContext.getUserSecurityFilter(true));
+		query.setSelectArgument(new NewObjectSelect(ApiModelHeader.class, "id", "modified"));
+
+		if(since != null) {
+			query.addWhere(WhereClauseFactory.create(WhereParameter.Comparator.GT, "modified", since));
+		}
+
+		addTermsToLatestQuery(query);
+
+		List<ApiModelHeader> results = persistenceService.findAll(query);
+		return results;
 	}
 
 	@Override
