e15af4c2c4e645d3441f23e8ba1399aeca12f686
dianalai <dlai@mlock.com>
2015-10-08 16:56:07 -0400

    WEB-5840 Added logic for searching assets with or without procedure definitions

---
+++ fid-core/src/main/java/com/n4systems/util/persistence/search/terms/SubSelectNotInTerm.java
@@ -0,0 +1,22 @@
+package com.n4systems.util.persistence.search.terms;
+
+import com.n4systems.util.persistence.QueryBuilder;
+import com.n4systems.util.persistence.SubSelectInClause;
+import com.n4systems.util.persistence.SubSelectNotInClause;
+import com.n4systems.util.persistence.WhereClause;
+
+public class SubSelectNotInTerm extends SingleTermDefiner {
+
+    String value;
+    QueryBuilder subQuery;
+
+    public SubSelectNotInTerm(String value, QueryBuilder subQuery) {
+        this.value = value;
+        this.subQuery = subQuery;
+    }
+
+    @Override
+    protected WhereClause<?> getWhereParameter() {
+        return new SubSelectNotInClause("id", subQuery);
+    }
+}
