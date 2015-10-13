bd7126f12d8706235bd7cb99c9f116ed39d19659
dianalai <dlai@mlock.com>
2015-10-13 11:03:26 -0400

    WEB-5844 Changed choice renderer for performed by drop down

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/massevent/MultiEventPage.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/massevent/MultiEventPage.java
@@ -425,7 +425,19 @@ public abstract class MultiEventPage<T extends Event> extends FieldIDTemplatePag
 
     protected void createEventTypeDetailsSection(IModel<T> event, OuterEventForm form) {
         PropertyModel<User> performedByModel = new PropertyModel<>(event, "performedBy");
-        DropDownChoice<User> performedBy = new FidDropDownChoice<>("performedBy", performedByModel, new ExaminersModel(performedByModel), new ListableChoiceRenderer<>());
+        //We should be using new ListableChoiceRenderer<User>() here but there is seems to be a hibernate proxy problem :(
+        DropDownChoice<User> performedBy = new FidDropDownChoice<>("performedBy", performedByModel, new ExaminersModel(performedByModel), new IChoiceRenderer<User>() {
+            @Override
+            public Object getDisplayValue(User object) {
+                return object.getDisplayName();
+            }
+
+            @Override
+            public String getIdValue(User object, int index) {
+                return object.getID() + "";
+            }
+        });
+
         DateTimePicker datePerformedPicker = new DateTimePicker("datePerformed", new UserToUTCDateModel(new PropertyModel<>(event, "date")), true).withNoAllDayCheckbox();
         datePerformedPicker.addToDateField(createUpdateAutoschedulesOnChangeBehavior());
 
