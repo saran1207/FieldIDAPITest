1e00238b77555f0cbf6693bd5b7cab19aa354a6c
rrana <ruppi.rana@gmail.com>
2016-01-28 17:21:28 -0500

    WEB-5986 Adding icons to show the schedules for asset types.

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/components/assettype/AssetTypeListPanel.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/components/assettype/AssetTypeListPanel.java
@@ -2,13 +2,15 @@ package com.n4systems.fieldid.wicket.components.assettype;
 
 import com.n4systems.fieldid.service.asset.AssetTypeService;
 import com.n4systems.fieldid.wicket.FieldIDSession;
-import com.n4systems.fieldid.wicket.components.NonWicketLink;
 import com.n4systems.fieldid.wicket.model.DayDisplayModel;
 import com.n4systems.fieldid.wicket.model.navigation.PageParametersBuilder;
 import com.n4systems.fieldid.wicket.pages.setup.assettype.CopyAssetTypePage;
 import com.n4systems.fieldid.wicket.pages.setup.assettype.EditAssetTypePage;
 import com.n4systems.fieldid.wicket.pages.setup.assettypegroup.ViewAssetTypeGroupPage;
 import com.n4systems.model.AssetType;
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.Link;
@@ -16,6 +18,7 @@ import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
@@ -60,6 +63,24 @@ public class AssetTypeListPanel extends Panel {
                 item.add(new Label("createdDate", new DayDisplayModel(new PropertyModel<Date>(assetType, "created"), true, timeZone)));
                 item.add(new Label("modifiedBy", new PropertyModel<String>(assetType, "modifiedBy.displayName")).setVisible(assetType.getModifiedBy() != null));
                 item.add(new Label("modifiedDate", new DayDisplayModel(new PropertyModel<Date>(assetType, "modified"), true, timeZone)));
+
+                int recurringSchedules = assetTypeService.getRecurringEvents(assetType).size();
+                int triggeredSchedules = assetType.getSchedules().size();
+
+                WebMarkupContainer triggerIcon = new WebMarkupContainer("triggerIcon");
+                triggerIcon.setOutputMarkupId(true);
+                triggerIcon.add(new AttributeAppender("class", new Model<String>("icon-flow-cascade"), ""));
+                triggerIcon.add(new AttributeModifier("title", true, new Model<String>(Integer.toString(triggeredSchedules) + " tirggered schedules.")));
+                triggerIcon.setVisible(triggeredSchedules > 0);
+                item.add(triggerIcon);
+
+                WebMarkupContainer recurringIcon = new WebMarkupContainer("recurringIcon");
+                recurringIcon.setOutputMarkupId(true);
+                recurringIcon.add(new AttributeAppender("class", new Model<String>("icon-arrows-cw"), ""));
+                recurringIcon.add(new AttributeModifier("title", true, new Model<String>(Integer.toString(recurringSchedules) + " recurring schedules.")));
+                recurringIcon.setVisible(recurringSchedules > 0);
+                item.add(recurringIcon);
+
                 item.add(new BookmarkablePageLink<Void>("edit", EditAssetTypePage.class, PageParametersBuilder.uniqueId(assetType.getId())));
                 item.add(new BookmarkablePageLink<Void>("copy", CopyAssetTypePage.class, PageParametersBuilder.uniqueId(assetType.getId())));
             }
