f4767e0d2e2b7475cde43c46cdb2280cc6c5ac37
dianalai <dlai@mlock.com>
2016-03-09 11:11:09 -0500

    WEB-6114 Fixed permission for displaying Limited Asset Edit link

--- fid-web/src/main/java/com/n4systems/fieldid/permissions/UserSecurityGuard.java
+++ fid-web/src/main/java/com/n4systems/fieldid/permissions/UserSecurityGuard.java
@@ -5,12 +5,13 @@ public interface UserSecurityGuard {
 	//Inspection Permissions
 	public boolean isAllowedTag();
 	public boolean isAllowedManageSystemConfig();
+	public boolean isAllowedManageSystemUsers();
 	public boolean isAllowedManageEndUsers();
 	public boolean isAllowedCreateEvent();
 	public boolean isAllowedEditEvent();
 	public boolean isAllowedManageJobs();
 	public boolean isAllowedManageSafetyNetwork();
-	public boolean isAllowedManageSystemUsers();
+	public boolean isAllowedEditAssetDetails();
 
 	//Loto Permissions
 	public boolean isAllowedAuthorEditProcedure();
