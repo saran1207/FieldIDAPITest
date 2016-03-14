bba4b3dd51e7b449672b693e22aba1006d9159bc
dianalai <dlai@mlock.com>
2016-03-14 16:13:10 -0400

    WEB-5733 Completed Button Panel

--- fid-core/src/main/java/com/n4systems/fieldid/service/event/ButtonGroupService.java
+++ fid-core/src/main/java/com/n4systems/fieldid/service/event/ButtonGroupService.java
@@ -18,7 +18,14 @@ public class ButtonGroupService extends CrudService<ButtonGroup>{
         return persistenceService.isUniqueName(ButtonGroup.class, name, null);
     }
 
-    public void touchEventTypes(ButtonGroup buttonGroup) {
+    @Override
+    public ButtonGroup update(ButtonGroup buttonGroup) {
+        touchEventTypes(buttonGroup);
+        return super.update(buttonGroup);
+    }
+
+    //When button groups are updated we need to update the event types so that the updated groups get synced
+    private void touchEventTypes(ButtonGroup buttonGroup) {
         String queryString = "SELECT DISTINCT et FROM " + EventType.class.getName() + " et INNER JOIN eventforms ef ON et.eventform_id = ef.id\n" +
                 "                                   INNER JOIN eventforms_criteriasections efcs on ef.id = efcs.eventform_id\n" +
                 "                                   INNER JOIN criteriasections_criteria csc ON efcs.sections_id = csc.criteriasections_id\n" +
