70a04b55a616225ea4c730def7c3d93dde8865ab
Mark Frederiksen <mfrederiksen@gmail.com>
2016-05-12 18:19:06 -0400

    Fixed broken unit test

--- fid-core/src/test/java/com/n4systems/util/persistence/SubSelectInClauseTest.java
+++ fid-core/src/test/java/com/n4systems/util/persistence/SubSelectInClauseTest.java
@@ -1,15 +1,16 @@
 package com.n4systems.util.persistence;
-import static org.junit.Assert.*;
-
-import org.junit.Test;
 
 import com.n4systems.exceptions.InvalidQueryException;
+import com.n4systems.model.api.UnsecuredEntity;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class SubSelectInClauseTest {
 	
 	@Test
 	public void test_generated_query() {
-		QueryBuilder<Long> subQuery = new QueryBuilder<Long>(null) {
+		QueryBuilder<Long> subQuery = new QueryBuilder<Long>(UnsecuredEntity.class) {
 			@Override
 			public String getQueryString() throws InvalidQueryException {
 				return "sub_query";
