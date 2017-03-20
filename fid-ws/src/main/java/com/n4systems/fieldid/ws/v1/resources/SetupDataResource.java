53e890c4a5087f5a5d226dd182882732e118c711
dianalai <dlai@mlock.com>
2017-03-20 10:04:52 -0400

    Added time logging for sync resources

--- fid-ws/src/main/java/com/n4systems/fieldid/ws/v1/resources/SetupDataResource.java
+++ fid-ws/src/main/java/com/n4systems/fieldid/ws/v1/resources/SetupDataResource.java
@@ -1,5 +1,7 @@
 package com.n4systems.fieldid.ws.v1.resources;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Date;
 import java.util.List;
 
@@ -12,6 +14,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,6 +28,9 @@ import com.n4systems.util.persistence.WhereParameter.Comparator;
 
 @Component
 public abstract class SetupDataResource<A, E extends AbstractEntity> extends ApiResource<A, E> {
+
+	private static Logger logger = Logger.getLogger(SetupDataResource.class);
+
 	private final Class<E> entityClass;
 	private final boolean allowArchived;
 	
@@ -67,10 +73,20 @@ public abstract class SetupDataResource<A, E extends AbstractEntity> extends Api
 
 	protected ListResponse<A> getApiPage(DateParam after, int page, int pageSize) {
 		QueryBuilder<E> builder = createFindAllBuilder(after);
+		Instant b = Instant.now();
 		List<E> entityModels = persistenceService.findAll(builder, page, pageSize);
-		Long total = persistenceService.count(builder);
+		Instant a = Instant.now();
+		logger.info("List Query: " + Duration.between(b,a).toNanos());
 
+		Instant b1 = Instant.now();
+		Long total = persistenceService.count(builder);
+		Instant a1 = Instant.now();
+		logger.info("Count Query: " + Duration.between(b1,a1).toNanos());
+
+		Instant b2 = Instant.now();
 		List<A> apiModels = convertAllEntitiesToApiModels(entityModels);
+		Instant a2 = Instant.now();
+		logger.info("Convert Entities: " + Duration.between(b2,a2).toNanos());
 		return new ListResponse<A>(apiModels, page, pageSize, total);
 	}
 
