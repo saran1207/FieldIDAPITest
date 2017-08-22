7539cd7a3915ce1ba226c42b7175cab3cbc65856
agrabovskis <arvid.grabovskis@ecompliance.com>
2017-08-22 13:52:12 -0400

    WEB-6528 - remove drop primary key from sql migration since it doesn't exist in higher environments.

--- fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201708081541_ChangePrimaryKey_masterevents_subevents.sql
+++ fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201708081541_ChangePrimaryKey_masterevents_subevents.sql
@@ -1,4 +1,3 @@
-ALTER TABLE masterevents_subevents DROP PRIMARY KEY, ADD PRIMARY KEY(masterevents_event_id, subevents_event_id, orderidx);
 ALTER TABLE masterevents_subevents DROP FOREIGN KEY masterevents_subevents_ibfk_2;
 ALTER TABLE masterevents_subevents DROP INDEX index_masterevents_subevents_on_subevents_event;
 ALTER TABLE masterevents_subevents ADD CONSTRAINT masterevents_subevents_ibfk_2 FOREIGN KEY (subevents_event_id) REFERENCES events(id) ON DELETE NO ACTION ON UPDATE NO ACTION;
