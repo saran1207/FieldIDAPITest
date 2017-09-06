3cce32ec29ffecb95a7380d5a67f603c5d4635af
agrabovskis <arvid.grabovskis@ecompliance.com>
2017-09-06 15:28:45 -0400

    WEB-5734 Convert Start Event page from Struts to Wicket

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/event/StartEventPage.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/event/StartEventPage.java
@@ -1,9 +1,11 @@
 package com.n4systems.fieldid.wicket.pages.event;
 
+import com.n4systems.fieldid.permissions.UserPermissionFilter;
 import com.n4systems.fieldid.wicket.model.FIDLabelModel;
 import com.n4systems.fieldid.wicket.model.navigation.PageParametersBuilder;
 import com.n4systems.fieldid.wicket.pages.FieldIDFrontEndPage;
 import com.n4systems.fieldid.wicket.pages.search.SmartSearchListPage;
+import com.n4systems.security.Permissions;
 import com.n4systems.services.config.ConfigService;
 import com.n4systems.util.ConfigEntry;
 import com.n4systems.util.ConfigurationProvider;
@@ -16,7 +18,7 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-
+@UserPermissionFilter(userRequiresOneOf={Permissions.CREATE_EVENT, Permissions.EDIT_EVENT})
 public class StartEventPage extends FieldIDFrontEndPage {
 
     public StartEventPage(PageParameters params) {
