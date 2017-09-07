00e05f3654228943067d2c6ff952a3ddc9668821
agrabovskis <arvid.grabovskis@ecompliance.com>
2017-09-07 15:47:56 -0400

    WEB-6633 Create Wicket version of the Struts Oops page

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/OopsPage.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/OopsPage.java
@@ -3,6 +3,7 @@ package com.n4systems.fieldid.wicket.pages;
 import com.n4systems.fieldid.wicket.model.FIDLabelModel;
 import com.n4systems.util.ConfigEntry;
 import org.apache.log4j.Logger;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.util.string.StringValue;
@@ -12,6 +13,12 @@ import java.util.Random;
 public class OopsPage extends FieldIDFrontEndPage {
 
     private static final Logger logger = Logger.getLogger(OopsPage.class);
+
+    public static String PARAM_ERROR_TYPE_KEY = "ERROR_TYPE";
+
+    public OopsPage() {
+        this(null);
+    }
 
     public OopsPage(PageParameters params) {
         super(params);
@@ -25,15 +32,29 @@ public class OopsPage extends FieldIDFrontEndPage {
 
     private void addComponents(PageParameters params) {
 
-        StringValue oopsErrorTypeStr = params.get("ERROR_TYPE");
-        OopsPageErrorType oopsErrorType = oopsErrorTypeStr != null ?
-                OopsPageErrorType.valueOf(oopsErrorTypeStr.toString()) : OopsPageErrorType.GENERIC;
+        OopsPageErrorType oopsErrorType;
+        if (params != null) {
+            StringValue oopsErrorTypeStr = params.get(PARAM_ERROR_TYPE_KEY);
+            oopsErrorType = oopsErrorTypeStr != null ?
+                            OopsPageErrorType.valueOf(oopsErrorTypeStr.toString()) : OopsPageErrorType.GENERIC;
+        }
+        else
+            oopsErrorType = OopsPageErrorType.GENERIC;
 
         Label errorTitleLabel;
         Label messageTextLabel;
         Label helpTextLabel;
+        WebMarkupContainer homeButton = new WebMarkupContainer("homeButton");
+        homeButton.setVisible(false);
 
         switch(oopsErrorType) {
+            case NEEDS_PERMISSION:
+                errorTitleLabel = new Label("errorTitle", new FIDLabelModel("title.oops_needs_permission"));
+                messageTextLabel = new Label("messageText", new FIDLabelModel("label.missing_permission_for_action"));
+                helpTextLabel = new Label("helpText", new FIDLabelModel("label.contact_admin_for_info"));
+                homeButton.setVisible(true);
+                break;
+
             default:
                 errorTitleLabel = new Label("errorTitle", new FIDLabelModel("title.oops_something_went_wrong"));
                 messageTextLabel = new Label("messageText", new FIDLabelModel("message.problem_has_occured"));
@@ -46,6 +67,7 @@ public class OopsPage extends FieldIDFrontEndPage {
         add(errorTitleLabel);
         add(messageTextLabel);
         add(helpTextLabel);
+        add(homeButton);
     }
 
     private String generateTicketNumber() {
