9b05da7862f7d66df2cbd25aab832435ca3c83f2
agrabovskis <arvid.grabovskis@ecompliance.com>
2018-03-19 11:39:38 -0400

    WEB-6711 Improve webservice exception logging to include tenant id and user id. Also change return code for JSON parse exception from server error (500) to bad request (400).

--- fid-ws/src/main/java/com/n4systems/fieldid/config/CatchAllExceptionMapper.java
+++ fid-ws/src/main/java/com/n4systems/fieldid/config/CatchAllExceptionMapper.java
@@ -1,7 +1,10 @@
 package com.n4systems.fieldid.config;
 
 import com.n4systems.fieldid.service.SecurityContextInitializer;
+import com.n4systems.services.SecurityContext;
 import org.apache.log4j.Logger;
+import org.codehaus.jackson.JsonParseException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -16,17 +19,39 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class CatchAllExceptionMapper implements ExceptionMapper<Throwable> {
 	private Logger logger = Logger.getLogger(CatchAllExceptionMapper.class);
-	
+
+	@Autowired
+	protected SecurityContext securityContext;
+
 	@Override
 	public Response toResponse(Throwable exception) {
 		/*
 		 * XXX: due to the Jersey filter design, the response filer cannot reset the security context if an exception is thrown.
 		 * It is important we do this here, as it's probable our last chance to clear it. - mf
 		 */
+
+		String tenantId;
+		String userId;
+		try {
+			tenantId = securityContext.getTenantSecurityFilter().getTenantId().toString();
+			userId = securityContext.getUserSecurityFilter().getUserId().toString();
+		}
+		catch(Exception ex) {
+			tenantId = "unknown";
+			userId = "unknown";
+		}
+
 		SecurityContextInitializer.resetSecurityContext();
-		
-		logger.error("Uncaught exception in webservice", exception);
-		return Response.status(Status.INTERNAL_SERVER_ERROR).type(MediaType.TEXT_PLAIN_TYPE).build();
+
+		if (exception instanceof JsonParseException) {
+			logger.error("JSON parse exception for tenantId '" + tenantId + "', userId '" + userId + "'", exception);
+			/* Json parse error is a bad data error as opposed to a server error */
+			return Response.status(Status.BAD_REQUEST).type(MediaType.TEXT_PLAIN_TYPE).build();
+		}
+		else {
+			logger.error("Uncaught exception in webservice for tenantId '" + tenantId + "', userId '" + userId + "'", exception);
+			return Response.status(Status.INTERNAL_SERVER_ERROR).type(MediaType.TEXT_PLAIN_TYPE).build();
+		}
 	}
 
 }
