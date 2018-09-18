dca93ec7584ab8805290e30bd08df4d1b6560c2d
agrabovskis <arvid.grabovskis@ecompliance.com>
2018-09-18 18:06:37 -0400

    Web-6627 - add temporary debugging info.

--- fid-web/src/main/java/com/n4systems/fieldid/sso/SsoUserDetailsService.java
+++ fid-web/src/main/java/com/n4systems/fieldid/sso/SsoUserDetailsService.java
@@ -2,19 +2,30 @@ package com.n4systems.fieldid.sso;
 
 import com.n4systems.model.sso.SsoSpMetadata;
 import com.n4systems.sso.dao.SsoMetadataDao;
+import org.apache.log4j.Logger;
+import org.opensaml.saml2.core.Attribute;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.ProviderNotFoundException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.saml.SAMLCredential;
 import org.springframework.security.saml.userdetails.SAMLUserDetailsService;
 
+import java.util.List;
+
 public class SsoUserDetailsService implements SAMLUserDetailsService {
+
+    private static final Logger logger = Logger.getLogger(SsoUserDetailsService.class);
 
     @Autowired
     private SsoMetadataDao ssoMetadataDao;
 
     @Override
     public Object loadUserBySAML(SAMLCredential credential) throws UsernameNotFoundException {
+
+        List<Attribute> attributes = credential.getAttributes();
+        for (Attribute attribute: attributes) {
+            logger.info("SsoUserDetailsService: credential attribute: " + attribute.getName());
+        }
 
         String spEntityId = credential.getLocalEntityID();
         SsoSpMetadata spMetadata = ssoMetadataDao.getSp(spEntityId);
