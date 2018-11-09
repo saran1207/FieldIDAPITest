9cea49e1e5b449a4a341f3921ad074053b092e28
Theo Cociorba <theo.cociorba@ecompliance.com>
2018-11-09 13:02:49 -0500

    Changed DB names to lower case

--- fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201810231319_RemoveSafetyNetworkForDelta.sql
+++ fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201810231319_RemoveSafetyNetworkForDelta.sql
@@ -1,3 +1,3 @@
-UPDATE ORG_CUSTOMER SET linked_id=NULL WHERE linked_id=15719078;
-DELETE FROM TYPEDORGCONNECTIONS WHERE orgconnection_id=175;
-DELETE FROM ORG_CONNECTIONS WHERE id=175;
+UPDATE org_customer SET linked_id=NULL WHERE linked_id=15719078;
+DELETE FROM typedorgconnections WHERE orgconnection_id=175;
+DELETE FROM org_connections WHERE id=175;
