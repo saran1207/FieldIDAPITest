fa1778d41fcc7f93f3c9fc661b7363307d3dcc3f
Greg Johnson <gregory.johnson@ecompliance.com>
2019-01-22 15:59:01 -0500

    FIDW-6964 added sql script

---
+++ fid-migrations/src/main/resources/com/n4systems/fieldid/migrator/migrations/M201901221427_AssetIndexesCleanup.sql
@@ -0,0 +1,110 @@
+
+alter table assets add unique key pk_assets_id (id);
+alter table assets drop index productserial_uniqueid_idx;
+
+alter table assets add unique key uk_assets_mobileguid (mobileguid);
+alter table assets drop index mobile_guid_unique;
+
+alter table assets add index idx_asssets_assigneduser_id (assigneduser_id);
+alter table assets drop index index_products_on_assigneduser_id;
+
+alter table assets add index idx_asssets_customerorder_id (customerorder_id);
+alter table assets drop index index_products_on_customerorder_id;
+
+alter table assets add index idx_asssets_identifiedby_id (identifiedby_id);
+alter table assets drop index index_products_on_identifiedby_uniqueid;
+
+alter table assets add index idx_asssets_mobileguid (mobileguid);
+alter table assets drop index index_products_on_mobileguid;
+
+alter table assets add index idx_asssets_modifiedby (modifiedby);
+alter table assets drop index index_products_on_modifiedby;
+
+alter table assets add index idx_asssets_purchaseorder (purchaseorder);
+alter table assets drop index index_productserial_on_purchaseorder;
+
+alter table assets add index idx_asssets_tenant_id (tenant_id);
+alter table assets drop index smart_search_customer_ref_number;
+alter table assets drop index smart_search_rfid;
+alter table assets drop index smart_search_serial_number;
+
+alter table assets add index idx_asssets_rfidnumber (rfidnumber);
+alter table assets drop index index_products_on_rfidnumber;
+
+alter table assets add index idx_asssets_identifier (identifier);
+alter table assets drop index index_products_on_serialnumber;
+
+alter table assets add index idx_asssets_shoporder_id (shoporder_id);
+alter table assets drop index index_products_on_shoporder_id;
+alter table assets drop index productserial_rordermaster_idx;
+
+alter table assets add index idx_asssets_state (state);
+alter table assets drop index index_products_on_state;
+
+alter table assets add index idx_asssets_type_id (type_id);
+alter table assets drop index index_products_on_type_id;
+
+alter table assets add index idx_asssets_owner_id (owner_id);
+alter table assets drop index fk_products_owner;
+
+alter table assets add index idx_asssets_published (published);
+alter table assets drop index index_products_on_published;
+
+alter table assets add index idx_asssets_linked_id (linked_id);
+alter table assets drop index fk_linked_product_id;
+
+alter table assets add index idx_asssets_network_id (network_id);
+alter table assets drop index index_products_on_network_id;
+
+alter table assets add index idx_asssets_predefinedlocation_id (predefinedlocation_id);
+
+set @exist := (select count(*) from information_schema.statistics where table_name = 'assets' and index_name = 'fk_products_predefinedlocations' and table_schema = database());
+set @sqlstmt := if( @exist > 0, 'alter table assets drop index fk_products_predefinedlocations;', 'select ''INFO: nothing to drop''');
+PREPARE stmt FROM @sqlstmt;
+EXECUTE stmt;
+
+alter table assets add index idx_asssets_assetstatus_id (assetstatus_id);
+alter table assets drop index index_assets_on_assetstatus_id;
+
+alter table assets add index idx_asssets_createdby (createdby);
+
+set @exist := (select count(*) from information_schema.statistics where table_name = 'assets' and index_name = 'fk_created_by_user' and table_schema = database());
+set @sqlstmt := if( @exist > 0, 'alter table assets drop index fk_created_by_user;', 'select ''INFO: nothing to drop''');
+PREPARE stmt FROM @sqlstmt;
+EXECUTE stmt;
+
+alter table assets add index idx_asssets_location (location);
+alter table assets drop index freeform_location_idx;
+
+alter table assets add index idx_asssets_state_tenantid_linkedid (`state`,`tenant_id`,`linked_id`);
+alter table assets drop index index_products_state_tenantid_linkedid;
+
+alter table assets add index idx_asssets_ownerid_tenantid_mobileguid_state (`owner_id`,`tenant_id`,`mobileguid`,`state`);
+alter table assets drop index index_owner_id_tenant_id_mobileguid_state;
+
+create index idx_assets_identified on assets (identified desc);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
