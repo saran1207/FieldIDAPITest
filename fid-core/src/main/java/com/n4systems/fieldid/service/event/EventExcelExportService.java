6db040a10e59ed6c766aba51303894a59d5e7565
agrabovskis <arvid.grabovskis@ecompliance.com>
2019-01-30 15:27:02 -0500

    FIDW-6989 Add NewRelic logging for Asset export to Excel, Event export to Excel and Event print background tasks.

--- fid-core/src/main/java/com/n4systems/fieldid/service/event/EventExcelExportService.java
+++ fid-core/src/main/java/com/n4systems/fieldid/service/event/EventExcelExportService.java
@@ -3,12 +3,16 @@ package com.n4systems.fieldid.service.event;
 import com.n4systems.ejb.PageHolder;
 import com.n4systems.fieldid.service.export.ExcelExportService;
 import com.n4systems.fieldid.service.search.ReportService;
+import com.n4systems.model.downloadlink.DownloadLink;
 import com.n4systems.model.search.EventReportCriteria;
 import com.n4systems.util.persistence.search.ResultTransformer;
 import com.n4systems.util.views.TableView;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class EventExcelExportService extends ExcelExportService<EventReportCriteria> {
+
+    static Logger logger = Logger.getLogger(EventExcelExportService.class);
 
     @Autowired
     private ReportService reportService;
@@ -21,5 +25,10 @@ public class EventExcelExportService extends ExcelExportService<EventReportCrite
     @Override
     protected PageHolder<TableView> performSearch(EventReportCriteria criteria, ResultTransformer<TableView> resultTransformer, int pageNumber, int pageSize, boolean useSelection) {
         return reportService.performSearch(criteria,resultTransformer,pageNumber, pageSize, useSelection);
+    }
+
+    @Override
+    protected void logTaskStartMsg(EventReportCriteria criteria, String downloadUrl, DownloadLink downloadLink) {
+        logger.info("Started Excel export of " + criteria.getSelection().getNumSelectedIds() + " events for download " + downloadLink);
     }
 }
