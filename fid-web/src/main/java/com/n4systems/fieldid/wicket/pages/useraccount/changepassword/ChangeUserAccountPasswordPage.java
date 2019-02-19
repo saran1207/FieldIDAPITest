4dc35c76220c46d9814c0615a8e59c1c0135f183
Theo Cociorba <theo.cociorba@ecompliance.com>
2019-02-19 11:28:33 -0500

    FIDW-5726 Moved UserAccount Form Panel into the ChangeUserAccountPasswordPage

--- fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/useraccount/changepassword/ChangeUserAccountPasswordPage.java
+++ fid-web/src/main/java/com/n4systems/fieldid/wicket/pages/useraccount/changepassword/ChangeUserAccountPasswordPage.java
@@ -1,73 +1,158 @@
 package com.n4systems.fieldid.wicket.pages.useraccount.changepassword;
 
+import com.n4systems.fieldid.handler.password.PasswordHelper;
+import com.n4systems.fieldid.service.tenant.TenantSettingsService;
 import com.n4systems.fieldid.service.user.UserService;
 import com.n4systems.fieldid.wicket.FieldIDSession;
-import com.n4systems.fieldid.wicket.components.feedback.FIDFeedbackPanel;
 import com.n4systems.fieldid.wicket.model.FIDLabelModel;
-import com.n4systems.fieldid.wicket.pages.setup.user.UsersListPage;
 import com.n4systems.fieldid.wicket.pages.useraccount.AccountSetupPage;
+import com.n4systems.model.security.PasswordPolicy;
 import com.n4systems.model.user.User;
-import com.n4systems.security.UserType;
+import org.apache.log4j.Logger;
 import org.apache.wicket.Component;
-import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.behavior.Behavior;
+import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.SubmitLink;
-import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.form.FormComponent;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.validation.EqualPasswordInputValidator;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.validation.IValidatable;
+import org.apache.wicket.validation.IValidator;
+import org.apache.wicket.validation.ValidationError;
 
 public class ChangeUserAccountPasswordPage extends AccountSetupPage {
+    private static final Logger logger = Logger.getLogger(ChangeUserAccountPasswordPage.class);
+    private String currentPassword;
+    private String newPassword;
+    private String confirmPassword;
+
+    private PasswordTextField currentPasswordField;
+    private PasswordTextField newPasswordField;
+    private PasswordTextField confirmPasswordField;
 
     @SpringBean protected UserService userService;
+    @SpringBean private TenantSettingsService tenantSettingsService;
 
-    protected UserType userType;
     private Long uniqueId;
-    protected IModel<User> userModel;
-
-    public ChangeUserAccountPasswordPage(UserType userType) {
-        this.userType = userType;
-    }
+    private IModel<User> userModel;
 
     public ChangeUserAccountPasswordPage(IModel<User> userModel) {
         super();
         this.uniqueId = userModel.getObject().getId();
         this.userModel = userModel;
+        addComponents();
     }
 
     public ChangeUserAccountPasswordPage(PageParameters parameters) {
         super(parameters);
         uniqueId = parameters.get("uniqueID").toLong();
         userModel = Model.of(loadExistingUser());
+        addComponents();
     }
 
     public ChangeUserAccountPasswordPage() {
         super();
         uniqueId = getSessionUser().getId();
         userModel = Model.of(loadExistingUser());
+        addComponents();
     }
 
-    protected void doSave() {
-        update();
-        setResponsePage(ChangeUserAccountPasswordPage.class);
+    private void addComponents() {
+
+        Form form = new Form("form") {
+            @Override
+            protected void onSubmit() {
+                updateUser();
+                setResponsePage(ChangeUserAccountPasswordPage.class);
+            }
+        };
+        add(form);
+
+        form.add(new Link("cancel") {
+            @Override
+            public void onClick() {
+                setResponsePage(ChangeUserAccountPasswordPage.class);
+            }
+        });
+
+        form.add(new Label("userName", new PropertyModel<String>(userModel, "userID")));
+
+        form.add(currentPasswordField = new PasswordTextField("currentPassword", new PropertyModel<String>(this, "newPassword")));
+        currentPasswordField.add(new IValidator<String>() {
+            @Override
+            public void validate(IValidatable validatable) {
+                if(userService.authenticateUserByPassword(FieldIDSession.get().getTenant().getName(),userModel.getObject().getUserID(),(String) validatable.getValue()) == null) {
+                    ValidationError error = new ValidationError();
+                    error.addMessageKey("errors.data.userpassword");
+                    validatable.error(error);
+                }
+            }
+        });
+        currentPasswordField.add(new Behavior() {
+            @Override public void onComponentTag(Component c, ComponentTag tag) {
+                FormComponent fc = (FormComponent) c;
+                if (!fc.isValid()) {
+                    tag.append("class", "error", " ");
+                }
+            }
+        });
+        currentPasswordField.setOutputMarkupPlaceholderTag(true);
+
+        form.add(newPasswordField = new PasswordTextField("newPassword", new PropertyModel<String>(this, "newPassword")));
+        newPasswordField.add(new Behavior() {
+            @Override public void onComponentTag(Component c, ComponentTag tag) {
+                FormComponent fc = (FormComponent) c;
+                if (!fc.isValid()) {
+                    tag.append("class", "error", " ");
+                }
+            }
+        });
+        newPasswordField.setOutputMarkupPlaceholderTag(true);
+        form.add(confirmPasswordField = new PasswordTextField("confirmPassword", new PropertyModel<String>(this, "confirmPassword")));
+
+        confirmPasswordField.add(new Behavior() {
+            @Override public void onComponentTag(Component c, ComponentTag tag) {
+                FormComponent fc = (FormComponent) c;
+                if (!fc.isValid()) {
+                    tag.append("class", "error", " ");
+                }
+            }
+        });
+        confirmPasswordField.setOutputMarkupPlaceholderTag(true);
+
+        final EqualPasswordInputValidator equalPasswordInputValidator = new EqualPasswordInputValidator( newPasswordField, confirmPasswordField);
+
+        newPasswordField.add(new IValidator<String>() {
+            @Override
+            public void validate(IValidatable<String> validatable) {
+                PasswordHelper passwordHelper = new PasswordHelper(getPasswordPolicy());
+                if (!passwordHelper.isValidPassword(validatable.getValue())) {
+
+                    PasswordPolicy policy = passwordHelper.getPasswordPolicy();
+                    String message = new FIDLabelModel("error.password_policy", policy.getMinLength() + "",
+                            policy.getMinCapitals() + "",
+                            policy.getMinNumbers() + "",
+                            policy.getMinSymbols() + "").getObject();
+
+                    ValidationError error = new ValidationError();
+                    error.setMessage(message);
+                    validatable.error(error);
+                }
+            }
+        });
+        form.add(equalPasswordInputValidator);
     }
 
-    protected Component createAccountPanel(String id, Form form) {
-        return new UserAccountFormPanel(id, userModel,form);
-    }
 
-    protected Component accountPanel;
-
-    protected User loadExistingUser() {
-        return userService.getUser(uniqueId);
-    }
-
-    @Override
-    protected void onInitialize() {
-        super.onInitialize();
-        add(new AddUserForm("addUserForm"));
+    private User loadExistingUser() {
+        return getCurrentUser();
     }
 
     @Override
@@ -75,46 +160,34 @@ public class ChangeUserAccountPasswordPage extends AccountSetupPage {
         return new Label(labelId, new FIDLabelModel("label.change_password"));
     }
 
-    protected UserAccountFormPanel getUserAccountFormPanel() {
-        return (UserAccountFormPanel)accountPanel;
-    }
-
-    protected User update() {
+    private void updateUser() {
         User user = userModel.getObject();
-        user.updatePassword(getUserAccountFormPanel().getNewPassword());
+        user.updatePassword(getNewPassword());
 
         userService.update(user);
-
-        return user;
+        logger.info("account profile password has been changed for " + getSessionUser().getUserID());
+        FieldIDSession.get().info(new FIDLabelModel("message.users_password_updated").getObject());
     }
 
-    protected void addConfirmBehavior(SubmitLink submitLink) {
+    private PasswordPolicy getPasswordPolicy() {
+        return tenantSettingsService.getTenantSettings().getPasswordPolicy();
     }
 
-    protected void onOwnerPicked(AjaxRequestTarget target) {}
-
-
-    class AddUserForm extends Form {
-
-        SubmitLink submitLink;
-
-        public AddUserForm(String id) {
-            super(id);
-
-            add(accountPanel = createAccountPanel("accountPanel",this));
-
-            add(submitLink = new SubmitLink("save"));
-
-            addConfirmBehavior(submitLink);
-
-            add(new BookmarkablePageLink<UsersListPage>("cancel", ChangeUserAccountPasswordPage.class));
-        }
-
-        @Override
-        protected void onSubmit() {
-            doSave();
-            FieldIDSession.get().info(new FIDLabelModel("message.users_password_updated").getObject());
-        }
+    private String getNewPassword() {
+        return newPassword;
     }
+
+    private void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    private String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    private void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
 
 }
