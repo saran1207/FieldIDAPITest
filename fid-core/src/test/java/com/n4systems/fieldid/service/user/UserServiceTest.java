0ef981d546372c29e09cfa16770544f2cc5616ef
Theo Cociorba <theo.cociorba@ecompliance.com>
2019-02-26 14:46:54 -0500

    FIDW-5724 Added Mockito junit test

--- fid-core/src/test/java/com/n4systems/fieldid/service/user/UserServiceTest.java
+++ fid-core/src/test/java/com/n4systems/fieldid/service/user/UserServiceTest.java
@@ -1,58 +1,75 @@
 package com.n4systems.fieldid.service.user;
 
-import com.n4systems.fieldid.junit.FieldIdServiceTest;
+import com.n4systems.exceptions.InvalidQueryException;
 import com.n4systems.fieldid.service.PersistenceService;
-import com.n4systems.fieldid.service.org.OrgService;
-import com.n4systems.model.Tenant;
-import com.n4systems.model.builders.OrgBuilder;
-import com.n4systems.model.builders.UserBuilder;
-import com.n4systems.model.orgs.BaseOrg;
-import com.n4systems.model.security.UserSecurityFilter;
-import com.n4systems.model.user.User;
-import com.n4systems.services.SecurityContext;
-import com.n4systems.test.TestMock;
-import com.n4systems.test.TestTarget;
+import com.n4systems.model.security.TenantOnlySecurityFilter;
+import com.n4systems.model.user.UserGroup;
+import com.n4systems.util.persistence.QueryBuilder;
+import com.n4systems.util.persistence.WhereParameter;
 import org.junit.Before;
 import org.junit.Test;
 
-import static com.n4systems.model.builders.TenantBuilder.n4;
+import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
-public class UserServiceTest extends FieldIdServiceTest {
+public class UserServiceTest {
 
-    private @TestTarget UserService userService;
-    private @TestMock OrgService orgService;
-    private @TestMock UserGroupService userGroupService;
-    private @TestMock PersistenceService persistenceService;
-    private @TestMock SecurityContext securityContext;
-    private User user;
-    private User currentUser;
-    private BaseOrg org;
-    private UserSecurityFilter securityFilter;
-    private Tenant tenant;
+    private UserService userServiceTest;
+    private PersistenceService persistenceServiceTest;
 
-    @Override
     @Before
     public void setUp() {
-        super.setUp();
-        tenant = n4();
-        org = OrgBuilder.aPrimaryOrg().build();
-        user = UserBuilder.aFullUser().build();
-        securityFilter = new UserSecurityFilter(user);
-        user.setTenant(tenant);
-        user.setOwner(org);
-        user.setUserID("arvid");
-
-        currentUser = UserBuilder.aFullUser().build();
-        currentUser.setTenant(tenant);
-        currentUser.setOwner(org);
-        currentUser.setUserID("theoc");
+        userServiceTest = mock(UserService.class);
+        persistenceServiceTest = mock(PersistenceService.class);
     }
 
     @Test
-    public void test_findDuplicateUser() {
-        boolean isUserIdUnique = userService.userIdIsUnique(tenant.getId(),null,currentUser.getId());
-        assertTrue(isUserIdUnique);
+    public void userIdIsUnique_When_User_Is_Unique_True() {
+        when(userServiceTest.userIdIsUnique(123L,"testUnique",4321L)).thenReturn(true);
+        assertTrue(userServiceTest.userIdIsUnique(123L,"testUnique",4321L));
+        verify(userServiceTest,atLeastOnce()).userIdIsUnique(123L,"testUnique",4321L);
     }
 
+    @Test
+    public void userIdIsUnique_When_User_Is_Not_Unique_True() {
+        when(userServiceTest.userIdIsUnique(123L,"testFalse",4321L)).thenReturn(false);
+        assertFalse(userServiceTest.userIdIsUnique(123L,"testFalse",4321L));
+        verify(userServiceTest,atLeastOnce()).userIdIsUnique(123L,"testFalse",4321L);
+    }
+
+    @Test
+    public void userIdIsUnique_Persistence_Returns_A_Result_True() {
+
+        QueryBuilder<Long> queryBuilder = new QueryBuilder<Long>(UserGroup.class, new TenantOnlySecurityFilter(123L)).setCountSelect();
+        queryBuilder.addWhere(WhereParameter.Comparator.EQ, "userID", "useID", "testUnique");
+        queryBuilder.addWhere(WhereParameter.Comparator.NE, "id", "id", 4321L);
+
+        when(persistenceServiceTest.exists(queryBuilder)).thenReturn(true);
+        assertTrue(persistenceServiceTest.exists(queryBuilder));
+        verify(persistenceServiceTest,atLeastOnce()).exists(queryBuilder);
+    }
+
+    @Test
+    public void userIdIsUnique_Persistence_Returns_No_Results_True() {
+
+        QueryBuilder<Long> queryBuilder = new QueryBuilder<Long>(UserGroup.class, new TenantOnlySecurityFilter(123L)).setCountSelect();
+        queryBuilder.addWhere(WhereParameter.Comparator.EQ, "userID", "useID", "testFalse");
+        queryBuilder.addWhere(WhereParameter.Comparator.NE, "id", "id", 4321L);
+
+        when(persistenceServiceTest.exists(queryBuilder)).thenReturn(false);
+        assertFalse(persistenceServiceTest.exists(queryBuilder));
+        verify(persistenceServiceTest,atLeastOnce()).exists(queryBuilder);
+    }
+
+    @Test(expected=InvalidQueryException.class)
+    public void userIdIsUnique_Persistence_Has_InvalidQueryException_True() {
+
+        QueryBuilder<Long> queryBuilder = new QueryBuilder<Long>(UserGroup.class, new TenantOnlySecurityFilter(123L)).setCountSelect();
+        queryBuilder.addWhere(WhereParameter.Comparator.EQ, "userIDTest", "useIDTest", "testException");
+        queryBuilder.addWhere(WhereParameter.Comparator.NE, "id", "id", 4321L);
+
+        when(persistenceServiceTest.exists(queryBuilder)).thenThrow(new InvalidQueryException("The expected message for InvalidQueryException"));
+        persistenceServiceTest.exists(queryBuilder);
+    }
 }
