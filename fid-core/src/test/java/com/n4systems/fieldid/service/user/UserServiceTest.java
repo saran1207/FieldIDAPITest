ca875239944b7149917b0d98974a2894957a97b1
Theo Cociorba <theo.cociorba@ecompliance.com>
2019-02-27 16:07:22 -0500

    FIDW-5727 Re-factored junit test using mockito

--- fid-core/src/test/java/com/n4systems/fieldid/service/user/UserServiceTest.java
+++ fid-core/src/test/java/com/n4systems/fieldid/service/user/UserServiceTest.java
@@ -1,58 +1,56 @@
 package com.n4systems.fieldid.service.user;
 
-import com.n4systems.fieldid.junit.FieldIdServiceTest;
+import com.n4systems.exceptions.InvalidQueryException;
 import com.n4systems.fieldid.service.PersistenceService;
-import com.n4systems.fieldid.service.org.OrgService;
-import com.n4systems.model.Tenant;
-import com.n4systems.model.builders.OrgBuilder;
-import com.n4systems.model.builders.UserBuilder;
-import com.n4systems.model.orgs.BaseOrg;
-import com.n4systems.model.security.UserSecurityFilter;
-import com.n4systems.model.user.User;
-import com.n4systems.services.SecurityContext;
-import com.n4systems.test.TestMock;
-import com.n4systems.test.TestTarget;
+import com.n4systems.util.persistence.QueryBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
-import static com.n4systems.model.builders.TenantBuilder.n4;
+import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
-public class UserServiceTest extends FieldIdServiceTest {
 
-    private @TestTarget UserService userService;
-    private @TestMock OrgService orgService;
-    private @TestMock UserGroupService userGroupService;
-    private @TestMock PersistenceService persistenceService;
-    private @TestMock SecurityContext securityContext;
-    private User user;
-    private User currentUser;
-    private BaseOrg org;
-    private UserSecurityFilter securityFilter;
-    private Tenant tenant;
+public class UserServiceTest {
 
-    @Override
+    private UserService userService;
+    private PersistenceService persistenceServiceMock;
+
     @Before
     public void setUp() {
-        super.setUp();
-        tenant = n4();
-        org = OrgBuilder.aPrimaryOrg().build();
-        user = UserBuilder.aFullUser().build();
-        securityFilter = new UserSecurityFilter(user);
-        user.setTenant(tenant);
-        user.setOwner(org);
-
-        currentUser = UserBuilder.aFullUser().build();
-        currentUser.setTenant(tenant);
-        currentUser.setOwner(org);
+        userService = new UserService();
+        persistenceServiceMock = mock(PersistenceService.class);
+        userService.setPersistenceService(persistenceServiceMock);
     }
 
     @Test
-    public void test_UserSecurityCardNumberIsUnique() {
-        boolean isUserSecurityCardNumberUnique = userService.userSecurityCardNumberIsUnique(tenant.getId(),null,currentUser.getId());
-        assertTrue(isUserSecurityCardNumberUnique);
-        isUserSecurityCardNumberUnique = userService.userSecurityCardNumberIsUnique(tenant.getId(),"",currentUser.getId());
-        assertTrue(isUserSecurityCardNumberUnique);
+    public void userSecurityCardNumberIsUnique_When_CardNumber_Is_Unique_True() {
+        when(persistenceServiceMock.find(any(QueryBuilder.class))).thenReturn(0L);
+        assertTrue(userService.userSecurityCardNumberIsUnique(123L,"card_Number_Unique",4321L));
+    }
+
+    @Test
+    public void userSecurityCardNumberIsUnique_When_CardNumber_Is_Not_Unique_True() {
+        when(persistenceServiceMock.find(any(QueryBuilder.class))).thenReturn(1L);
+        assertFalse(userService.userSecurityCardNumberIsUnique(123L,"card_Number_Duplicate",4321L));
+    }
+
+    @Test
+    public void userSecurityCardNumberIsUnique_When_CardNumber_Is_Null_True() {
+        when(persistenceServiceMock.find(any(QueryBuilder.class))).thenReturn(1L);
+        assertTrue(userService.userSecurityCardNumberIsUnique(123L,null,4321L));
+    }
+
+    @Test
+    public void userSecurityCardNumberIsUnique_When_CardNumber_Is_Empty_True() {
+        when(persistenceServiceMock.find(any(QueryBuilder.class))).thenReturn(1L);
+        assertTrue(userService.userSecurityCardNumberIsUnique(123L,"",4321L));
+    }
+
+    @Test(expected=InvalidQueryException.class)
+    public void userIdIsUnique_Persistence_Has_InvalidQueryException_True() {
+        when(persistenceServiceMock.find(any(QueryBuilder.class))).thenThrow(new InvalidQueryException("The expected message for InvalidQueryException"));
+        userService.userSecurityCardNumberIsUnique(123L,"user_id",4321L);
     }
 
 }
